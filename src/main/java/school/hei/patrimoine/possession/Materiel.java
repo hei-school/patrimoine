d5523bedd58e649b80f3049c6cdc76df204e5ca4
ToavinaMaria <ToavinaMaria>
2024-05-14 21:10:47 +0300

    chore : add material value in the futur

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,8 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +16,11 @@ public final class Materiel extends Possession {
 
   @Override
   public int valeurComptableFuture(Instant tFutur) {
-    throw new NotImplemented();
+   ZonedDateTime anneeFutur = tFutur.atZone(ZoneId.systemDefault());
+   ZonedDateTime anneePresent = t.atZone(ZoneId.systemDefault());
+
+   int moyenneAnnee = anneeFutur.getYear() - anneePresent.getYear();
+
+   return (int) (valeurComptable*(moyenneAnnee*tauxDAppreciationAnnuelle));
   }
 }
