5a96e1fd1b5e0baf18d4c8232969e1794c092295
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-05-14 21:51:33 +0300

    feat: implement valeurComptableFuture

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -2,6 +2,7 @@ package school.hei.patrimoine.possession;
 
 import school.hei.patrimoine.NotImplemented;
 
+import java.time.Duration;
 import java.time.Instant;
 
 public final class Materiel extends Possession {
@@ -14,6 +15,9 @@ public final class Materiel extends Possession {
 
   @Override
   public int valeurComptableFuture(Instant tFutur) {
-    throw new NotImplemented();
+    Duration duree = Duration.between(t, tFutur);
+    int nombre_d_annees = (int) (duree.toDays() / 365);
+    int valeurComptableFuture = (int) (getValeurComptable() * tauxDAppreciationAnnuelle * nombre_d_annees);
+    return valeurComptableFuture;
   }
 }
