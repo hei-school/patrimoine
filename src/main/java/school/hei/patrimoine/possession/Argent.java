1df4a20e71914ad0b44dbc061c8cc76efea4c987
Sun-2302 <hei.dera.932@gmail.com>
2024-05-14 19:53:33 +0300

    feat: implement valeurComptableFuture

--- src/main/java/school/hei/patrimoine/possession/Argent.java
+++ src/main/java/school/hei/patrimoine/possession/Argent.java
@@ -1,16 +1,14 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
-
 import java.time.Instant;
 
 public final class Argent extends Possession {
-  public Argent(String nom, Instant t, int valeurComptable) {
-    super(nom, t, valeurComptable);
-  }
+    public Argent(String nom, Instant t, int valeurComptable) {
+        super(nom, t, valeurComptable);
+    }
 
-  @Override
-  public int valeurComptableFuture(Instant tFutur) {
-    throw new NotImplemented();
-  }
+    @Override
+    public int valeurComptableFuture(Instant tFutur) {
+        return this.valeurComptable;
+    }
 }
