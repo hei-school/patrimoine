86be651636cf0c98208cd1e8debf4f075af0c3ec
Lou Andria <lou@hei.school>
2024-05-14 09:15:30 +0300

    refactor: add lombok

--- src/main/java/school/hei/patrimoine/possession/Possession.java
+++ src/main/java/school/hei/patrimoine/possession/Possession.java
@@ -1,16 +1,15 @@
 package school.hei.patrimoine.possession;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.Instant;
 
+@AllArgsConstructor
+@Getter
 public sealed class Possession permits
     Argent, Materiel, TrainDeVie {
   protected final String nom;
   protected final Instant t;
   protected final int valeurComptable;
-
-  public Possession(String nom, Instant t, int valeurComptable) {
-    this.nom = nom;
-    this.t = t;
-    this.valeurComptable = valeurComptable;
-  }
 }
