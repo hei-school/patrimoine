c3cc2f6b236ba8f933266634a72acfe28bd0cd11
rado <hei.rado.2@gmail.com>
2024-05-14 22:38:41 +0300

    Optimized future accounting value calculation method

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,9 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +17,16 @@ public final class Materiel extends Possession {
 
   @Override
   public int valeurComptableFuture(Instant tFutur) {
-    throw new NotImplemented();
+    ZonedDateTime zdt1 = this.t.atZone(ZoneId.systemDefault());
+    ZonedDateTime zdt2 = tFutur.atZone(ZoneId.systemDefault());
+    long nombreDeMois = ChronoUnit.MONTHS.between(zdt1, zdt2);
+    double valeurActuelle = getValeurComptable();
+    double tauxMensuel = tauxDAppreciationAnnuelle / 12.0;
+
+    for (int mois = 1; mois <= nombreDeMois; mois++) {
+      valeurActuelle *= (1 + tauxMensuel);
+    }
+
+    return (int) Math.round(valeurActuelle);
   }
 }
