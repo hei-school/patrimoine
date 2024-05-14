0ba128ec7573468001c2a2dbe4a37cddf5be15b6
Tombosoa <hei.nantenaina.4@gmail.com>
2024-05-14 18:47:23 +0300

    feat: can calculate future comptable value

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,8 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +16,9 @@ public final class Materiel extends Possession {
 
   @Override
   public int valeurComptableFuture(Instant tFutur) {
-    throw new NotImplemented();
+    ZoneId zoneId = ZoneId.systemDefault();
+    long anneePassee = ChronoUnit.YEARS.between(t.atZone(zoneId), tFutur.atZone(zoneId));
+    double valeurFuture = valeurComptable + (valeurComptable * tauxDAppreciationAnnuelle * anneePassee);
+    return (int) valeurFuture;
   }
 }
