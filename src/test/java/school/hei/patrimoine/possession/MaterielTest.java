2baad4250cf60e39f97490a22c6d4fdea21e3867
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-05-14 10:00:25 +0300

    chore:change mon_mas_s_apprecie_negativement_dans_le_futur to getValeurComptable in MaterielTest

--- src/test/java/school/hei/patrimoine/possession/MaterielTest.java
+++ src/test/java/school/hei/patrimoine/possession/MaterielTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MaterielTest {
     @Test
-    void mon_mas_s_apprecie_negativement_dans_le_futur(){
+    void mon_mas_s_apprecie_negativement_dans_le_futur() {
         var au26Oct2021 = Instant.parse("2021-10-26T00:00:00.00Z");
         var mac = new Materiel(
                 "MacBook Pro",
@@ -19,7 +19,7 @@ class MaterielTest {
 
         var au26Juin2024 = Instant.parse("2024-06-26T00:00:00.00Z");
         assertTrue(
-                mon_mas_s_apprecie_negativement_dans_le_futur() > mac.valeurComptableFuture(au26Juin2024);
-    );
+                mac.getValeurComptable() > mac.valeurComptableFuture(au26Juin2024));
+
     }
 }
