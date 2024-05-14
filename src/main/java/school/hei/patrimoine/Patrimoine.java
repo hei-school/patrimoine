863c46317d5150c8fbebad86e7de9ff4c3da7a5e
HarenaFiantso <fiantsorav@gmail.com>
2024-05-14 18:50:17 +0300

    chore: implement getValeurComptable

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -14,7 +14,11 @@ public record Patrimoine(
     if (possessions.isEmpty()) {
       return 0;
     }
-    throw new NotImplemented();
+    int somme = 0;
+    for (Possession possession : possessions) {
+      somme += possession.getValeurComptable();
+    }
+    return somme;
   }
 
   public Patrimoine projectionFuture(Instant tFutur) {
