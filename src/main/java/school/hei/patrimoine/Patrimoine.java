01a7c121cc2793638b9c93bc6145d524dc5d2be7
Tiavina19 <hei.tiavina@gmail.com>
2024-05-14 22:31:10 +0300

    chore : implement getValeurComptable

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -3,15 +3,19 @@ package school.hei.patrimoine;
 import school.hei.patrimoine.possession.Possession;
 
 import java.time.Instant;
+import java.util.Iterator;
 import java.util.Set;
 
-public record Patrimoine(
+public record  Patrimoine(
     Personne possesseur, Instant t, Set<Possession> possessions) {
 
   public int getValeurComptable() {
-    if (possessions.isEmpty()) {
-      return 0;
+    int valeur = 0;
+    Iterator<Possession> it = possessions.iterator();
+    while (it.hasNext()) {
+      Possession possession = it.next();
+      valeur += possession.getValeurComptable();
     }
-    throw new NotImplemented();
+    return valeur;
   }
 }
