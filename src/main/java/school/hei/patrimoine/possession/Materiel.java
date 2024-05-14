cb65901eb5c5d5b8186f1d9fde64093a61873e5f
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-05-14 18:38:09 +0300

    feat: implement valeurComptableFuture(Instant tFutur)

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,19 +1,27 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.util.Date;
 
 public final class Materiel extends Possession {
-  private final double tauxDAppreciationAnnuelle;
+    private final double tauxDAppreciationAnnuelle;
 
-  public Materiel(String nom, Instant t, int valeurComptable, double tauxDAppreciationAnnuelle) {
-    super(nom, t, valeurComptable);
-    this.tauxDAppreciationAnnuelle = tauxDAppreciationAnnuelle;
-  }
+    public Materiel(String nom, Instant t, int valeurComptable, double tauxDAppreciationAnnuelle) {
+        super(nom, t, valeurComptable);
+        this.tauxDAppreciationAnnuelle = tauxDAppreciationAnnuelle;
+    }
 
-  @Override
-  public int valeurComptableFuture(Instant tFutur) {
-    throw new NotImplemented();
-  }
+    @Override
+    public int valeurComptableFuture(Instant tFutur) {
+        Date t1 = Date.from(t);
+        Date t1Futur = Date.from(tFutur);
+        long differenceDansLeTempsEnMilliseconde = t1Futur.getTime() - t1.getTime();
+        long jours = (differenceDansLeTempsEnMilliseconde / (1000 * 60 * 60 * 24)) % 365;
+        long mois = (differenceDansLeTempsEnMilliseconde / (1_000L * 60 * 60 * 24 * 365)) % 12;
+        long années = (differenceDansLeTempsEnMilliseconde / (1_000L * 60 * 60 * 24 * 365));
+        double tauxDappreciationMensuelle = tauxDAppreciationAnnuelle / 12;
+        double tauxDappreciationJournalier = tauxDappreciationMensuelle / 30;
+        return (int) ((tauxDAppreciationAnnuelle * années) + (tauxDappreciationMensuelle * mois) + (tauxDappreciationJournalier * jours));
+    }
 }
