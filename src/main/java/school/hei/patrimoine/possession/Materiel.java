60ed536a661db4aeb0cd759956b62e508df549ea
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-05-14 20:28:47 +0300

    chore:implement code to show futursComptableValue

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -2,7 +2,10 @@ package school.hei.patrimoine.possession;
 
 import school.hei.patrimoine.NotImplemented;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +17,8 @@ public final class Materiel extends Possession {
 
   @Override
   public int valeurComptableFuture(Instant tFutur) {
-    throw new NotImplemented();
+    int nombreAnnées = ((tFutur.  atZone(ZoneId.systemDefault()).getYear()) - t.atZone(ZoneId.systemDefault()).getYear());
+    double ValeurFutur = valeurComptable + (valeurComptable * (tauxDAppreciationAnnuelle * nombreAnnées));
+      return (int) ValeurFutur;
   }
 }
