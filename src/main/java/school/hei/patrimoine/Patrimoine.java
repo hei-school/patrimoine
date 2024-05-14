28c9ba1b9fc52b9f7ca2949309ad39619e503827
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-05-14 20:45:15 +0300

    feat: Complete the implementation of getValeurComptable()

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -6,12 +6,18 @@ import java.time.Instant;
 import java.util.Set;
 
 public record Patrimoine(
-    Personne possesseur, Instant t, Set<Possession> possessions) {
+        Personne possesseur, Instant t, Set<Possession> possessions) {
 
-  public int getValeurComptable() {
-    if (possessions.isEmpty()) {
-      return 0;
+    public int getValeurComptable() {
+        if (possessions.isEmpty()) {
+            return 0;
+        } else {
+            int total = 0;
+            for (Possession possession : possessions) {
+                total += possession.getValeurComptable();
+                return total;
+            }
+        }
+        throw new NotImplemented();
     }
-    throw new NotImplemented();
-  }
 }
