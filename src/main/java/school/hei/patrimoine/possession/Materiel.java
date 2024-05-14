8d2b0ed73596f94126fd612e4496caf6a53b6805
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-05-14 21:48:19 +0300

    feat: implement valeurComptableFuture calculation

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,7 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +15,9 @@ public final class Materiel extends Possession {
 
   @Override
   public int valeurComptableFuture(Instant tFutur) {
-    throw new NotImplemented();
+    long differenceJour = ChronoUnit.DAYS.between(t, tFutur);
+    long anneeApprox = differenceJour / 365;
+    double futureValue = getValeurComptable() * Math.pow(1 + tauxDAppreciationAnnuelle, anneeApprox);
+    return (int) futureValue;
   }
 }
