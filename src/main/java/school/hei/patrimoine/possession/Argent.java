e3e127069af0f938e985c11a7fde609af47f96b9
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-05-14 20:24:24 +0300

    feat: calcule valeur comptable future pour argent .

--- src/main/java/school/hei/patrimoine/possession/Argent.java
+++ src/main/java/school/hei/patrimoine/possession/Argent.java
@@ -1,16 +1,23 @@
 package school.hei.patrimoine.possession;
 
+import lombok.Getter;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
-
+import java.time.temporal.ChronoUnit;
+@Getter
 public final class Argent extends Possession {
   public Argent(String nom, Instant t, int valeurComptable) {
     super(nom, t, valeurComptable);
   }
-
+private static final double tauxDinteret = 0.04 / 365;
   @Override
   public int valeurComptableFuture(Instant tFutur) {
-    throw new NotImplemented();
+    if (tFutur.isBefore(this.t)) {
+      return this.valeurComptable;
+    }
+    long joursEntre = ChronoUnit.DAYS.between(this.t, tFutur);
+    double valeureFuture = this.getValeurComptable()  * Math.pow(1 + tauxDinteret, joursEntre);
+    return (int) valeureFuture;
   }
 }
--- src/test/java/school/hei/patrimoine/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/PatrimoineTest.java
@@ -2,8 +2,10 @@ package school.hei.patrimoine;
 
 import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.possession.Argent;
+import school.hei.patrimoine.possession.Possession;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -36,5 +38,16 @@ class PatrimoineTest {
             new Argent("Compte courant", au13mai24, 600_000)));
 
     assertEquals(1_200_000, patrimoineIloAu13mai24.getValeurComptable());
+  }
+  @Test
+  void valeur_comptable_future(){
+    var ilo = new Personne("Ilo");
+
+    var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
+    var au13mai29 = Instant.parse("2029-05-13T00:00:00.00Z");
+
+    var argent = new Argent("Compte epargne", au13mai24, 200_000);
+
+    assertEquals(200_000 * Math.pow(1 + 0.04 / 365, ChronoUnit.DAYS.between(au13mai24, au13mai29)),argent.valeurComptableFuture(au13mai29), 0.01);
   }
 }
