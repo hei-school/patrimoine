0808c2327189acf07e2c3a0782929d233bf3fcbf
tokiniaina <hei.tokiniaina@gmail.com>
2024-05-15 23:58:07 +0300

    feat: implement projectionFuture at Materiel

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,9 +1,13 @@
 package school.hei.patrimoine.possession;
 
+import lombok.Getter;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
+@Getter
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
 
@@ -12,8 +16,22 @@ public final class Materiel extends Possession {
     this.tauxDAppreciationAnnuelle = tauxDAppreciationAnnuelle;
   }
 
+
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    Instant instant = Instant.now();
+
+    var datePresent = LocalDate.ofInstant(instant, ZoneId.systemDefault());
+    var future= LocalDate.ofInstant(tFutur,ZoneId.systemDefault());
+
+    var annee = future.getYear() - datePresent.getYear();
+
+    double diminution = 0;
+
+    for (int i = 0; i < annee; i++) {
+      diminution=diminution+tauxDAppreciationAnnuelle;
+    }
+    int valeurFuture= (int) (valeurComptable*diminution);
+    return  new Materiel(nom,t,valeurFuture,tauxDAppreciationAnnuelle);
   }
 }
