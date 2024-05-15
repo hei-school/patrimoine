a1f69a8c65efe50e8d4ec543db2f578f9715526e
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-05-15 20:30:17 +0300

    feat: implement projection futur in Materiel

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,19 +1,28 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
-
+import java.time.Duration;
 import java.time.Instant;
 
 public final class Materiel extends Possession {
-  private final double tauxDAppreciationAnnuelle;
+    private final double tauxDAppreciationAnnuelle;
 
-  public Materiel(String nom, Instant t, int valeurComptable, double tauxDAppreciationAnnuelle) {
-    super(nom, t, valeurComptable);
-    this.tauxDAppreciationAnnuelle = tauxDAppreciationAnnuelle;
-  }
+    public Materiel(String nom, Instant t, int valeurComptable, double tauxDAppreciationAnnuelle) {
+        super(nom, t, valeurComptable);
+        this.tauxDAppreciationAnnuelle = tauxDAppreciationAnnuelle;
+    }
 
-  @Override
-  public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
-  }
+    @Override
+    public Possession projectionFuture(Instant tFutur) {
+        double nbJourFutur = Duration.between(t, tFutur).toDays() / 365;
+        double valeurComptaFutur = (tauxDAppreciationAnnuelle * valeurComptable * nbJourFutur) + valeurComptable;
+        return new Materiel(
+                nom,
+                t,
+                (int) valeurComptaFutur,
+                tauxDAppreciationAnnuelle
+        );
+    }
 }
+
+
+//
