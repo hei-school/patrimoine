273f9c03e5e3dad67d34a9275397c3e89ecf4181
Fifaliana <hei.fifaliana@gmail.com>
2024-05-15 23:52:06 +0300

    chore: implement Patrimoine::getValeurComptable

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -8,10 +8,14 @@ import java.util.Set;
 public record Patrimoine(
     Personne possesseur, Instant t, Set<Possession> possessions) {
   public int getValeurComptable() {
+    int total = 0 ;
     if (possessions.isEmpty()) {
       return 0;
     }
-    throw new NotImplemented();
+    for (Possession possession : possessions){
+      total += getValeurComptable() ;
+    }
+    return total ;
   }
 
   public Patrimoine projectionFuture(Instant tFutur) {
