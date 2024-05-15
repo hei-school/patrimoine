c8e545234895670a1e40e06d9a36360f3ea8d6c1
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-05-15 22:50:35 +0200

    chore: implement Patrimoine::getValeurComptable

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -1,17 +1,23 @@
 package school.hei.patrimoine;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import school.hei.patrimoine.possession.Possession;
 
 import java.time.Instant;
 import java.util.Set;
-
 public record Patrimoine(
     Personne possesseur, Instant t, Set<Possession> possessions) {
   public int getValeurComptable() {
+    int TotalValeurComptable = 0 ;
     if (possessions.isEmpty()) {
-      return 0;
+      TotalValeurComptable += 0;
+    }else{
+      for (Possession possession : possessions) {
+        TotalValeurComptable += possession.getValeurComptable();
+      }
     }
-    throw new NotImplemented();
+    return TotalValeurComptable;
   }
 
   public Patrimoine projectionFuture(Instant tFutur) {
