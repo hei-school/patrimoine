9a60d17264a0941173aa920f99a08fc3a62298a0
Marc Herilala <hei.marc.3@gmail.com>
2024-05-15 22:53:08 +0300

    refactor:projectionFutureMateriel

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,7 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +15,9 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    long differenceEntreTetFutureT= ChronoUnit.DAYS.between(getT(),tFutur)/365;
+    double futureValeur=valeurComptable+(valeurComptable*
+            (tauxDAppreciationAnnuelle*differenceEntreTetFutureT));
+    return new Materiel(nom,t,(int) futureValeur,tauxDAppreciationAnnuelle);
   }
 }
