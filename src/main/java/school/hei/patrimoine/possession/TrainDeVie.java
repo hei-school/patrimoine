83afb750081cc35c63e33f2e167a92918ecb64a1
herimaharavohaja <hei.haja.25@gmail.com>
2024-05-15 11:18:30 +0200

    fix: refectorate the heritage code

--- src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
+++ src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
@@ -1,10 +1,10 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
-
 import java.time.Instant;
+import java.time.ZoneId;
 
 public final class TrainDeVie extends Possession {
+
   private final Instant debut;
   private final Instant fin;
   private final int depensesMensuelle;
@@ -12,13 +12,13 @@ public final class TrainDeVie extends Possession {
   private final int dateDePonction;
 
   public TrainDeVie(
-      String nom,
-      int depensesMensuelle,
-      Instant debut,
-      Instant fin,
-      Argent financePar,
-      int dateDePonction) {
-    super(nom, null, 0); //TODO: dirty, redesign
+          String nom,
+          int depensesMensuelle,
+          Instant debut,
+          Instant fin,
+          Argent financePar,
+          int dateDePonction) {
+    super(nom, Instant.now(), depensesMensuelle * nbMoisEntre(debut, dateDePonction));
     this.debut = debut;
     this.fin = fin;
     this.depensesMensuelle = depensesMensuelle;
@@ -26,8 +26,35 @@ public final class TrainDeVie extends Possession {
     this.dateDePonction = dateDePonction;
   }
 
+  private static int nbMoisEntre(Instant debut, int dateDePonction) {
+      int moisDebut = debut.atZone(ZoneId.systemDefault()).getMonthValue();
+      int moisPonction = dateDePonction % 100;
+
+      int differenceMois = moisPonction - moisDebut;
+
+      if (differenceMois < 0) {
+        differenceMois += 12;
+      }
+
+      return differenceMois;
+    }
+
+
+
+
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    return new TrainDeVie(
+            nom,
+            depensesMensuelle,
+            debut,
+            tFutur,
+            financePar,
+            dateDePonction
+    );
+  }
+
+  public Argent financePar() {
+    return financePar;
   }
 }
