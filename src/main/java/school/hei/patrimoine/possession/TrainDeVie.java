9a6091d444ca154baca6a399dd01076cf4b24a62
Artiana <hei.rojo.3@gmail.com>
2024-05-16 01:12:49 +0300

    chore:implementation de projectionFuture dans trainDeVie

--- src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
+++ src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
@@ -14,12 +14,12 @@ public final class TrainDeVie extends Possession {
   private final int dateDePonction;
 
   public TrainDeVie(
-      String nom,
-      int depensesMensuelle,
-      Instant debut,
-      Instant fin,
-      Argent financePar,
-      int dateDePonction) {
+          String nom,
+          int depensesMensuelle,
+          Instant debut,
+          Instant fin,
+          Argent financePar,
+          int dateDePonction) {
     super(nom, null, 0); //TODO: dirty, redesign
     this.debut = debut;
     this.fin = fin;
@@ -29,7 +29,22 @@ public final class TrainDeVie extends Possession {
   }
 
   @Override
-  public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+  public TrainDeVie projectionFuture(Instant tFutur) {
+    Argent dansLeFuturFinancePar = new Argent(
+            this.financePar.getNom(),
+            tFutur,
+            this.financePar.valeurComptableFuture(tFutur)
+    );
+
+    TrainDeVie traindeVieFuture = new TrainDeVie(
+            nom,
+            depensesMensuelle,
+            tFutur,
+            fin,
+            dansLeFuturFinancePar,
+            dateDePonction
+    );
+
+    return traindeVieFuture;
   }
 }
