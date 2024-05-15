a2d13ea9c4b5888b0519e9061f65f69be9261fb1
junkaM <hei.mikanto.3@gmail.com>
2024-05-15 21:14:22 +0200

    Chore: projectionFututre implemented

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -1,7 +1,9 @@
 package school.hei.patrimoine;
 
+import school.hei.patrimoine.possession.Materiel;
 import school.hei.patrimoine.possession.Possession;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Set;
 
--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -2,6 +2,7 @@ package school.hei.patrimoine.possession;
 
 import school.hei.patrimoine.NotImplemented;
 
+import java.time.Duration;
 import java.time.Instant;
 
 public final class Materiel extends Possession {
@@ -14,6 +15,21 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+
+    int joursProjection = (int) Duration.between(this.t, tFutur).toDays();
+    double valeurComptable = this.valeurComptable;
+    double tauxDAppreciationJournalier = this.tauxDAppreciationAnnuelle / 365;
+
+    for (int j = 0; j < joursProjection; j++) {
+      valeurComptable += valeurComptable * tauxDAppreciationJournalier;
+    }
+
+    Materiel materiel = new Materiel(
+            this.nom,
+            tFutur,
+            (int) valeurComptable,
+            this.tauxDAppreciationAnnuelle
+    );
+    return materiel;
   }
 }
