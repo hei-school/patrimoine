68effaa43eb9aceb25bd03f6d7d9f9d1c6b7d105
RianAina <128467312+RianAina@users.noreply.github.com>
2024-05-15 21:30:03 +0300

    Chore : projectionFuture implemented

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -3,19 +3,29 @@ package school.hei.patrimoine;
 import school.hei.patrimoine.possession.Possession;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Set;
 
 public record Patrimoine(
     Personne possesseur, Instant t, Set<Possession> possessions) {
   public int getValeurComptable() {
-    int totalValeurComptable = 0;
-    for (Possession possession : possessions) {
-      totalValeurComptable += possession.getValeurComptable();
+      
+    if (possessions.isEmpty()) {
+      return 0;
+        
     }
-    return totalValeurComptable;
+      
+    throw new NotImplemented();
   }
 
   public Patrimoine projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    Set<Possession> possessionsFuture = new HashSet<>();
+    possessions.forEach(possession -> {
+        
+        possessionsFuture.add(
+                possession.projectionFuture(tFutur)
+            
+        );
+    });
+    return new Patrimoine(possesseur, tFutur, possessions);
   }
-}
