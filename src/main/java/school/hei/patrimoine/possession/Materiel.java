a9f0414219bbde85bf5e81b03c521a69570d84b5
Lalaina0904 <lalainap19.aps2b@gmail.com>
2024-05-15 23:09:31 +0300

    chore: implement Materiel::projectionFuture

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,6 +1,6 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
+import java.time.Duration;
 
 import java.time.Instant;
 
@@ -14,6 +14,14 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    long nombreDeJourJusquaTFutur = Duration.between(this.t, tFutur).toDays();
+    double tauxDAppreciationJournalier = this.tauxDAppreciationAnnuelle / 365;
+    double valeur = this.valeurComptable;
+    for (int jour = 0; jour < nombreDeJourJusquaTFutur; jour++) {
+      valeur += valeur * tauxDAppreciationJournalier;
+    }
+
+    return new Materiel(this.nom, tFutur, (int) valeur, this.tauxDAppreciationAnnuelle);
+
   }
 }
