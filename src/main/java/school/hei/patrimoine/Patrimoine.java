c1897d177e46748500f770bc51df3b12caed20c2
Sun-2302 <hei.dera.932@gmail.com>
2024-05-15 21:32:47 +0300

    chore: implement Patrimoine::projectionFuture

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -4,17 +4,24 @@ import school.hei.patrimoine.possession.Possession;
 
 import java.time.Instant;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public record Patrimoine(
-    Personne possesseur, Instant t, Set<Possession> possessions) {
-  public int getValeurComptable() {
-    if (possessions.isEmpty()) {
-      return 0;
+        Personne possesseur, Instant t, Set<Possession> possessions) {
+    public int getValeurComptable() {
+        if (possessions.isEmpty()) {
+            return 0;
+        }
+        return possessions.stream()
+                .mapToInt(Possession::getValeurComptable)
+                .sum();
     }
-    throw new NotImplemented();
-  }
 
-  public Patrimoine projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
-  }
+    public Patrimoine projectionFuture(Instant tFutur) {
+        Set<Possession> possessionsFuture = this.possessions.stream()
+                .map(possession -> possession.projectionFuture(tFutur))
+                .collect(Collectors.toSet());
+
+        return new Patrimoine(this.possesseur, tFutur, possessionsFuture);
+    }
 }
