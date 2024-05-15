f8e24bb293ff1f211607f17ab571294d99105eff
Harizo ANDRIANAIVO <124563616+Eclisher@users.noreply.github.com>
2024-05-15 23:45:59 +0300

    feat: codding notimplemented in ProjectionFuture

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,8 +1,9 @@
 package school.hei.patrimoine.possession;
 
 import school.hei.patrimoine.NotImplemented;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 
-import java.time.Instant;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +15,15 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    ZonedDateTime StartIngDate = this.t.atZone(ZoneId.systemDefault());
+    ZonedDateTime EndingDate = tFutur.atZone(ZoneId.systemDefault());
+    int nombreDeMois = (int) ChronoUnit.MONTHS.between(StartIngDate, EndingDate);
+
+    double MensualFactor = 1 + (tauxDAppreciationAnnuelle / 12);
+    double valeurFuture = getValeurComptable() * Math.pow(MensualFactor, nombreDeMois);
+    int valeurFinal = (int) Math.round(valeurFuture);
+
+    return new Materiel(this.nom, tFutur, valeurFinal, this.tauxDAppreciationAnnuelle);
+  }
   }
 }
