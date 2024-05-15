1b100ed4e40032c1a3547a744ce83bdffd88b35e
= <=>
2024-05-16 00:04:07 +0300

    feat: impliment getValeurComptable  method in patrimoine class

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -11,10 +11,10 @@ public record Patrimoine(
     if (possessions.isEmpty()) {
       return 0;
     }
-    throw new NotImplemented();
-  }
-
-  public Patrimoine projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
-  }
+    int valeur = 0;
+    for (Possession possession : possessions){
+      valeur += possession.getValeurComptable();
+    }
+    return valeur;
 }
+    }
