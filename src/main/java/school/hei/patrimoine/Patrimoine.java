39272143c1699850a89bbb97ef1a89497daf8ad1
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-05-15 19:40:35 +0300

    chore: refactor sum of possessions

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -6,15 +6,15 @@ import java.time.Instant;
 import java.util.Set;
 
 public record Patrimoine(
-    Personne possesseur, Instant t, Set<Possession> possessions) {
-  public int getValeurComptable() {
-    if (possessions.isEmpty()) {
-      return 0;
+        Personne possesseur, Instant t, Set<Possession> possessions) {
+    public int getValeurComptable() {
+        if (possessions.isEmpty()) {
+            return 0;
+        }
+            return possessions.stream().mapToInt(Possession::getValeurComptable).sum();
     }
-    throw new NotImplemented();
-  }
 
-  public Patrimoine projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
-  }
+    public Patrimoine projectionFuture(Instant tFutur) {
+        throw new NotImplemented();
+    }
 }
