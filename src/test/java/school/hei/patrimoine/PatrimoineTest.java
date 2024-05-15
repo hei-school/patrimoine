75e331f0e1efcc63c0e3895819accc7f3c8ffa83
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-05-15 16:13:31 +0300

    feat: ajouter le testValeurComptableFutureMateriel

--- src/test/java/school/hei/patrimoine/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/PatrimoineTest.java
@@ -2,8 +2,10 @@ package school.hei.patrimoine;
 
 import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.possession.Argent;
+import school.hei.patrimoine.possession.Materiel;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -37,4 +39,19 @@ class PatrimoineTest {
 
     assertEquals(1_200_000, patrimoineIloAu13mai24.getValeurComptable());
   }
+  @Test
+  void testValeurComptableFutureMateriel() {
+    var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
+    var au13mai29 = Instant.parse("2029-05-13T00:00:00.00Z");
+
+    var materiel = new Materiel("ordinateur", au13mai24, 400_000, 0.1);
+
+    long joursEntre = ChronoUnit.DAYS.between(au13mai24, au13mai29);
+    double tauxAmortissementQuotidien = Math.pow(1 - 0.1, 1.0 / 365);
+    double valeurAttendueMateriel = Math.round(400_000 * Math.pow(tauxAmortissementQuotidien, joursEntre));
+
+    double valeurComptableFutureMateriel = materiel.valeurComptableFuture(au13mai29);
+    assertEquals(valeurAttendueMateriel, valeurComptableFutureMateriel, 0.01);
+  }
+
 }
