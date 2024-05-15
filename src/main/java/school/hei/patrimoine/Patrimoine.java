9e5b759fd2b2d57f56448d8461221138e60c28c3
Tendry24 <hei.tendry.2@gmail.com>
2024-05-15 22:18:22 +0300

    refactor: projectionFuture implemented for all subclasses

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -8,10 +8,14 @@ import java.util.Set;
 public record Patrimoine(
     Personne possesseur, Instant t, Set<Possession> possessions) {
   public int getValeurComptable() {
+    int totalValeurComptable = 0;
     if (possessions.isEmpty()) {
       return 0;
     }
-    throw new NotImplemented();
+    for (Possession possession : possessions) {
+      totalValeurComptable += possession.getValeurComptable();
+    }
+    return totalValeurComptable;
   }
 
   public Patrimoine projectionFuture(Instant tFutur) {
