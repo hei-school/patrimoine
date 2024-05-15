ffdf6123e376ecd605f1499c8a0dfcc8fdb39ca5
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-05-15 22:04:42 +0300

    feat: implements methode projectionFuture in Materiel

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -14,7 +14,11 @@ public record Patrimoine(
     if (possessions.isEmpty()) {
       return 0;
     }
-    throw new NotImplemented();
+    int sommeDeToutesLesValeursComptables = 0;
+    for (Possession possession : possessions) {
+      sommeDeToutesLesValeursComptables += possession.getValeurComptable();
+    }
+    return sommeDeToutesLesValeursComptables;
   }
 
   public Patrimoine projectionFuture(Instant tFutur) {
--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,7 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +15,16 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    double nombresDeJoursEcouleesDepuisLAchat = TimeUnit.DAYS.toDays((tFutur.toEpochMilli()) - getT().toEpochMilli());
+    double nombresDAnneesEcoulesDepuisLAchat = nombresDeJoursEcouleesDepuisLAchat / 365;
+
+    double nouvelleValeurComptable = getValeurComptable()* Math.pow(tauxDAppreciationAnnuelle+1, nombresDAnneesEcoulesDepuisLAchat);
+
+    return new Materiel(
+            getNom(),
+            tFutur,
+            (int) nouvelleValeurComptable,
+            tauxDAppreciationAnnuelle
+    );
   }
 }
