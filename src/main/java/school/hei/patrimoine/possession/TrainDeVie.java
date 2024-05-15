909d5e67c8819a238f571bcc41acc66668661465
Maharavo1 <hei.maharavo@gmail.com>
2024-05-16 00:24:32 +0300

    chore: implement TrainDeVie::projectionFuture

--- src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
+++ src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
@@ -3,6 +3,7 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public final class TrainDeVie extends Possession {
   private final Instant debut;
@@ -12,12 +13,12 @@ public final class TrainDeVie extends Possession {
   private final int dateDePonction;
 
   public TrainDeVie(
-      String nom,
-      int depensesMensuelle,
-      Instant debut,
-      Instant fin,
-      Argent financePar,
-      int dateDePonction) {
+          String nom,
+          int depensesMensuelle,
+          Instant debut,
+          Instant fin,
+          Argent financePar,
+          int dateDePonction) {
     super(nom, null, 0); //TODO: dirty, redesign
     this.debut = debut;
     this.fin = fin;
@@ -28,6 +29,21 @@ public final class TrainDeVie extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    long differenceEnMois = ChronoUnit.MONTHS.between(debut, fin);
+    int depensesTotales = depensesMensuelle * (int) differenceEnMois;
+    int valeurComptableFutur = (int) (valeurComptable - depensesTotales);
+
+    return new TrainDeVie(
+            nom,
+            depensesMensuelle,
+            debut,
+            fin,
+            new Argent(
+                    this.nom,
+                    tFutur,
+                    valeurComptableFutur
+            ),
+            dateDePonction
+    );
   }
 }
