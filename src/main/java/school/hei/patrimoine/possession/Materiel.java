1ba7f737260c7c81fbccb56d366eff7d2c6676b6
rado <hei.rado.2@gmail.com>
2024-05-15 22:42:39 +0300

    feat: implement projectFuture

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,7 +1,5 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
-
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -17,6 +15,15 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    ZonedDateTime zdt1 = this.t.atZone(ZoneId.systemDefault());
+    ZonedDateTime zdt2 = tFutur.atZone(ZoneId.systemDefault());
+    long nombreDeJours = ChronoUnit.DAYS.between(zdt1, zdt2);
+    double valeurActuelle = getValeurComptable();
+    double tauxJournalier = tauxDAppreciationAnnuelle / 365.0;
+
+    for (int jour = 1; jour <= nombreDeJours; jour++) {
+      valeurActuelle *= (1 + tauxJournalier);
+    }
+    return new Materiel(getNom(), tFutur, (int) valeurActuelle, tauxDAppreciationAnnuelle);
   }
 }
