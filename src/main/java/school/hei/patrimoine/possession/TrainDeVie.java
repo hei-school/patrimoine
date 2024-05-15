804193564ac532c723e8dad1d251091c9762d4d0
Daris02 <hei.raymond.2@gmail.com>
2024-05-15 22:25:21 +0300

    feat: complete implement projectionFuture

--- src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
+++ src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
@@ -1,8 +1,8 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
-
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 public final class TrainDeVie extends Possession {
   private final Instant debut;
@@ -12,13 +12,13 @@ public final class TrainDeVie extends Possession {
   private final int dateDePonction;
 
   public TrainDeVie(
-      String nom,
-      int depensesMensuelle,
-      Instant debut,
-      Instant fin,
-      Argent financePar,
-      int dateDePonction) {
-    super(nom, null, 0); //TODO: dirty, redesign
+          String nom,
+          int depensesMensuelle,
+          Instant debut,
+          Instant fin,
+          Argent financePar,
+          int dateDePonction) {
+    super(nom, Instant.now(), financePar.getValeurComptable());
     this.debut = debut;
     this.fin = fin;
     this.depensesMensuelle = depensesMensuelle;
@@ -28,6 +28,18 @@ public final class TrainDeVie extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    int restant = financePar.getValeurComptable();
+    if ( tFutur.isAfter(debut) &&
+            (financePar.getT().isAfter(debut) || financePar.getT().equals(debut)) &&
+            (financePar.getT().isBefore(fin) || financePar.getT().equals(fin)) ) {
+      LocalDate debutToLocalDate = LocalDate.ofInstant(debut, ZoneId.of("UTC"));
+      LocalDate tFuturToLocalDate = LocalDate.ofInstant(tFutur, ZoneId.of("UTC"));
+      while (debutToLocalDate.isBefore(tFuturToLocalDate)) {
+        if (debutToLocalDate.getDayOfMonth() == dateDePonction) restant -= depensesMensuelle;
+        debutToLocalDate = debutToLocalDate.plusDays(1);
+      }
+    }
+    Argent newFinancePar = new Argent(financePar.nom, tFutur, restant);
+    return new TrainDeVie( nom, depensesMensuelle, debut, fin, newFinancePar, dateDePonction);
   }
 }
