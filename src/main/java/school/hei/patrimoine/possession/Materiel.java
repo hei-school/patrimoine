a47855902f158b4728c76160d381b13603cf40d4
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-05-15 16:07:31 +0300

    feat: implementer methode projectionFuture .

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,7 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +15,14 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    if (tFutur.isBefore(this.t)) {
+      return new Materiel(this.nom, this.t, this.valeurComptable, this.tauxDAppreciationAnnuelle);
+    }
+    int joursEntre = (int) ChronoUnit.DAYS.between(this.t, tFutur);
+    double tauxAmortissementQuotidien = Math.pow(1 - this.tauxDAppreciationAnnuelle, 1.0 / 365);
+
+    double valeurFuture = this.valeurComptable * Math.pow(tauxAmortissementQuotidien, joursEntre);
+    return new Materiel( this.nom,this.t, (int) Math.round(valeurFuture),this.tauxDAppreciationAnnuelle);
   }
-}
+  }
+
