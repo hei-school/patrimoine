00d880e92338f16e28bac0ffee1090334e9d2bba
Lou Andria <lou@hei.school>
2024-05-15 09:28:24 +0300

    wip: test: train_de_vie_est_finance_par_compte_courant

--- src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
+++ src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
@@ -5,8 +5,25 @@ import school.hei.patrimoine.NotImplemented;
 import java.time.Instant;
 
 public final class TrainDeVie extends Possession {
-  public TrainDeVie(String nom, Instant t, int valeurComptable) {
-    super(nom, t, valeurComptable);
+  private final Instant debut;
+  private final Instant fin;
+  private final int depensesMensuelle;
+  private final Argent financePar;
+  private final int dateDePonction;
+
+  public TrainDeVie(
+      String nom,
+      int depensesMensuelle,
+      Instant debut,
+      Instant fin,
+      Argent financePar,
+      int dateDePonction) {
+    super(nom, null, 0); //TODO: dirty, redesign
+    this.debut = debut;
+    this.fin = fin;
+    this.depensesMensuelle = depensesMensuelle;
+    this.financePar = financePar;
+    this.dateDePonction = dateDePonction;
   }
 
   @Override
