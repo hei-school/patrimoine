2f65e427123293072d558fa1dccbaefca60ea010
Sun-2302 <hei.dera.932@gmail.com>
2024-05-15 21:21:59 +0300

    feat: implement TrainDeVie::projectionFuture

--- src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
+++ src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
@@ -1,33 +1,58 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
+import lombok.Getter;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 
+@Getter
 public final class TrainDeVie extends Possession {
-  private final Instant debut;
-  private final Instant fin;
-  private final int depensesMensuelle;
-  private final Argent financePar;
-  private final int dateDePonction;
+    private final Instant debut;
+    private final Instant fin;
+    private final int depensesMensuelle;
+    private final Argent financePar;
+    private final int dateDePonction;
 
-  public TrainDeVie(
-      String nom,
-      int depensesMensuelle,
-      Instant debut,
-      Instant fin,
-      Argent financePar,
-      int dateDePonction) {
-    super(nom, null, 0); //TODO: dirty, redesign
-    this.debut = debut;
-    this.fin = fin;
-    this.depensesMensuelle = depensesMensuelle;
-    this.financePar = financePar;
-    this.dateDePonction = dateDePonction;
-  }
+    public TrainDeVie(
+            String nom,
+            int depensesMensuelle,
+            Instant debut,
+            Instant fin,
+            Argent financePar,
+            int dateDePonction) {
+        super(nom, debut, financePar.getValeurComptable());
+        this.debut = debut;
+        this.fin = fin;
+        this.depensesMensuelle = depensesMensuelle;
+        this.financePar = financePar;
+        this.dateDePonction = dateDePonction;
+    }
 
-  @Override
-  public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
-  }
+    @Override
+    public Possession projectionFuture(Instant tFutur) {
+        LocalDate debutEnLocalDate = this.debut.atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate tFuturEnLocalDate = tFutur.atZone(ZoneId.systemDefault()).toLocalDate();
+        int nombreDePonction = Period.between(debutEnLocalDate, tFuturEnLocalDate).getMonths();
+
+        if (this.dateDePonction <= tFuturEnLocalDate.getDayOfMonth() &&
+                tFuturEnLocalDate.getDayOfMonth() < debutEnLocalDate.getDayOfMonth()) {
+            nombreDePonction += 1;
+        }
+
+        int valeurComptableFuture = this.financePar.getValeurComptable()-(this.depensesMensuelle * nombreDePonction);
+        Argent financeParFutur = new Argent(
+                this.financePar.getNom(),
+                this.financePar.getT(),
+                valeurComptableFuture
+        );
+        return new TrainDeVie(
+                this.nom,
+                this.depensesMensuelle,
+                this.debut,
+                this.fin,
+                financeParFutur,
+                this.dateDePonction);
+    }
 }
