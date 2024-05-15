7ffa5b63eee9e0610d6d628c6b1a61832b195ef4
Tombosoa <hei.nantenaina.4@gmail.com>
2024-05-15 21:52:35 +0300

    chore: implement projectionFuture

--- src/main/java/school/hei/patrimoine/possession/Argent.java
+++ src/main/java/school/hei/patrimoine/possession/Argent.java
@@ -3,6 +3,8 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public final class Argent extends Possession {
   public Argent(String nom, Instant t, int valeurComptable) {
@@ -11,6 +13,8 @@ public final class Argent extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    ZoneId zoneId = ZoneId.systemDefault();
+    double anneePassee = (double) ChronoUnit.DAYS.between(t.atZone(zoneId), tFutur.atZone(zoneId)) / 365;
+    return new Argent(nom, tFutur, (int) (valeurComptable * anneePassee));
   }
 }
--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,8 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +16,9 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    ZoneId zoneId = ZoneId.systemDefault();
+    double anneePassee = (double) ChronoUnit.DAYS.between(t.atZone(zoneId), tFutur.atZone(zoneId)) / 365;
+    double valeurFuture = valeurComptable + (valeurComptable * tauxDAppreciationAnnuelle * anneePassee);
+    return new Materiel(nom, tFutur, (int) valeurFuture, tauxDAppreciationAnnuelle);
   }
 }
