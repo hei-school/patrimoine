5eeebe9620efe8eab5dd93a7ad564223a266ee92
tokiniaina <hei.tokiniaina@gmail.com>
2024-05-15 23:57:49 +0300

    feat: implement projectionFuture at Argent

--- src/main/java/school/hei/patrimoine/possession/Argent.java
+++ src/main/java/school/hei/patrimoine/possession/Argent.java
@@ -3,6 +3,8 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 public final class Argent extends Possession {
   public Argent(String nom, Instant t, int valeurComptable) {
@@ -11,6 +13,12 @@ public final class Argent extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    Instant instant = Instant.now();
+    LocalDate datePresent = LocalDate.ofInstant(instant, ZoneId.systemDefault());
+    LocalDate future= LocalDate.ofInstant(tFutur,ZoneId.systemDefault());
+    int annee = future.getYear() - datePresent.getYear();
+    int valeurFuture= (int) (valeurComptable*annee);
+    return  new Argent(nom,t,valeurFuture);
+  }
   }
 }
