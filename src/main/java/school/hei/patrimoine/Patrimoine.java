3fdef173e9163f39add8ea009ec60ef6eb2c144a
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-05-15 22:50:57 +0300

    feat: implement all projectionFuture

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -3,18 +3,27 @@ package school.hei.patrimoine;
 import school.hei.patrimoine.possession.Possession;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Set;
 
 public record Patrimoine(
-    Personne possesseur, Instant t, Set<Possession> possessions) {
-  public int getValeurComptable() {
-    if (possessions.isEmpty()) {
-      return 0;
+        Personne possesseur, Instant t, Set<Possession> possessions) {
+    public int getValeurComptable() {
+        if (possessions.isEmpty()) {
+            return 0;
+        }
+        return possessions
+                .stream()
+                .mapToInt(Possession::getValeurComptable)
+                .sum();
     }
-    throw new NotImplemented();
-  }
 
-  public Patrimoine projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
-  }
+    public Patrimoine projectionFuture(Instant tFutur) {
+        Set<Possession> possessionsProjeteesDansLeFutur = new HashSet<>();
+        for (Possession possession : possessions) {
+            Possession possessionFutur = possession.projectionFuture(tFutur);
+            possessionsProjeteesDansLeFutur.add(possessionFutur);
+        }
+        return new Patrimoine(this.possesseur, tFutur, possessionsProjeteesDansLeFutur);
+    }
 }
