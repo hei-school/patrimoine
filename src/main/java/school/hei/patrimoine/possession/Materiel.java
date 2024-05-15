0c78ac6823465a15575e5478dc5920df1c571fd6
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-05-15 20:34:16 +0300

    Refactor: Implement future projection calculation method in Materiel class

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,7 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +15,11 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    long joursPasses = TimeUnit.MILLISECONDS.toDays(tFutur.toEpochMilli() - getT().toEpochMilli());
+    double anneesPassees = joursPasses / 365.0;
+
+    double nouvelleValeurComptable = getValeurComptable() * Math.pow(1 + tauxDAppreciationAnnuelle, anneesPassees);
+
+    return new Materiel(getNom(), tFutur, (int) nouvelleValeurComptable, tauxDAppreciationAnnuelle);
   }
 }
