3f2d02499c1e746ff2a79d415748c3a4ae8972a3
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2024-05-16 04:22:26 +0300

    refactor: implement the method projectionFuture in the class Materiel

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,5 +1,6 @@
 package school.hei.patrimoine.possession;
 
+import java.time.Duration;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
@@ -14,6 +15,9 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    long secondes = Duration.between(this.t, tFutur).getSeconds();
+    double annees = secondes / (365.25 * 24 * 3600);
+    double nouvelleValeur = this.valeurComptable * Math.pow(1 + tauxDAppreciationAnnuelle, annees);
+    return new Materiel(this.nom, tFutur, (int) nouvelleValeur, this.tauxDAppreciationAnnuelle);
   }
 }
