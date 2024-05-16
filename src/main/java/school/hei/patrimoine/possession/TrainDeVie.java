699c685f5fadb93d42cf49482569d39a8ebb2d5a
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-05-16 21:59:04 +0300

    Fix: Resolve conflict

--- src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
+++ src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
@@ -1,12 +1,9 @@
 package school.hei.patrimoine.possession;
 
-import lombok.Getter;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 
-@Getter
 public final class TrainDeVie extends Possession {
   private final Instant debut;
   private final Instant fin;
@@ -21,26 +18,18 @@ public final class TrainDeVie extends Possession {
       Instant fin,
       Argent financePar,
       int dateDePonction) {
-    super(nom, null, 0); 
+    super(nom, null, 0);
     this.debut = debut;
     this.fin = fin;
     this.depensesMensuelle = depensesMensuelle;
-    this.financePar = financePar;
     this.dateDePonction = dateDePonction;
+
+    this.financePar = financePar;
+    this.financePar.addFinancés(this);
   }
 
   @Override
-  public Possession projectionFuture(Instant tFutur) {
-    long moisEntreDates = ChronoUnit.MONTHS.between(debut, fin);
-    int totalDepenses = depensesMensuelle * (int) moisEntreDates;
-
-    return new TrainDeVie(
-            getNom(),
-            totalDepenses,
-            debut,
-            tFutur,
-            financePar,
-            dateDePonction
-    );
+  public TrainDeVie projectionFuture(Instant tFutur) {
+    throw new NotImplemented();
   }
 }
