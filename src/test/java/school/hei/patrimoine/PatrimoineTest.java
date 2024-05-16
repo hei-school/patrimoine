d49096e032ef31ab10bd40a30c6a8ccd6a3ed97d
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-05-16 23:33:51 +0300

    test: assertion in patrimoine_possede_un_train_de_vie_financé_par_argent()

--- src/test/java/school/hei/patrimoine/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/PatrimoineTest.java
@@ -8,49 +8,54 @@ import java.time.Instant;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PatrimoineTest {
 
-  @Test
-  void patrimoine_vide_vaut_0() {
-    var ilo = new Personne("Ilo");
+    @Test
+    void patrimoine_vide_vaut_0() {
+        var ilo = new Personne("Ilo");
 
-    var patrimoineIloAu13mai24 = new Patrimoine(
-        ilo,
-        Instant.parse("2024-05-13T00:00:00.00Z"),
-        Set.of());
+        var patrimoineIloAu13mai24 = new Patrimoine(
+                ilo,
+                Instant.parse("2024-05-13T00:00:00.00Z"),
+                Set.of());
 
-    assertEquals(0, patrimoineIloAu13mai24.getValeurComptable());
-  }
+        assertEquals(0, patrimoineIloAu13mai24.getValeurComptable());
+    }
 
-  @Test
-  void patrimoine_a_de_l_argent() {
-    var ilo = new Personne("Ilo");
+    @Test
+    void patrimoine_a_de_l_argent() {
+        var ilo = new Personne("Ilo");
 
-    var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
-    var patrimoineIloAu13mai24 = new Patrimoine(
-        ilo,
-        au13mai24,
-        Set.of(
-            new Argent("Espèces", au13mai24, 400_000),
-            new Argent("Compte epargne", au13mai24, 200_000),
-            new Argent("Compte courant", au13mai24, 600_000)));
+        var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
+        var patrimoineIloAu13mai24 = new Patrimoine(
+                ilo,
+                au13mai24,
+                Set.of(
+                        new Argent("Espèces", au13mai24, 400_000),
+                        new Argent("Compte epargne", au13mai24, 200_000),
+                        new Argent("Compte courant", au13mai24, 600_000)));
 
-    assertEquals(1_200_000, patrimoineIloAu13mai24.getValeurComptable());
-  }
+        assertEquals(1_200_000, patrimoineIloAu13mai24.getValeurComptable());
+    }
 
-  @Test
-  void patrimoine_possede_un_train_de_vie_financé_par_argent() {
-    var ilo = new Personne("Ilo");
+    @Test
+    void patrimoine_possede_un_train_de_vie_financé_par_argent() {
+        var ilo = new Personne("Ilo");
 
-    var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
-    var financeur = new Argent("Espèces", au13mai24, 400_000);
+        var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
+        var financeur = new Argent("Espèces", au13mai24, 400_000);
 
-    var trainDeVie = new TrainDeVie(null, 0, null, null, financeur, 0);
+        var trainDeVie = new TrainDeVie(null, 0, null, null, financeur, 0);
 
-    var patrimoineIloAu13mai24 = new Patrimoine(
-        ilo,
-        au13mai24,
-        Set.of(financeur, trainDeVie));
-  }
+        var patrimoineIloAu13mai24 = new Patrimoine(
+                ilo,
+                au13mai24,
+                Set.of(financeur, trainDeVie));
+
+        assertTrue(patrimoineIloAu13mai24.possessions().contains(financeur));
+    }
+
+
 }
