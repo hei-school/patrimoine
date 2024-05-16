4f787889db18c4e7fe54525acd9c2c79472b6e83
nirinaHelena <hei.nirina.7@gmail.com>
2024-05-16 20:41:07 +0300

    test: patrimoine de Ilo le 26juin24 et le 14juillet24

---
+++ src/test/java/school/hei/patrimoine/possession/ArgentTest.java
@@ -0,0 +1,12 @@
+package school.hei.patrimoine.possession;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgentTest {
+
+    @Test
+    void projectionFuture() {
+    }
+}
