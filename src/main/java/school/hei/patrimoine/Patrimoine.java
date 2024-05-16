05b638980b51341ad01eaf58e41de50961c1f0a5
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-05-16 07:19:46 +0300

    feat: run patrimoine test

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -10,8 +10,11 @@ public record Patrimoine(
   public int getValeurComptable() {
     if (possessions.isEmpty()) {
       return 0;
+    }else {
+      return possessions.stream()
+              .mapToInt(Possession::getValeurComptable)
+              .sum();
     }
-    throw new NotImplemented();
   }
 
   public Patrimoine projectionFuture(Instant tFutur) {
