e1331db56514a1f9dc82622de4ca54feca884ecf
rado <hei.rado.2@gmail.com>
2024-05-16 21:15:24 +0300

    test: add test train de vie

--- src/test/java/school/hei/patrimoine/possession/TrainDeVieTest.java
+++ src/test/java/school/hei/patrimoine/possession/TrainDeVieTest.java
@@ -1,9 +1,12 @@
 package school.hei.patrimoine.possession;
 
+
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
-import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 class TrainDeVieTest {
   @Test
@@ -27,7 +30,8 @@ class TrainDeVieTest {
   void un_train_de_vie_financé_par_argent() {
     var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
     var financeur = new Argent("Espèces", au13mai24, 400_000);
+    var depense = 10_000;
 
-    var trainDeVie = new TrainDeVie(null, 0, null, null, financeur, 0);
+    assertFalse(financeur.valeurComptable<depense );
   }
 }
