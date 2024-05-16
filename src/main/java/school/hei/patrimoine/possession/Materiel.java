5d6e7cfcc83cfd00d2461ada150d9afe07bd4b80
Abi-Nf <hei.abigail.3@gmail.com>
2024-05-16 15:25:58 +0300

    chore: implement Materiel::projectionFuture

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,8 +1,8 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
-
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -13,7 +13,10 @@ public final class Materiel extends Possession {
   }
 
   @Override
-  public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+  public Materiel projectionFuture(Instant tFutur) {
+    ZoneId zoneId = ZoneId.systemDefault();
+    int nombreDAnnnee = (int) ChronoUnit.YEARS.between(t.atZone(zoneId), tFutur.atZone(zoneId));
+    double valeurComptableFutur = valeurComptable + (valeurComptable * (nombreDAnnnee * tauxDAppreciationAnnuelle));
+    return new Materiel(nom, tFutur, (int) valeurComptableFutur, tauxDAppreciationAnnuelle);
   }
 }
