db1dcc45530a1db15018fa11be3f4374c13e3e76
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-05-16 19:57:13 +0300

    feat: implement projection futur in Materiel

--- src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
+++ src/main/java/school/hei/patrimoine/possession/TrainDeVie.java
@@ -1,13 +1,19 @@
 package school.hei.patrimoine.possession;
 
+import lombok.Getter;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public final class TrainDeVie extends Possession {
   private final Instant debut;
   private final Instant fin;
+  @Getter
   private final int depensesMensuelle;
+  @Getter
   private final Argent financePar;
   private final int dateDePonction;
 
@@ -30,6 +36,24 @@ public final class TrainDeVie extends Possession {
 
   @Override
   public TrainDeVie projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    boolean tFuturEstApresFin = tFutur.isAfter(fin);
+    LocalDate debutPonctionnement = LocalDate.ofInstant(debut, ZoneId.systemDefault());
+    LocalDate finPonctionnement = LocalDate.ofInstant(tFuturEstApresFin ? fin : tFutur, ZoneId.systemDefault());
+    long nombreMoisEntreTetTFutur =  ChronoUnit.MONTHS.between(
+            debutPonctionnement.withDayOfMonth(1),
+            finPonctionnement.withDayOfMonth(1)
+    );
+    if(finPonctionnement.getDayOfMonth() < dateDePonction)
+      nombreMoisEntreTetTFutur--;
+
+    int totalDepense = depensesMensuelle * (int) nombreMoisEntreTetTFutur;
+    return new TrainDeVie(
+            nom,
+            tFuturEstApresFin ? 0 : depensesMensuelle,
+            debut,
+            fin,
+            new Argent(financePar.nom, tFutur, financePar.valeurComptable - totalDepense),
+            dateDePonction
+    );
   }
 }
