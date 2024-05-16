ddc015651707046da9c4d9e19d3b4481bf52e343
HRojotiana <hei.rojotiana@gmail.com>
2024-05-16 21:15:33 +0300

    chore: implement Materiel::projectionFuture

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,7 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -13,7 +14,10 @@ public final class Materiel extends Possession {
   }
 
   @Override
-  public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+  public Materiel projectionFuture(Instant tFutur) {
+    float nombreDeJoursEntreTEtTFuturEnAnnee = ChronoUnit.DAYS.between(t, tFutur) / 365;
+    int valeurComptableFutur = (int) (valeurComptable + (valeurComptable * nombreDeJoursEntreTEtTFuturEnAnnee * tauxDAppreciationAnnuelle));
+    Materiel materielFutur = new Materiel(nom, tFutur, valeurComptableFutur, tauxDAppreciationAnnuelle);
+    return materielFutur;
   }
 }
