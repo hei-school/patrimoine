f0626c502dc142e0a761382fd9bc4ba47aa3be59
ManoaHEI <hei.manoa.4@gmail.com>
2024-05-16 22:17:32 +0400

    chore: Materiel.projectionFuture 'lissage année'

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,8 +1,7 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
-
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +13,18 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    int nombreDeJoursDansUneAnnée = 365;
+    int nombreDeJoursDepuisAcquisition = (int) ChronoUnit.DAYS.between(t, tFutur);
+    int valeurComptableFuture = valeurComptable;
+    while (nombreDeJoursDepuisAcquisition >= nombreDeJoursDansUneAnnée) {
+      valeurComptableFuture = (int) (valeurComptableFuture - (valeurComptableFuture * tauxDAppreciationAnnuelle));
+      nombreDeJoursDepuisAcquisition -= nombreDeJoursDansUneAnnée;
+    }
+
+    if (nombreDeJoursDepuisAcquisition != 0) {
+      valeurComptableFuture = (int) (valeurComptableFuture - (valeurComptableFuture * (tauxDAppreciationAnnuelle / (nombreDeJoursDansUneAnnée / nombreDeJoursDepuisAcquisition))));
+    }
+
+    return new Materiel(nom, tFutur, valeurComptableFuture, tauxDAppreciationAnnuelle);
   }
 }
