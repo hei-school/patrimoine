d2fbbbf9735d8ebe4f3a0ff9e8e907164cbaff0e
ToavinaMaria <ToavinaMaria>
2024-05-17 02:46:22 +0300

    chore: remit final method valeurComptableFuture

--- src/main/java/school/hei/patrimoine/possession/Possession.java
+++ src/main/java/school/hei/patrimoine/possession/Possession.java
@@ -14,7 +14,7 @@ public sealed abstract class Possession permits
   protected final Instant t;
   protected final int valeurComptable;
 
-  public int valeurComptableFuture(Instant tFutur) {
+  public final int valeurComptableFuture(Instant tFutur) {
     return projectionFuture(tFutur).getValeurComptable();
   }
 
