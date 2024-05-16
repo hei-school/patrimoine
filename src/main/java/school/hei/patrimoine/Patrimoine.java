49e0a70d695e6ccd6e2bf0b22f7ac9184891047b
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-05-16 21:17:42 +0300

    chore: refactor code

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -4,23 +4,24 @@ import school.hei.patrimoine.possession.Possession;
 
 import java.time.Instant;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toSet;
 
 public record Patrimoine(
-    Personne possesseur, Instant t, Set<Possession> possessions) {
-  public int getValeurComptable() {
-    if (possessions.isEmpty()) {
-      return 0;
+        Personne possesseur, Instant t, Set<Possession> possessions) {
+    public int getValeurComptable() {
+        if (possessions.isEmpty()) {
+            return 0;
+        }
+        throw new NotImplemented();
     }
-    throw new NotImplemented();
-  }
 
-  public Patrimoine projectionFuture(Instant tFutur) {
-    return new Patrimoine(
-        possesseur,
-        tFutur,
-        possessions.stream().map(p -> p.projectionFuture(tFutur)).collect(toSet()));
-  }
+    public Patrimoine projectionFuture(Instant tFutur) {
+        return new Patrimoine(
+                possesseur,
+                tFutur,
+                possessions.stream()
+                        .map(p -> p.projectionFuture(tFutur))
+                        .collect(toSet()));
+    }
 }
