3b1012ec1b8a132d452c85a56de8b20dab7b64aa
Lalaina0904 <lalainap19.aps2b@gmail.com>
2024-05-16 22:30:16 +0300

    feat: assert smt usefull TrainDeVieTest

--- src/test/java/school/hei/patrimoine/possession/TrainDeVieTest.java
+++ src/test/java/school/hei/patrimoine/possession/TrainDeVieTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.time.Instant;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class TrainDeVieTest {
   @Test
   void train_de_vie_est_finance_par_compte_courant() {
@@ -20,7 +22,8 @@ class TrainDeVieTest {
         aLaDiplomation,
         compteCourant,
         1);
-    //TODO: assert something useful
+
+    assertTrue(compteCourant.valeurComptable>vieEstudiantine.getValeurComptable());
   }
 
   @Test
