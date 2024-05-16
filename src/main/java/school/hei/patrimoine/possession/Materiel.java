98ba3d92f293fd196c64f624b54abd623ff9ee22
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-05-16 21:29:59 +0300

    chore: reajust Materiel::ProjectionFuture and Patrimoine::getValeurComptable,projectionFuture

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,19 +1,19 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
-
 import java.time.Instant;
 
 public final class Materiel extends Possession {
-  private final double tauxDAppreciationAnnuelle;
+    private final double tauxDAppreciationAnnuelle;
 
-  public Materiel(String nom, Instant t, int valeurComptable, double tauxDAppreciationAnnuelle) {
-    super(nom, t, valeurComptable);
-    this.tauxDAppreciationAnnuelle = tauxDAppreciationAnnuelle;
-  }
+    public Materiel(String nom, Instant t, int valeurComptable, double tauxDAppreciationAnnuelle) {
+        super(nom, t, valeurComptable);
+        this.tauxDAppreciationAnnuelle = tauxDAppreciationAnnuelle;
+    }
 
-  @Override
-  public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
-  }
+    @Override
+    public Possession projectionFuture(Instant tFutur) {
+        long yearsElapsed = tFutur.getEpochSecond() - t.getEpochSecond() / 31556952L;
+        double depreciatedValue = valeurComptable * Math.pow(1 + tauxDAppreciationAnnuelle, yearsElapsed);
+        return new Materiel(nom, tFutur, (int) depreciatedValue, tauxDAppreciationAnnuelle);
+    }
 }
