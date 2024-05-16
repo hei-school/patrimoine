e917b919473c1353ab0ea465a9479cdc7ed3acf0
harenabs <hei.harena.6@gmail.com>
2024-05-16 13:50:53 +0300

    impl: Materiel::projectionFuture

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -3,6 +3,7 @@ package school.hei.patrimoine.possession;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,6 +15,12 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    double valeurComptableApresUnAn = valeurComptable * Math.abs(tauxDAppreciationAnnuelle);
+    double nombreDanneeEntreDeuxInstants =  ChronoUnit.DAYS.between(t,tFutur) / 365.2425;
+    int valeurComptableFuture = (int) ( valeurComptable - (valeurComptableApresUnAn * nombreDanneeEntreDeuxInstants));
+    if (valeurComptableFuture < 0) {
+      valeurComptableFuture = 0;
+    }
+    return new Materiel(this.nom, tFutur, valeurComptableFuture, this.tauxDAppreciationAnnuelle);
   }
 }
