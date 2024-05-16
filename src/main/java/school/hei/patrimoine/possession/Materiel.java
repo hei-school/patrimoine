ed24a062f03928891ec4affe241ed90272b0df09
Salohyy212 <zerambaysalohy@gmail.com>
2024-05-16 20:19:15 +0300

    fix: conversion en année de nombreAnnée

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -2,6 +2,7 @@ package school.hei.patrimoine.possession;
 
 import school.hei.patrimoine.NotImplemented;
 
+import java.time.Duration;
 import java.time.Instant;
 
 public final class Materiel extends Possession {
@@ -14,7 +15,7 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    double nombreAnnees = tFutur.getEpochSecond() - this.getT().getEpochSecond()/365;
+    double nombreAnnees = Duration.between(t, tFutur).toDays() / 365;
     double valeurComptableFuture = getValeurComptable() * Math.pow((1 + tauxDAppreciationAnnuelle), nombreAnnees);
     return new Materiel(this.getNom(), tFutur, (int) valeurComptableFuture, tauxDAppreciationAnnuelle);
   }
