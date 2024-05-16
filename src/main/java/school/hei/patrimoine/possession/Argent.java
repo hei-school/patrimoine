75048ceb63e670b8c29e240548fe6b813c1abba1
Lou Andria <lou@hei.school>
2024-05-16 09:24:09 +0300

    chore: bidirectional Argent<-->TrainDeVie

--- src/main/java/school/hei/patrimoine/possession/Argent.java
+++ src/main/java/school/hei/patrimoine/possession/Argent.java
@@ -2,14 +2,26 @@ package school.hei.patrimoine.possession;
 
 
 import java.time.Instant;
+import java.util.Set;
 
 public final class Argent extends Possession {
+  private final Set<TrainDeVie> financés;
+
   public Argent(String nom, Instant t, int valeurComptable) {
+    this(nom, t, valeurComptable, Set.of());
+  }
+
+  private Argent(String nom, Instant t, int valeurComptable, Set<TrainDeVie> financés) {
     super(nom, t, valeurComptable);
+    this.financés = financés;
   }
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
     return new Argent(this.nom, tFutur, this.valeurComptable);
+  }
+
+  void addFinancés(TrainDeVie trainDeVie) {
+    financés.add(trainDeVie);
   }
 }
