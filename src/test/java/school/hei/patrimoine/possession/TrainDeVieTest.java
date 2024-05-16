8f4c751d850e049b30e358258e8a4b9d48172cce
HarivolaANDY <hei.andy.43@gmail.com>
2024-05-16 07:36:46 +0300

    test: add TrainDeVieTest assertion

--- src/test/java/school/hei/patrimoine/possession/TrainDeVieTest.java
+++ src/test/java/school/hei/patrimoine/possession/TrainDeVieTest.java
@@ -21,6 +21,6 @@ class TrainDeVieTest {
         aLaDiplomation,
         compteCourant,
         1);
-    //TODO: assert something useful
+    assertTrue(compteCourant.getValeurComptable() > vieEstudiantine.getDepensesMensuelle());
   }
 }
