5cbebfacd587450b0b8efcc05d7d69183ba6577a
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-05-16 23:36:28 +0300

    test: add valeur__argent_comptable_futur()

---
+++ src/test/java/school/hei/patrimoine/possession/ArgentTest.java
@@ -0,0 +1,31 @@
+package school.hei.patrimoine.possession;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgentTest {
+
+    @Test
+    void valeur__argent_comptable_futur(){
+        var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
+        var compteCourant = new Argent("Compte courant", au13mai24, 600_000);
+
+        var aLOuvertureDeHEI = Instant.parse("2021-10-26T00:00:00.00Z");
+        var aLaDiplomation = Instant.parse("2024-12-26T00:00:00.00Z");
+        var vieEstudiantine = new TrainDeVie(
+                "Ma super(?) vie d'etudiant",
+                500_000,
+                aLOuvertureDeHEI,
+                aLaDiplomation,
+                compteCourant,
+                1);
+
+        var au01juin24 = Instant.parse("2024-06-01T00:00:00.00Z");
+
+        assertEquals(100_000,compteCourant.projectionFuture(au01juin24).getValeurComptable());
+    }
+
+}
