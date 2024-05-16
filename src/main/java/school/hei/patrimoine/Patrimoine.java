a8ed781c284021b3ae352c75f494d228bdd23b94
Ravo1401 <hei.ravo@gmail.com>
2024-05-16 21:52:51 +0200

    feat: implement Materiel:projectionFuture

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -4,21 +4,23 @@ import school.hei.patrimoine.possession.Possession;
 
 import java.time.Instant;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toSet;
 
 public record Patrimoine(
-    Personne possesseur, Instant t, Set<Possession> possessions) {
+        Personne possesseur, Instant t, Set<Possession> possessions) {
   public int getValeurComptable() {
     if (possessions.isEmpty()) {
       return 0;
     }
-    int somme = 0;
-    for (Possession possession : possessions) {
-      somme += possession.getValeurComptable();
-    }
-    return somme;
+    return possessions.stream().mapToInt(Possession::getValeurComptable).sum();
   }
 
   public Patrimoine projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    return new Patrimoine(
+            possesseur,
+            tFutur,
+            possessions.stream().map(p -> p.projectionFuture(tFutur)).collect(toSet()));
   }
 }
--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,9 +1,7 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
-
+import java.time.Duration;
 import java.time.Instant;
-import java.time.ZoneId;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -14,15 +12,10 @@ public final class Materiel extends Possession {
   }
 
   @Override
-  public Possession projectionFuture(Instant tFutur) {
-    int year= ((tFutur.atZone(ZoneId.systemDefault()).getYear()) - t.atZone(ZoneId.systemDefault()).getYear());
-    int futureValue =(int) (getValeurComptable() * Math.pow(1 + tauxDAppreciationAnnuelle, year));
-
-    return new Materiel(
-            getNom(),
-            tFutur,
-            futureValue,
-            tauxDAppreciationAnnuelle
-    );
+  public Materiel projectionFuture(Instant tFutur) {
+    double joursEcoules = Duration.between(t, tFutur).toDays();
+    double valeurAjouteeJournaliere = valeurComptable * (tauxDAppreciationAnnuelle / 365);
+    int valeurComptableFuture = (int) (valeurComptable + (valeurAjouteeJournaliere * joursEcoules));
+    return new Materiel(nom, tFutur, valeurComptableFuture, tauxDAppreciationAnnuelle);
   }
 }
