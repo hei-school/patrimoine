86b2104905e6fa11d1541c9908fffa4a7d23c548
HarenaFiantso <fiantsorav@gmail.com>
2024-05-16 20:24:46 +0300

    correction: chore: new Material::projectionFuture

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,7 +1,7 @@
 package school.hei.patrimoine.possession;
 
+import java.time.Duration;
 import java.time.Instant;
-import java.util.concurrent.TimeUnit;
 
 public final class Materiel extends Possession {
   private final double tauxDAppreciationAnnuelle;
@@ -13,12 +13,9 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    long joursEcoules = TimeUnit.MILLISECONDS.toDays(tFutur.toEpochMilli() - getT().toEpochMilli());
-    double anneesEcoulees = joursEcoules / 365.0;
-
-    double nouvelleValeurComptable =
-        getValeurComptable() * Math.pow(1 + tauxDAppreciationAnnuelle, anneesEcoulees);
-
-    return new Materiel(getNom(), tFutur, (int) nouvelleValeurComptable, tauxDAppreciationAnnuelle);
+    double joursDeDifference = Duration.between(t, tFutur).toDays();
+    int valeurAjoutee =
+        (int) (valeurComptable * ((tauxDAppreciationAnnuelle * joursDeDifference) / 365));
+    return new Materiel(nom, tFutur, valeurComptable + valeurAjoutee, tauxDAppreciationAnnuelle);
   }
 }
