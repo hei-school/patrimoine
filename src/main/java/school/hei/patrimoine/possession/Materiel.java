306f8dcf0225c2a13f1610e37142f544c6fed92e
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-05-16 21:18:46 +0300

    impl: Materiel::projectionFuture

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -1,19 +1,28 @@
 package school.hei.patrimoine.possession;
 
-import school.hei.patrimoine.NotImplemented;
 
+import java.time.Duration;
 import java.time.Instant;
 
 public final class Materiel extends Possession {
-  private final double tauxDAppreciationAnnuelle;
+    private final double tauxDAppreciationAnnuelle;
 
-  public Materiel(String nom, Instant t, int valeurComptable, double tauxDAppreciationAnnuelle) {
-    super(nom, t, valeurComptable);
-    this.tauxDAppreciationAnnuelle = tauxDAppreciationAnnuelle;
-  }
+    public Materiel(String nom, Instant t, int valeurComptable, double tauxDAppreciationAnnuelle) {
+        super(nom, t, valeurComptable);
+        this.tauxDAppreciationAnnuelle = tauxDAppreciationAnnuelle;
+    }
 
-  @Override
-  public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
-  }
+    @Override
+    public Materiel projectionFuture(Instant tFutur) {
+        double differenceJour = Duration.between(t, tFutur).toDays();
+        int valeurAjoute = (int) (valeurComptable * ((tauxDAppreciationAnnuelle * differenceJour) / 365));
+        int valeurComptableFutur = valeurComptable + valeurAjoute;
+
+        return new Materiel(
+                nom,
+                tFutur,
+                valeurComptableFutur,
+                tauxDAppreciationAnnuelle
+        );
+    }
 }
