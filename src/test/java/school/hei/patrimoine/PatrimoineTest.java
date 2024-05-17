00c32f9caedaab35e911a337b7021d07aa2bf887
kirisaki_vk <kirisaki.vk@gmail.com>
2024-05-17 06:34:44 +0300

    test: patrimoine_ilo_le_26_juin_2024 & patrimoine_ilo_le_14_juillet_2024

--- src/test/java/school/hei/patrimoine/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/PatrimoineTest.java
@@ -2,12 +2,13 @@ package school.hei.patrimoine;
 
 import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.possession.Argent;
+import school.hei.patrimoine.possession.Materiel;
 import school.hei.patrimoine.possession.TrainDeVie;
 
 import java.time.Instant;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PatrimoineTest {
 
@@ -52,5 +53,75 @@ class PatrimoineTest {
         ilo,
         au13mai24,
         Set.of(financeur, trainDeVie));
+  }
+
+  @Test
+  void patrimoine_ilo_le_26_juin_2024() {
+    var ilo = new Personne("Ilo");
+
+    var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
+    var especes = new Argent("Espèces", au13mai24, 400_000);
+    var compteEpargne = new Argent("Compte epargne", au13mai24, 200_000);
+    var compteCourant = new Argent("Compte courant", au13mai24, 600_000);
+
+    var ordinateur = new Materiel("Ordinateur", au13mai24, 2_000_000, -0.10);
+    var effetsVestimentaires = new Materiel(
+            "Effets vestimentaires",
+            au13mai24,
+            1_000_000,
+            -0.20);
+
+    var aLOuvertureDeHEI = Instant.parse("2021-10-26T00:00:00.00Z");
+    var aLaDiplomation = Instant.parse("2024-12-26T00:00:00.00Z");
+    var trainDeVie = new TrainDeVie(
+            "Train de vie",
+            500_000,
+            aLOuvertureDeHEI,
+            aLaDiplomation,
+            compteCourant,
+            1);
+
+    var patrimoineIloAu13mai24 = new Patrimoine(
+            ilo,
+            au13mai24,
+            Set.of(especes, compteEpargne, compteCourant, ordinateur, effetsVestimentaires, trainDeVie));
+
+    var au26juin24 = Instant.parse("2024-06-26T00:00:00.00Z");
+    assertTrue(patrimoineIloAu13mai24.projectionFuture(au26juin24).getValeurComptable() < 3_700_000);
+  }
+
+  @Test
+  void patrimoine_ilo_le_14_juillet_2024() {
+    var ilo = new Personne("Ilo");
+
+    var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
+    var especes = new Argent("Espèces", au13mai24, 400_000);
+    var compteEpargne = new Argent("Compte epargne", au13mai24, 200_000);
+    var compteCourant = new Argent("Compte courant", au13mai24, 600_000);
+
+    var ordinateur = new Materiel("Ordinateur", au13mai24, 2_000_000, -0.10);
+    var effetsVestimentaires = new Materiel(
+            "Effets vestimentaires",
+            au13mai24,
+            1_000_000,
+            -0.20);
+
+    var aLOuvertureDeHEI = Instant.parse("2021-10-26T00:00:00.00Z");
+    var aLaDiplomation = Instant.parse("2024-12-26T00:00:00.00Z");
+    var trainDeVie = new TrainDeVie(
+            "Train de vie",
+            500_000,
+            aLOuvertureDeHEI,
+            aLaDiplomation,
+            compteCourant,
+            1);
+
+    var patrimoineIloAu13mai24 = new Patrimoine(
+            ilo,
+            au13mai24,
+            Set.of(especes, compteEpargne, compteCourant, ordinateur, effetsVestimentaires, trainDeVie));
+
+    var au14juillet24 = Instant.parse("2024-07-14T00:00:00.00Z");
+    assertTrue(patrimoineIloAu13mai24.projectionFuture(au14juillet24).getValeurComptable() < 3_700_000);
   }
 }
