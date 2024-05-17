a140fbf69aae5ead55bcff00513aea67d77db135
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2024-05-17 04:44:54 +0300

    feat: add new test for method projectionFuture

--- src/test/java/school/hei/patrimoine/possession/MaterielTest.java
+++ src/test/java/school/hei/patrimoine/possession/MaterielTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MaterielTest {
@@ -20,5 +21,22 @@ class MaterielTest {
     var au26juin24 = Instant.parse("2024-06-26T00:00:00.00Z");
     assertTrue(
         mac.getValeurComptable() > mac.valeurComptableFuture(au26juin24));
+  }
+
+  @Test
+  void testProjectionFuture() {
+    String nom = "Ordinateur portable";
+    Instant t = Instant.parse("2022-01-01T00:00:00Z");
+    int valeurComptable = 1000;
+    double tauxDAppreciationAnnuelle = 0.05; // 5%
+    Materiel materiel = new Materiel(nom, t, valeurComptable, tauxDAppreciationAnnuelle);
+
+    Instant tFutur = Instant.parse("2023-01-01T00:00:00Z");
+
+    Materiel materielProjete = materiel.projectionFuture(tFutur);
+
+    double valeurComptableAttendue = valeurComptable * Math.pow(1 + tauxDAppreciationAnnuelle, 1); // Pour une année de projection
+
+    assertEquals((int)Math.round(valeurComptableAttendue), materielProjete.getValeurComptable());
   }
 }
