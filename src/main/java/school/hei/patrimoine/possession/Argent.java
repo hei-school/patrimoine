cf6311ab4d6a74d97c7c1a14103133900b298414
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2024-05-17 05:04:05 +0300

    chore: implement method financementsFutur

--- src/main/java/school/hei/patrimoine/possession/Argent.java
+++ src/main/java/school/hei/patrimoine/possession/Argent.java
@@ -1,5 +1,6 @@
 package school.hei.patrimoine.possession;
 
+import lombok.Getter;
 import school.hei.patrimoine.NotImplemented;
 
 import java.time.Instant;
@@ -8,32 +9,36 @@ import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
 
+@Getter
 public final class Argent extends Possession {
-  private final Set<TrainDeVie> financés;
+  private final Set<TrainDeVie> finances;
 
   public Argent(String nom, Instant t, int valeurComptable) {
     this(nom, t, valeurComptable, new HashSet<>());
   }
 
-  private Argent(String nom, Instant t, int valeurComptable, Set<TrainDeVie> financés) {
+  Argent(String nom, Instant t, int valeurComptable, Set<TrainDeVie> finances) {
     super(nom, t, valeurComptable);
-    this.financés = financés;
+    this.finances = finances;
   }
 
-  @Override
-  public Argent projectionFuture(Instant tFutur) {
-    return new Argent(
-        nom,
-        tFutur,
-        valeurComptable - financementsFutur(tFutur),
-        financés.stream().map(f -> f.projectionFuture(tFutur)).collect(toSet()));
-  }
+    @Override
+    public Argent projectionFuture(Instant tFutur) {
+      return new Argent(
+          nom,
+          tFutur,
+          valeurComptable - financementsFutur(tFutur),
+          finances.stream().map(f -> f.projectionFuture(tFutur)).collect(toSet()));
+    }
 
-  private int financementsFutur(Instant tFutur) {
-    throw new NotImplemented();
+  int financementsFutur(Instant tFutur) {
+    return finances.stream()
+        .filter(trainDeVie -> trainDeVie.getDateDePonction() <= tFutur.getEpochSecond())
+        .map(TrainDeVie::getDepensesMensuelle)
+        .reduce(0, Integer::sum);
   }
 
   void addFinancés(TrainDeVie trainDeVie) {
-    financés.add(trainDeVie);
+    finances.add(trainDeVie);
   }
 }
