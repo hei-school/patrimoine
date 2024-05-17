da45008f3a0aeafd5da8a2d29cc09eeaeda50c71
herimaharavohaja <hei.haja.25@gmail.com>
2024-05-17 06:58:51 +0200

    chore: remote Materiel

--- src/main/java/school/hei/patrimoine/possession/Materiel.java
+++ src/main/java/school/hei/patrimoine/possession/Materiel.java
@@ -2,6 +2,7 @@ package school.hei.patrimoine.possession;
 
 import school.hei.patrimoine.NotImplemented;
 
+import java.time.Duration;
 import java.time.Instant;
 
 public final class Materiel extends Possession {
@@ -14,6 +15,14 @@ public final class Materiel extends Possession {
 
   @Override
   public Possession projectionFuture(Instant tFutur) {
-    throw new NotImplemented();
+    long diffSeconds = Duration.between(this.t, tFutur).getSeconds();
+    long diffDays = diffSeconds / (24 * 60 * 60); // Convert seconds to days
+    double depreciationRatePerDay = this.tauxDAppreciationAnnuelle / 365.0;
+    double totalDepreciation = depreciationRatePerDay * diffDays * this.valeurComptable / 100;
+    int newValeurComptable = (int) (this.valeurComptable - totalDepreciation);
+
+    newValeurComptable = Math.max(newValeurComptable, 0);
+
+    return new Materiel(this.nom, tFutur, newValeurComptable, this.tauxDAppreciationAnnuelle);
   }
 }
