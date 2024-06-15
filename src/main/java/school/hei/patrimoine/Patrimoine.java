d52c78df1005ac306561a9eab7b337431e2a2365
Lou Andria <lou@hei.school>
2024-06-15 11:49:19 +0200

    feat: impl Patrimoine::getValeurComptable

--- src/main/java/school/hei/patrimoine/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/Patrimoine.java
@@ -4,17 +4,14 @@ import school.hei.patrimoine.possession.Possession;
 
 import java.time.Instant;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toSet;
 
 public record Patrimoine(
     Personne possesseur, Instant t, Set<Possession> possessions) {
+
   public int getValeurComptable() {
-    if (possessions.isEmpty()) {
-      return 0;
-    }
-    throw new NotImplemented();
+    return possessions.stream().map(Possession::getValeurComptable).reduce(0, Integer::sum);
   }
 
   public Patrimoine projectionFuture(Instant tFutur) {
