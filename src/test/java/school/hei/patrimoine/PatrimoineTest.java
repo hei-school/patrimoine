9e1e494e9dfd154ad6d944ffaa1257e26fcb2cdf
Lou Andria <lou@hei.school>
2024-06-15 12:52:21 +0200

    test: patrimoine::getProjectionFuture

--- src/test/java/school/hei/patrimoine/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/PatrimoineTest.java
@@ -7,6 +7,7 @@ import school.hei.patrimoine.possession.FluxArgent;
 import java.time.Instant;
 import java.util.Set;
 
+import static java.time.temporal.ChronoUnit.DAYS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PatrimoineTest {
@@ -42,15 +43,22 @@ class PatrimoineTest {
   @Test
   void patrimoine_possede_un_train_de_vie_financé_par_argent() {
     var ilo = new Personne("Ilo");
-
     var au13mai24 = Instant.parse("2024-05-13T00:00:00.00Z");
-    var financeur = new Argent("Espèces", au13mai24, 400_000);
-
-    var trainDeVie = new FluxArgent(null, 0, null, null, financeur, 0);
+    var financeur = new Argent("Espèces", au13mai24, 600_000);
+    var trainDeVie = new FluxArgent(
+        "Vie courante",
+        -100_000,
+        au13mai24.minus(100, DAYS),
+        au13mai24.plus(100, DAYS),
+        financeur, 15);
 
     var patrimoineIloAu13mai24 = new Patrimoine(
         ilo,
         au13mai24,
         Set.of(financeur, trainDeVie));
+
+    assertEquals(500_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plus(10, DAYS)).getValeurComptable());
+    assertEquals(200_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plus(100, DAYS)).getValeurComptable());
+    assertEquals(200_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plus(1_000, DAYS)).getValeurComptable());
   }
 }
