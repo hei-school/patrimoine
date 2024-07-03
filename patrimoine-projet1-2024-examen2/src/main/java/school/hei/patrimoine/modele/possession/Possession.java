2bed2b57b24e4a0b0f931ffcf62a4b7a8e44cc69
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2024-07-03 12:46:46 +0300

    feat: add method convertValeurComptable in Possession

--- patrimoine-projet1-2024-examen2/src/main/java/school/hei/patrimoine/modele/possession/Possession.java
+++ patrimoine-projet1-2024-examen2/src/main/java/school/hei/patrimoine/modele/possession/Possession.java
@@ -17,10 +17,21 @@ public sealed abstract class Possession implements Serializable /*note(no-serial
   protected final String nom;
   protected final LocalDate t;
   protected final int valeurComptable;
+  protected final String devise;
+
 
   public final int valeurComptableFuture(LocalDate tFutur) {
     return projectionFuture(tFutur).getValeurComptable();
   }
 
   public abstract Possession projectionFuture(LocalDate tFutur);
+
+  public double convertValeurComptable(String targetDevise, double tauxChange, double tauxAppreciation, long jours) {
+    if (this.devise.equals(targetDevise)) {
+      return valeurComptable;
+    } else {
+      double tauxConversion = tauxChange * Math.pow(1 + tauxAppreciation, jours / 365.0);
+      return valeurComptable / tauxConversion;
+    }
+  }
 }
