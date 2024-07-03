831e81ce107e032281b2074cc551c857fb655794
tokiniaina <hei.tokiniaina@gmail.com>
2024-07-03 12:36:37 +0300

    refactor: endette >>> Zety_s_endette

--- src/test/java/school/hei/patrimoine/modele/PatrimoineZetyTest.java
+++ src/test/java/school/hei/patrimoine/modele/PatrimoineZetyTest.java
@@ -35,19 +35,11 @@ public class PatrimoineZetyTest {
 
     }
     @Test
-    public void endette() {
+    public void Zety_s_endette() {
         var Zety = new Personne("Zety");
         var au3Juillet24 = LocalDate.of(2024, JULY, 3);
         var au17Sept24 = LocalDate.of(2024, SEPTEMBER, 17);
         var au18Sept24 = LocalDate.of(2024, SEPTEMBER, 18);
-
-        var ordinateur = new Materiel(
-                "ordinateur",
-                au3Juillet24,
-                1_200_000,
-                au3Juillet24,
-                -0.10
-        );
         var vetements = new Materiel(
                 "vetements",
                 au3Juillet24,
@@ -55,35 +47,30 @@ public class PatrimoineZetyTest {
                 au3Juillet24,
                 -0.50
         );
-        var argent_en_espece = new Argent("Argent en espece de Zety", au3Juillet24, 800_000);
-        var argent_ecollage_de_zety = new Argent("Ecollage de Zety", au3Juillet24, 200_000);
-        var argent_dans_le_compte = new Argent("Argent dans le compte de Zety", au3Juillet24, 100_000);
-        var frais_de_scolarite = new FluxArgent("Frais de scolarite de Zety", argent_ecollage_de_zety, LocalDate.of(2023, NOVEMBER, 1), LocalDate.of(2024, AUGUST, 31), -200_000, 27);
-        var compte_bancaire = new FluxArgent("Compte bancaire de Zety", argent_dans_le_compte, au3Juillet24, au17Sept24, -20_000, 25);
-        var emprunt = new Argent("Emprunt de Zety", au18Sept24, 10_000_000);
-        var dette = new Argent("Dette de Zety", au18Sept24, -11_000_000);
-        var patrimoine_Zety_le_3_juillet_2024 = new Patrimoine(
-                "patrimoine de zety 17 septembre 2024",
-                Zety,
+        var ordinateur = new Materiel(
+                "ordinateur",
                 au3Juillet24,
-                Set.of(new GroupePossession(
-                                "possession de Zety",
-                                au3Juillet24,
-                                Set.of(ordinateur,
-                                        vetements,
-                                        argent_en_espece,
-                                        frais_de_scolarite,
-                                        compte_bancaire,
-                                        emprunt,
-                                        dette
-                                )
-                        )
+                1_200_000,
+                au3Juillet24,
+                -0.10
+        );
+        var argentEnEspece = new Argent("argent en espece", au3Juillet24, 800_000);
+        var argentEcolage = new Argent("ecolage", au3Juillet24, 200_000);
+        var argentDansLeCompte = new Argent("Argent dans le compte de Zety", au3Juillet24, 100_000);
+        var fraisDeScolarite = new FluxArgent("Frais de scolarite", argentEcolage, LocalDate.of(2023, NOVEMBER, 1), LocalDate.of(2024, AUGUST, 31), -200_000, 27);
+        var compteBancaire = new FluxArgent("Compte bancaire", argentDansLeCompte, au3Juillet24, au17Sept24, -20_000, 25);
+        var emprunt = new Argent("Emprunt", au18Sept24, 10_000_000);
+        var dette = new Argent("Dette", au18Sept24, -11_000_000);
+        var patrimoine = new Patrimoine(
+                "patrimoine de zety 17 septembre 2024", Zety, au3Juillet24, Set.of(new GroupePossession("possession de Zety", au3Juillet24, Set.of(ordinateur, vetements, argentEnEspece, fraisDeScolarite, compteBancaire, emprunt, dette))
                 )
         );
-        var patrimoine_le_17_septembre = patrimoine_Zety_le_3_juillet_2024.projectionFuture(au17Sept24).getValeurComptable();
-        var patrimoine_le_18_septembre = patrimoine_Zety_le_3_juillet_2024.projectionFuture(au18Sept24).getValeurComptable();
-        var diminution_de_patimoine = patrimoine_le_17_septembre - patrimoine_le_18_septembre;
-        assertEquals(1002384, diminution_de_patimoine);
+        //calcul de la valeur du patrimoine a date de 17 septembre
+        var patrimoineLe17Septembre = patrimoine.projectionFuture(au17Sept24).getValeurComptable();
+        //calcul de la valeur du patrimoine a date de 17 septembre
+        var patrimoineLe18Septembre = patrimoine.projectionFuture(au18Sept24).getValeurComptable();
+        var diminutionPatimoine = patrimoineLe17Septembre - patrimoineLe18Septembre;
+        assertEquals(1002384, diminutionPatimoine);
     }
 }
 
