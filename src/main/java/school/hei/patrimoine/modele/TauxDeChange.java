eae288653d210bc21f5cafbade35818a2ab506cd
AndoKami <hei.ando.1007@gmail.com>
2024-07-03 11:21:03 +0300

    Add: adding 2 new files for devise and taux de change

---
+++ src/main/java/school/hei/patrimoine/modele/TauxDeChange.java
@@ -0,0 +1,24 @@
+package school.hei.patrimoine.modele;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class TauxDeChange {
+    private Devise source;
+    private Devise destination;
+    private double taux;
+    private double tauxAppreciationAnnuelle;
+
+    public TauxDeChange(Devise source, Devise destination, double taux, double tauxAppreciationAnnuelle) {
+        this.source = source;
+        this.destination = destination;
+        this.taux = taux;
+        this.tauxAppreciationAnnuelle = tauxAppreciationAnnuelle;
+    }
+
+    public double convertir(double montant, LocalDate dateInitiale, LocalDate dateFinale) {
+        long jours = ChronoUnit.DAYS.between(dateInitiale, dateFinale);
+        double appreciationTotale = Math.pow(1 + tauxAppreciationAnnuelle, jours / 365.0);
+        return montant * taux * appreciationTotale;
+    }
+}
