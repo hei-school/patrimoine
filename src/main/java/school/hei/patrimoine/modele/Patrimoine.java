09dd6632c3899f9b0e3cb8f77fb4991566c2adad
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-07-03 12:39:29 +0300

    feat: add class tauxdechange and change strucutre of code in patrimoine

--- src/main/java/school/hei/patrimoine/modele/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/modele/Patrimoine.java
@@ -1,6 +1,8 @@
 package school.hei.patrimoine.modele;
 
+import school.hei.patrimoine.modele.possession.Devise;
 import school.hei.patrimoine.modele.possession.Possession;
+import school.hei.patrimoine.modele.possession.TauxDechange;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -9,18 +11,24 @@ import java.util.Set;
 import static java.util.stream.Collectors.toSet;
 
 public record Patrimoine(
-    String nom, Personne possesseur, LocalDate t, Set<Possession> possessions)
-    implements Serializable/*note(no-serializable)*/ {
+        String nom, Personne possesseur, LocalDate t, Set<Possession> possessions)
+        implements Serializable/*note(no-serializable)*/ {
 
   public int getValeurComptable() {
     return possessions.stream().mapToInt(Possession::getValeurComptable).sum();
   }
+
   public Patrimoine projectionFuture(LocalDate tFutur) {
     return new Patrimoine(
-        nom,
-        possesseur,
-        tFutur,
-        possessions.stream().map(p -> p.projectionFuture(tFutur)).collect(toSet()));
+            nom,
+            possesseur,
+            tFutur,
+            possessions.stream().map(p -> p.projectionFuture(tFutur)).collect(toSet()));
+  }
+
+  public Patrimoine projectionFuture(LocalDate tFutur, Devise devise, TauxDechange tauxDeChange){
+    //TODO: implement
+    return null;
   }
 
   public Possession possessionParNom(String nom) {
