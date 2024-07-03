07ca82ce886f70e47f19badc8752ee77f646f5e4
andyxrakoto <hei.andinirina@gmail.com>
2024-07-03 12:07:35 +0200

    Feat: implement of my last test

--- src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.possession.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 import static java.time.Month.MAY;
@@ -178,7 +179,7 @@ class PatrimoineTest {
 
     var donParents = new FluxArgent(
             "Don des parents", argentEspeces, LocalDate.of(2024, 1, 15),
-            LocalDate.of(2024, DECEMBER, 15), 100_000, 15);
+            LocalDate.of(2024, 12, 15), 100_000, 15);
 
     var trainDeVie = new FluxArgent(
             "Train de vie", argentEspeces, au1octobre24,
@@ -194,7 +195,7 @@ class PatrimoineTest {
             Set.of(ordinateur, vetements, argentEspeces, fraisScolarite, compteBancaire, fraisTenueCompte, donParents, trainDeVie, paiementScolarite));
 
     LocalDate dateEpuisementEspeces = au3juillet24;
-    while (patrimoineZetyAu3juillet24.projectionFuture(dateEpuisementEspeces).getValeurComptable() > 0) {
+    while (patrimoineZetyAu3juillet24.projectionFuture(dateEpuisementEspeces).getValeurComptable() >= 0) {
       dateEpuisementEspeces = dateEpuisementEspeces.plusDays(1);
     }
 
@@ -237,5 +238,66 @@ class PatrimoineTest {
             Set.of(ordinateur, vetements, argentEspeces, fraisScolarite, compteBancaire, fraisTenueCompte, donParents, trainDeVie, paiementScolarite));
 
     assertEquals(-608686, patrimoineDeZetyAu3juillet24.projectionFuture(au14fevrier25).getValeurComptable());
+  }
+  @Test
+  void Etudier_En_Allemange() {
+    var patrimoineZetyAu3Juillet2024 = patrimoineDeZety();
+    var au21septembre2024 = LocalDate.of(2024, 9, 21);
+    var au1Janvier2024 = LocalDate.of(2024, 1, 1);
+    var au1ctobre2024 = LocalDate.of(2024, 1, 1);
+    var au13Fevrier2025 = LocalDate.of(2025, 2, 13);
+
+    var fraisScolariteEnUnMois = new FluxArgent("fraisScolariteEnMois", (Argent) patrimoineZetyAu3Juillet2024.possessionParNom("zetyCompteBancaire"), au21septembre2024, au21septembre2024.plusDays(1), 2_500_000, 21);
+    var argentDepuisParent = new FluxArgent(
+            "argentDepuisParent",
+            (Argent) patrimoineZetyAu3Juillet2024.possessionParNom("argentEspeces"),
+            au1Janvier2024,
+            LocalDate.MAX,
+            100_000,
+            15
+    );
+    var trainDeVie = new FluxArgent(
+            "trainDeVie",
+            (Argent) patrimoineZetyAu3Juillet2024.possessionParNom("argentEspeces"),
+            au1ctobre2024,
+            au13Fevrier2025,
+            250_000,
+            1
+    );
+  }
+
+
+  public Patrimoine patrimoineDeZety() {
+    var zety = new Personne("Zety");
+    var au3juillet2024 = LocalDate.of(2024, 6, 3);
+    var debutFraisScolarite = LocalDate.of(2023, 11, 1);
+    var finFraisScolarite = LocalDate.of(2024, 8, 31);
+
+    var ordinateur = new Materiel("ordinateur", au3juillet2024, 1_200_000, au3juillet2024, -0.10);
+    var vetements = new Materiel("vetements", au3juillet2024, 1_500_000, au3juillet2024, -0.50);
+    var argentEspeces = new Argent("argentEspeces", au3juillet2024, 800_000);
+    var fraisDeScolariteParMois = new FluxArgent(
+            "zetyFluxDArgentSurScolarite",
+            argentEspeces,
+            debutFraisScolarite,
+            finFraisScolarite,
+            -200_000,
+            27
+    );
+
+    var compteBancaire = new Argent("zetyCompteBancaire", au3juillet2024, 100_000);
+    var tenueDeCompteBancaire = new FluxArgent("zetyFluxDArgentBancaire", compteBancaire, au3juillet2024, LocalDate.MAX, -20_000, 25);
+
+    return new Patrimoine(
+            "patrimoineZetyAu3juillet2024",
+            zety,
+            au3juillet2024,
+            new HashSet<>(Set.of(
+                    ordinateur,
+                    vetements,
+                    argentEspeces,
+                    compteBancaire
+            ))
+    );
   }
 }
