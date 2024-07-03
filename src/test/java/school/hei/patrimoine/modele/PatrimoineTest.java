6c5fdf19c3e7bf7a132843dc0b25ad1f9da7de88
Fifaliana <hei.fifaliana@gmail.com>
2024-07-03 11:39:08 +0300

    test: Add test for date when Zety runs out of cash

--- src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.possession.*;
 
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.Set;
 
 import static java.time.Month.*;
@@ -116,6 +117,49 @@ class PatrimoineTest {
 
     var evolutionJournaliere = evolutionPatrimoine.getEvolutionJournaliere();
     assertEquals(-1_000_000, evolutionJournaliere.get(au18septembre24).getValeurComptable() - evolutionJournaliere.get(au17septembre24).getValeurComptable());
+  }
+
+  @Test
+  void date_ou_zety_n_a_plus_d_espèces() {
+    var zety = new Personne("Zety");
+    var au1janvier24 = LocalDate.of(2024, JANUARY, 1);
+    var au21septembre24 = LocalDate.of(2024, SEPTEMBER, 21);
+    var au15janvier24 = LocalDate.of(2024, JANUARY, 15);
+    var au1eroctobre24 = LocalDate.of(2024, OCTOBER, 1);
+    var au1erfevrier25 = LocalDate.of(2025, FEBRUARY, 1);
+    var au31janvier25 = LocalDate.of(2025, JANUARY, 31);
+
+    var compteBancaire = new Argent("Compte bancaire", au1janvier24, 100_000);
+
+    var fraisScolarite = new FluxArgent("Frais de scolarité", compteBancaire, au21septembre24, au21septembre24, -2_500_000, 21);
+
+    var donParents = new FluxArgent("Don des parents", compteBancaire, au15janvier24, au1erfevrier25, 100_000, 15);
+
+    var trainDeVie = new FluxArgent("Train de vie", compteBancaire, au1eroctobre24, au1erfevrier25, -250_000, 1);
+
+    var fraisTenueCompte = new FluxArgent("Frais de tenue de compte", compteBancaire, au1janvier24, au31janvier25, -20_000, 25);
+
+    var patrimoineZetyAu1janvier24 = new Patrimoine(
+            "patrimoineZetyAu1janvier24",
+            zety,
+            au1janvier24,
+            Set.of(compteBancaire, fraisScolarite, donParents, trainDeVie, fraisTenueCompte));
+
+    var evolutionPatrimoine = new EvolutionPatrimoine(
+            "Nom",
+            patrimoineZetyAu1janvier24,
+            au1janvier24,
+            au31janvier25);
+
+    var evolutionJournaliere = evolutionPatrimoine.getEvolutionJournaliere();
+
+    var dateSansEspèces = evolutionJournaliere.entrySet().stream()
+            .filter(entry -> entry.getValue().getValeurComptable() < 0)
+            .map(Map.Entry::getKey)
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("Zety never runs out of cash."));
+
+    assertEquals(LocalDate.of(2025, JANUARY, 31), dateSansEspèces);
   }
   private static Patrimoine getPatrimoine(LocalDate au17septembre24, LocalDate au18septembre24, Personne zety) {
     var compteBancaire = new Argent("Compte bancaire", au17septembre24, 100_000);
