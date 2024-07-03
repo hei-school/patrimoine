d06206a224e23168ae24f3b4c4b6bbf798bf81f9
Harenabs21 <hei.harena.6@gmail.com>
2024-07-03 12:52:12 +0300

    feat(test): test passé valeur_comptable_de_patrimoine_zety_apres_emprunt()

--- src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
@@ -1,10 +1,7 @@
 package school.hei.patrimoine.modele;
 
 import org.junit.jupiter.api.Test;
-import school.hei.patrimoine.modele.possession.Argent;
-import school.hei.patrimoine.modele.possession.FluxArgent;
-import school.hei.patrimoine.modele.possession.GroupePossession;
-import school.hei.patrimoine.modele.possession.Materiel;
+import school.hei.patrimoine.modele.possession.*;
 
 import java.time.LocalDate;
 import java.util.Set;
@@ -89,26 +86,45 @@ class PatrimoineTest {
   }
 
   private static Patrimoine getPatrimoineZetyAu3juillet2024(LocalDate au3juillet2024, Personne zety) {
-    var ordinateur_de_zety = new Materiel("ordinateurDeZety",au3juillet2024,1_200_000,au3juillet2024,-0.10);
-    var vetement_de_zety = new Materiel("vetements", au3juillet2024,1_500_000, au3juillet2024,-0.50);
-    var argent_en_espece = new Argent("argentDeZety", au3juillet2024,800_000);
-    var ecollage_par_mois = new FluxArgent("ecolage_203_2024",argent_en_espece,LocalDate.of(2023,11,1),LocalDate.of(2024,8,31),-200_000,27);
-    var compte_bancaire = new Argent("compte bancaire", au3juillet2024,100_000);
-    var compte_bancaire_avec_le_ponctionnement = new FluxArgent("ponctionnement",compte_bancaire, au3juillet2024,LocalDate.of(9999,12,31),-20_000,25);
-    return new Patrimoine("patrimoine de zety", zety, au3juillet2024,Set.of(ordinateur_de_zety,vetement_de_zety,argent_en_espece,ecollage_par_mois,compte_bancaire,compte_bancaire_avec_le_ponctionnement));
+    var ordinateur_de_zety = new Materiel("ordinateurDeZety", au3juillet2024, 1_200_000, au3juillet2024, -0.10);
+    var vetement_de_zety = new Materiel("vetements", au3juillet2024, 1_500_000, au3juillet2024, -0.50);
+    var argent_en_espece = new Argent("argentDeZety", au3juillet2024, 800_000);
+    var ecollage_par_mois = new FluxArgent("ecolage_203_2024", argent_en_espece, LocalDate.of(2023, 11, 1), LocalDate.of(2024, 8, 31), -200_000, 27);
+    var compte_bancaire = new Argent("compte bancaire", au3juillet2024, 100_000);
+    var compte_bancaire_avec_le_ponctionnement = new FluxArgent("ponctionnement", compte_bancaire, au3juillet2024, LocalDate.of(9999, 12, 31), -20_000, 25);
+    return new Patrimoine("patrimoine de zety", zety, au3juillet2024, Set.of(ordinateur_de_zety, vetement_de_zety, argent_en_espece, ecollage_par_mois, compte_bancaire, compte_bancaire_avec_le_ponctionnement));
   }
 
   @Test
-  void patrimoine_zety_au_3_juillet_2024_jusquau_17_septembre_2024(){
+  void patrimoine_zety_au_3_juillet_2024_jusquau_17_septembre_2024() {
     var zety = new Personne("Zety");
-    var au3juillet2024 = LocalDate.of(2024,7,3);
+    var au3juillet2024 = LocalDate.of(2024, 7, 3);
     var patrimoine_zety_au_3juillet2024 = getPatrimoineZetyAu3juillet2024(au3juillet2024, zety);
-    assertEquals(3_600_000,patrimoine_zety_au_3juillet2024.getValeurComptable());
-    var au_17_septembre_2024 = LocalDate.of(2024,9,17);
+    assertEquals(3_600_000, patrimoine_zety_au_3juillet2024.getValeurComptable());
+    var au_17_septembre_2024 = LocalDate.of(2024, 9, 17);
     var patrimoine_zety_au_17_septembre_2024 = patrimoine_zety_au_3juillet2024.projectionFuture(au_17_septembre_2024);
     var valeur_comptable_future = patrimoine_zety_au_17_septembre_2024.getValeurComptable();
     assertTrue(patrimoine_zety_au_3juillet2024.getValeurComptable() > valeur_comptable_future);
-    assertEquals(2978848,valeur_comptable_future);
+    assertEquals(2978848, valeur_comptable_future);
+  }
+
+  @Test
+  void valeur_comptable_de_patrimoine_zety_apres_emprunt() {
+    var zety = new Personne("Zety");
+    var au3juillet2024 = LocalDate.of(2024, 7, 3);
+    var patrimoine_zety_au_3juillet2024 = getPatrimoineZetyAu3juillet2024(au3juillet2024, zety);
+    var au_17_septembre_2024 = LocalDate.of(2024, 9, 17);
+    var patrimoine_zety_au_17_septembre_2024 = patrimoine_zety_au_3juillet2024.projectionFuture(au_17_septembre_2024);
+    var au_18_septembre_2024 = LocalDate.of(2024, 9, 18);
+    var patrimoine_zety_au_18_septembre_2024 = patrimoine_zety_au_17_septembre_2024.projectionFuture(au_18_septembre_2024);
+    var argent_ajoute_zety = new Argent("Argent ajoute", au_18_septembre_2024, 10_000_000);
+    var dette_zety = new Dette("dette", au_18_septembre_2024, -11_000_000);
+    var fluxDArgentAvecDette = new FluxArgent("avec dette", argent_ajoute_zety, au_18_septembre_2024, au_18_septembre_2024.plusYears(1), -1_000_000, 18);
+    patrimoine_zety_au_18_septembre_2024.ajouterPossessions(argent_ajoute_zety);
+    patrimoine_zety_au_18_septembre_2024.ajouterPossessions(fluxDArgentAvecDette);
+    patrimoine_zety_au_18_septembre_2024.ajouterPossessions(dette_zety);
+    assertTrue(patrimoine_zety_au_17_septembre_2024.getValeurComptable() > patrimoine_zety_au_18_septembre_2024.getValeurComptable());
+    assertEquals(1976685, patrimoine_zety_au_18_septembre_2024.getValeurComptable());
   }
 
 
