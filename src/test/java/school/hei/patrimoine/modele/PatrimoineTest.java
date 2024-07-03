295d86d99dcf59f7646290d006d06e9558f67611
RianAina <hei.rianaina@gmail.com>
2024-07-03 11:17:12 +0300

    feat: cleared the first question

--- src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
@@ -4,14 +4,59 @@ import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.possession.Argent;
 import school.hei.patrimoine.modele.possession.FluxArgent;
 import school.hei.patrimoine.modele.possession.GroupePossession;
+import school.hei.patrimoine.modele.possession.Materiel;
 
 import java.time.LocalDate;
 import java.util.Set;
 
+import static java.time.Month.JULY;
 import static java.time.Month.MAY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PatrimoineTest {
+
+
+  @Test /* ---------------------------------- */
+  void patrimoine_zety_17_septembre () {
+    var zety = new Personne("Zety");
+    var au3juillet2024 = LocalDate.of(2024, JULY, 3);
+    var financeur = new Argent("Espèces", au3juillet2024.minusDays(1), au3juillet2024, 2_000_000);
+    var fraisDeScolarite = 2_000_000;
+
+    var ordinateur = new Materiel(
+            "MacBook Pro",
+            au3juillet2024,
+            1_200_000,
+            au3juillet2024.minusDays(0),
+            0);
+
+    var vetement = new Materiel(
+            "Vetement",
+            au3juillet2024,
+            1_500_000,
+            au3juillet2024.minusDays(0),
+            0);
+
+
+    var patrimoineZetyAu17septembre24 = new Patrimoine(
+            "patrimoineZetyAu17septembre24",
+            zety,
+            au3juillet2024,
+            Set.of(
+                    new Argent("Espèces", au3juillet2024, 800_000),
+                    new Argent("Compte bancaire", au3juillet2024, 60_000),
+                    new GroupePossession("Le groupe", au3juillet2024, Set.of(financeur, ordinateur, vetement))
+            )
+    );
+
+    assertEquals(5_560_000, patrimoineZetyAu17septembre24.projectionFuture(au3juillet2024.plusDays(46)).getValeurComptable());
+
+  }
+
+
+  void patrimoine_zety_entre_17et18_septembre () {
+
+  }
 
   @Test
   void patrimoine_vide_vaut_0() {
