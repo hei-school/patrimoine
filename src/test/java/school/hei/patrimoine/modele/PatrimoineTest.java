e862654bed1ee545c6369315e09907091376ca10
Lalaina0904 <lalainap19.aps2b@gmail.com>
2024-07-03 11:40:40 +0300

    test: PatrimoineTest zety on 17 sept 2024

--- src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.possession.Argent;
 import school.hei.patrimoine.modele.possession.FluxArgent;
 import school.hei.patrimoine.modele.possession.GroupePossession;
+import school.hei.patrimoine.modele.possession.Materiel;
 
 import java.time.LocalDate;
 import java.util.Set;
 
-import static java.time.Month.MAY;
+import static java.time.Month.*;
+import static java.time.Month.DECEMBER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PatrimoineTest {
@@ -84,4 +86,29 @@ class PatrimoineTest {
     assertEquals(200_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plusDays(100)).getValeurComptable());
     assertEquals(200_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plusDays(1_000)).getValeurComptable());
   }
+
+  @Test
+  void patrimoine_de_Zety_le_17_septembre_2024() {
+    var zety = new Personne("Zety");
+    var au3juillet24 = LocalDate.of(2024, JULY, 3);
+    var au17septembre24 = LocalDate.of(2024, SEPTEMBER, 17);
+
+    var ordinateur = new Materiel("Ordinateur", au3juillet24, 1_200_000, au3juillet24, -0.10);
+    var vetements = new Materiel("Vêtements", au3juillet24, 1_500_000, au3juillet24, -0.50);
+    var argent = new Argent("Espèce", au3juillet24, 800_000);
+    var fraisScolarite = new FluxArgent("Frais de scolarité", argent, LocalDate.of(2023, NOVEMBER, 27), LocalDate.of(2024, AUGUST, 27), -200_000, 27);
+    var compteBancaire = new Argent("Compte bancaire", au3juillet24, 100_000);
+    var fraisTenueCompte = new FluxArgent("Frais de tenue de compte", compteBancaire, au3juillet24.withDayOfMonth(25), LocalDate.of(2024, DECEMBER, 25), -20_000, 25);
+
+    var patrimoineZetyAu3juillet24 = new Patrimoine(
+            "patrimoineZetyAu3juillet24",
+            zety,
+            au3juillet24,
+            Set.of(ordinateur, vetements, argent, fraisScolarite, compteBancaire, fraisTenueCompte));
+
+    var patrimoineZetyAu17septembre24 = patrimoineZetyAu3juillet24.projectionFuture(au17septembre24);
+
+    assertEquals(2978848, patrimoineZetyAu17septembre24.getValeurComptable());
+  }
+
 }
