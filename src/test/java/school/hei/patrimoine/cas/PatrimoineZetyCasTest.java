4db508ea55aa9a967024d5326748a05aa85f6dde
Andriamahaly Loiqua <hei.loiqua@gmail.com>
2024-07-03 13:26:03 +0300

    fix error date

--- src/test/java/school/hei/patrimoine/cas/PatrimoineZetyCasTest.java
+++ src/test/java/school/hei/patrimoine/cas/PatrimoineZetyCasTest.java
@@ -11,6 +11,11 @@ import java.time.LocalDate;
 import java.util.Set;
 import java.util.logging.Logger;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static java.time.Month.AUGUST;
+import static java.time.Month.NOVEMBER;
+import static java.time.Month.SEPTEMBER;
+import static java.time.Month.JULY;
+import static java.time.Month.OCTOBER;
 
 public class PatrimoineZetyCasTest {
 
@@ -19,21 +24,21 @@ public class PatrimoineZetyCasTest {
     @Test
     public void testValeurPatrimoine() {
         var zety = new Personne("Zety");
-        var dateDebut = LocalDate.of(2024, JUILLET, 3);
+        var dateDebut = LocalDate.of(2024, JULY, 3);
 
         var ordinateur = new Materiel("Ordinateur", dateDebut, 1_200_000, dateDebut, -0.10);
         var vetements = new Materiel("Vêtements", dateDebut, 1_500_000, dateDebut, -0.50);
         var argentLiquide = new Argent("Argent liquide", dateDebut, 800_000);
         var compteBancaire = new Argent("Compte bancaire", dateDebut, 100_000);
 
-        new FluxArgent("Frais de scolarité", argentLiquide, LocalDate.of(2023, NOVEMBRE, 27), LocalDate.of(2024, AOUT, 27), -200_000, 27);
-        new FluxArgent("Frais bancaires", compteBancaire, dateDebut, LocalDate.of(2024, SEPTEMBRE, 17), -20_000, 25);
+        new FluxArgent("Frais de scolarité", argentLiquide, LocalDate.of(2023, NOVEMBER, 27), LocalDate.of(2024, AUGUST, 27), -200_000, 27);
+        new FluxArgent("Frais bancaires", compteBancaire, dateDebut, LocalDate.of(2024, SEPTEMBER, 17), -20_000, 25);
 
         var possessions = Set.of(ordinateur, vetements, argentLiquide, compteBancaire);
 
         var patrimoineZety = new Patrimoine("Patrimoine de Zety", zety, dateDebut, possessions);
 
-        LocalDate dateFutur = LocalDate.of(2024, SEPTEMBRE, 17);
+        LocalDate dateFutur = LocalDate.of(2024, SEPTEMBER, 17);
         Patrimoine patrimoineFutur = patrimoineZety.projectionFuture(dateFutur);
 
         int valeurComptable = patrimoineFutur.getValeurComptable();
@@ -47,18 +52,18 @@ public class PatrimoineZetyCasTest {
     @Test
     public void testValeurPatrimoineAvecDette() {
         var zety = new Personne("Zety");
-        var dateDebut = LocalDate.of(2024, JUILLET, 3);
+        var dateDebut = LocalDate.of(2024, JULY, 3);
 
         var ordinateur = new Materiel("Ordinateur", dateDebut, 1_200_000, dateDebut, -0.10);
         var vetements = new Materiel("Vêtements", dateDebut, 1_500_000, dateDebut, -0.50);
         var argentLiquide = new Argent("Argent liquide", dateDebut, 800_000);
         var compteBancaire = new Argent("Compte bancaire", dateDebut, 100_000);
 
-        new FluxArgent("Frais de scolarité", argentLiquide, LocalDate.of(2023, NOVEMBRE, 27), LocalDate.of(2024, AOUT, 27), -200_000, 27);
-        new FluxArgent("Frais bancaires", compteBancaire, dateDebut, LocalDate.of(2024, SEPTEMBRE, 17), -20_000, 25);
+        new FluxArgent("Frais de scolarité", argentLiquide, LocalDate.of(2023, NOVEMBER, 27), LocalDate.of(2024, AUGUST, 27), -200_000, 27);
+        new FluxArgent("Frais bancaires", compteBancaire, dateDebut, LocalDate.of(2024, SEPTEMBER, 17), -20_000, 25);
 
         // Ajouter la dette
-        var dateDette = LocalDate.of(2024, SEPTEMBRE, 18);
+        var dateDette = LocalDate.of(2024, SEPTEMBER, 18);
         var montantDette = -10_000_000; // Valeur négative pour la dette
         var dette = new Dette("Prêt bancaire", dateDette, montantDette);
 
@@ -66,8 +71,8 @@ public class PatrimoineZetyCasTest {
 
         var patrimoineZety = new Patrimoine("Patrimoine de Zety", zety, dateDebut, possessions);
 
-        LocalDate dateAvantDette = LocalDate.of(2024, SEPTEMBRE, 17);
-        LocalDate dateApresDette = LocalDate.of(2024, SEPTEMBRE, 18);
+        LocalDate dateAvantDette = LocalDate.of(2024, SEPTEMBER, 17);
+        LocalDate dateApresDette = LocalDate.of(2024, SEPTEMBER, 18);
 
         Patrimoine patrimoineAvantDette = patrimoineZety.projectionFuture(dateAvantDette);
         Patrimoine patrimoineApresDette = patrimoineZety.projectionFuture(dateApresDette);
@@ -86,18 +91,16 @@ public class PatrimoineZetyCasTest {
     @Test
     public void testValeurPatrimoineAvecManqueDEspeces() {
         var zety = new Personne("Zety");
-        var dateDebut = LocalDate.of(2024, JUILLET, 3);
+        var dateDebut = LocalDate.of(2024, JULY, 3);
 
         var ordinateur = new Materiel("Ordinateur", dateDebut, 1_200_000, dateDebut, -0.10);
         var vetements = new Materiel("Vêtements", dateDebut, 1_500_000, dateDebut, -0.50);
         var argentLiquide = new Argent("Argent liquide", dateDebut, 800_000);
         var compteBancaire = new Argent("Compte bancaire", dateDebut, 100_000);
 
-        new FluxArgent("Frais de scolarité", argentLiquide, LocalDate.of(2023, NOVEMBRE, LocalDate.of(2023, NOVEMBRE, 27), LocalDate.of(2024, AOUT, 27), -200_000, 27);
-        new FluxArgent("Frais bancaires", compteBancaire, dateDebut, LocalDate.of(2024, SEPTEMBRE, 17), -20_000, 25);
-
+        new FluxArgent("Frais bancaires", compteBancaire, dateDebut, LocalDate.of(2024, SEPTEMBER, 17), -20_000, 25);
         // Ajouter la dette
-        var dateDette = LocalDate.of(2024, SEPTEMBRE, 18);
+        var dateDette = LocalDate.of(2024, SEPTEMBER, 18);
         var montantDette = -10_000_000; // Valeur négative pour la dette
         var dette = new Dette("Prêt bancaire", dateDette, montantDette);
 
@@ -105,7 +108,7 @@ public class PatrimoineZetyCasTest {
 
         var patrimoineZety = new Patrimoine("Patrimoine de Zety", zety, dateDebut, possessions);
 
-        LocalDate dateManqueDEspeces = LocalDate.of(2024, OCTOBRE, 1);
+        LocalDate dateManqueDEspeces = LocalDate.of(2024, OCTOBER, 1);
 
         Patrimoine patrimoineManqueDEspeces = patrimoineZety.projectionFuture(dateManqueDEspeces);
 
@@ -113,7 +116,7 @@ public class PatrimoineZetyCasTest {
 
         logger.info("Valeur comptable du patrimoine de Zety au 1 octobre 2024: " + valeurComptableManqueDEspeces + " Ar");
 
-
+        // Calculer la valeur attendue en prenant en compte le manque d'espèces
         int expectedValeurComptableManqueDEspeces = 2_978_848 + montantDette;
 
         assertEquals(expectedValeurComptableManqueDEspeces, valeurComptableManqueDEspeces);
