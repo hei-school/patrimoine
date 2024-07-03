8262ec1a58a0d3e0e00af3c6a077873244746022
RASOANIRINA Mialisoa Lisa <hei.lisa.30@gmail.com>
2024-07-03 12:35:59 +0300

    feat(possession): add currency

--- src/main/java/school/hei/patrimoine/modele/possession/Possession.java
+++ src/main/java/school/hei/patrimoine/modele/possession/Possession.java
@@ -6,21 +6,42 @@ import lombok.Getter;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Currency;
 
 @EqualsAndHashCode
-@AllArgsConstructor
 @Getter
-public sealed abstract class Possession implements Serializable /*note(no-serializable)*/ permits
-    Argent, FluxArgent, TransfertArgent,
-    Materiel, AchatMaterielAuComptant,
-    GroupePossession {
+
+public abstract sealed class Possession implements Serializable permits
+        Argent, FluxArgent, TransfertArgent,
+        Materiel, AchatMaterielAuComptant,
+        GroupePossession {
+
   protected final String nom;
   protected final LocalDate t;
   protected final int valeurComptable;
+  protected final String devise;
+
+  public Possession(String nom, LocalDate t, int valeurComptable, String devise) {
+    this.nom = nom;
+    this.t = t;
+    this.valeurComptable = valeurComptable;
+    this.devise = devise;
+  }
+
+  public int getValeurComptable() {
+    return valeurComptable;
+  }
+
+  public int getValeurComptableEnDevise(String codeDevise, double tauxChange, double tauxAppreciationAnnuel) {
+    double tauxChangeLisse = Math.pow(1 + tauxAppreciationAnnuel, ChronoUnit.DAYS.between(LocalDate.now(), t));
+    return (int) Math.round(valeurComptable * tauxChange * tauxChangeLisse);
+  }
+
+  public abstract Possession projectionFuture(LocalDate tFutur);
 
   public final int valeurComptableFuture(LocalDate tFutur) {
     return projectionFuture(tFutur).getValeurComptable();
   }
 
-  public abstract Possession projectionFuture(LocalDate tFutur);
 }
