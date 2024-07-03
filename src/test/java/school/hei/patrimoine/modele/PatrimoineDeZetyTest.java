64c9e83771116e197af31d6562c2ac603540cd1f
Artahal01 <hei.lahatra@gmail.com>
2024-07-03 12:17:11 +0300

    feat : update a file to test Zety's patrimoine for question 3 and 4

--- src/test/java/school/hei/patrimoine/modele/PatrimoineDeZetyTest.java
+++ src/test/java/school/hei/patrimoine/modele/PatrimoineDeZetyTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.possession.*;
 
 import java.time.LocalDate;
+import java.util.Currency;
 import java.util.Set;
 
 import static java.time.Month.*;
@@ -36,15 +37,50 @@ public class PatrimoineDeZetyTest {
         LocalDate dateEvaluation = LocalDate.of(2024, 9, 17);
         int valeurPatrimoine = zetyPatrimoine.projectionFuture(dateEvaluation).getValeurComptable();
 
-        assertEquals(515_500, valeurPatrimoine);
+        assertEquals(2_978_848, valeurPatrimoine);
         /*FIN QUESTION 1*/
 
         //QUESTION 2
         LocalDate au18Sept24 = LocalDate.of(2024, 9, 18);
-        int valeurPatrimoine2 = zetyPatrimoine.projectionFuture(au18Sept24).getValeurComptable();
+        int valeurPatrimoine2 = valeurPatrimoine - 1_000_000; //qui est le cout du pret
 
-        assertEquals( 515_500, valeurPatrimoine2);
+        assertEquals(1_978_848, valeurPatrimoine);
+
         /*FIN QUESTION 2*/
-    }
 
+        //QUESTION 3
+        FluxArgent donEspece = new FluxArgent("Don de la part des parents de Zety",
+                new Argent("Espece", LocalDate.of(2024, JANUARY, 1), 100_000), LocalDate.of(2024, JANUARY, 1), LocalDate.of(2024, AUGUST, 27), 100_000, 15);
+        FluxArgent trainDeVie = new FluxArgent("Train de Vie",
+                new Argent("Train de Vie", LocalDate.of(2024, OCTOBER, 1), 250_000), LocalDate.of(2024, OCTOBER, 1), LocalDate.of(2025, FEBRUARY, 13), -250_000, 1);
+
+        /*FIN QUESTION 3*/
+
+        //QUESTION 4
+        // Projection du patrimoine au 14 février 2025
+        LocalDate au03juillet24 = LocalDate.of(2024, 7, 3);
+        LocalDate dateEvaluation2 = LocalDate.of(2025, 2, 14);
+
+
+        Argent argentEspeces = new Argent("Argent en espèces", au03juillet24, 800_000);
+        Argent compteBancaire = new Argent("Compte bancaire", au03juillet24, 100_000);
+        FluxArgent fraisCompte = new FluxArgent("Frais de compte", compteBancaire, LocalDate.of(2024, 7, 25), LocalDate.MAX, -20_000, 25);
+
+
+        Set<Possession> possessions = Set.of(ordinateur, vetements, argentEspeces, compteBancaire, fraisScolarite, fraisCompte);
+        Personne possesseur = new Personne("Zety");
+        Patrimoine patrimoineDeZety = new Patrimoine("Patrimoine de Zety", possesseur, au03juillet24, possessions);
+
+
+        double valeurComptable = patrimoineDeZety.projectionFuture(dateEvaluation2).getValeurComptable();
+
+        assertEquals(2_521_314, valeurComptable);
+
+
+        Currency euro = Currency.getInstance("EUR");
+        Currency ar = Currency.getInstance("MGA");
+        double tauxChange = 4821;
+        double tauxAppreciation = -0.10;
+
+    }
 }
