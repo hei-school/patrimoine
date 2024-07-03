b88310befd7757c780697727e2e9f9aa399af5c4
HRojotiana <hei.rojotiana@gmail.com>
2024-07-03 11:54:20 +0300

    chore: test evolution patrimoine 17 to 18 september

--- src/test/java/school/hei/patrimoine/modele/EvolutionPatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/modele/EvolutionPatrimoineTest.java
@@ -1,13 +1,12 @@
 package school.hei.patrimoine.modele;
 
 import org.junit.jupiter.api.Test;
-import school.hei.patrimoine.modele.possession.Argent;
-import school.hei.patrimoine.modele.possession.FluxArgent;
+import school.hei.patrimoine.modele.possession.*;
 
 import java.time.LocalDate;
 import java.util.Set;
 
-import static java.time.Month.MAY;
+import static java.time.Month.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EvolutionPatrimoineTest {
@@ -40,5 +39,30 @@ class EvolutionPatrimoineTest {
     assertEquals(500_000, evolutionJournaliere.get(LocalDate.of(2024, MAY, 15)).getValeurComptable());
     assertEquals(500_000, evolutionJournaliere.get(LocalDate.of(2024, MAY, 16)).getValeurComptable());
     assertEquals(500_000, evolutionJournaliere.get(LocalDate.of(2024, MAY, 17)).getValeurComptable());
+  }
+
+  @Test
+  void patrimoine_de_zety_apres_dette(){
+    var zety = new Personne("Zety");
+    var au3Juillet2024 = LocalDate.of(2024, JULY, 3);
+    var au17Septembre2024 = LocalDate.of(20224, SEPTEMBER, 17);
+    var au18Septembre2024 = LocalDate.of(2024, SEPTEMBER, 18);
+
+    var ordinateur = new Materiel("Ordinateur", au3Juillet2024, 1_200_000, au3Juillet2024, -0.10);
+    var vêtements = new Materiel("vêtements", au3Juillet2024, 1_500_000, au3Juillet2024, -0.50);
+
+    var argentEnEspèces = new Argent("Espèces", au3Juillet2024, 800_000);
+    var compteBancaire = new Argent("Compte bancaire", au3Juillet2024, 100_000);
+    var fraisDeScolarité = new FluxArgent("Frais de scolarité", argentEnEspèces, LocalDate.of(2023, NOVEMBER, 27), LocalDate.of(2024, AUGUST, 27), -200_000, 27);
+    var fraisCompteBancaire = new FluxArgent("frais compte bancaire", compteBancaire, au3Juillet2024, au17Septembre2024, -20_000, 25);
+
+    var dette = new Dette("dette", au18Septembre2024, -11_000_000);
+
+    var patrimoineDeZetyAu3Juillet2024 = new Patrimoine("patrimoine de zety au 3 juillet 2024",
+            zety,
+            au3Juillet2024,
+            Set.of(ordinateur, vêtements, argentEnEspèces, compteBancaire, fraisDeScolarité, fraisCompteBancaire, dette));
+
+    assertEquals(11002384, patrimoineDeZetyAu3Juillet2024.projectionFuture(LocalDate.of(2024, SEPTEMBER, 17)).getValeurComptable() - patrimoineDeZetyAu3Juillet2024.projectionFuture(au18Septembre2024).getValeurComptable());
   }
 }
