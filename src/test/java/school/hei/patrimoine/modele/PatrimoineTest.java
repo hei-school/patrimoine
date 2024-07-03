f0479ad6aa317e7db87c2002fa38bfa52df1aae3
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-07-03 09:17:04 +0300

    test: calcul le patrimoine de Zety au 17 septembre 2024

--- src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.possession.Argent;
 import school.hei.patrimoine.modele.possession.FluxArgent;
 import school.hei.patrimoine.modele.possession.GroupePossession;
+import school.hei.patrimoine.modele.possession.Materiel;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 import static java.time.Month.MAY;
+import static java.util.Calendar.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PatrimoineTest {
@@ -84,4 +86,47 @@ class PatrimoineTest {
     assertEquals(200_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plusDays(100)).getValeurComptable());
     assertEquals(200_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plusDays(1_000)).getValeurComptable());
   }
+
+  @Test
+  void patrimoine_de_zety_au_17_septembre_2024() {
+    var zety = new Personne("Zety");
+    var dateActuelle = LocalDate.of(2024, 7, 3);
+
+    var ordinateur = new Materiel("Ordinateur", dateActuelle, 1_200_000, dateActuelle, -0.10);
+    var vetements = new Materiel("Vêtements", dateActuelle, 1_500_000, dateActuelle, -0.50);
+    var especes = new Argent("Espèces", dateActuelle, 800_000);
+
+    var dateDebutScolarite = LocalDate.of(2023, 11, 27);
+    var dateFinScolarite = LocalDate.of(2024, 8, 27);
+    var fraisScolarite = new FluxArgent(
+            "Frais de scolarité",
+            especes,
+            dateDebutScolarite,
+            dateFinScolarite,
+            -200_000,
+            27);
+
+    var compteBancaire = new Argent("Compte bancaire", dateActuelle, 100_000);
+    var fraisCompte = new FluxArgent(
+            "Frais de tenue de compte",
+            compteBancaire,
+            dateActuelle.minusMonths(1),
+            LocalDate.MAX,
+            -20_000,
+            25);
+
+    var patrimoineZety = new Patrimoine(
+            "Patrimoine de Zety",
+            zety,
+            dateActuelle,
+            Set.of(ordinateur, vetements, especes, fraisScolarite, compteBancaire, fraisCompte));
+
+    var dateFutur = LocalDate.of(2024, 9, 17);
+    var patrimoineFuture = patrimoineZety.projectionFuture(dateFutur);
+
+    int valeurTotaleAttendue = 2978848;
+
+    assertEquals(valeurTotaleAttendue, patrimoineFuture.getValeurComptable());
+  }
+
 }
