2c8959d682789aebc2e6f7f133ad50b592428e70
RASOANIRINA Mialisoa Lisa <hei.lisa.30@gmail.com>
2024-07-03 12:31:22 +0300

    feat(Patrimoine): add methode getValeurComptableEnDevise, convertirEnDevise

--- src/main/java/school/hei/patrimoine/modele/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/modele/Patrimoine.java
@@ -1,7 +1,5 @@
 package school.hei.patrimoine.modele;
-
 import school.hei.patrimoine.modele.possession.Possession;
-
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Set;
@@ -9,19 +7,33 @@ import java.util.Set;
 import static java.util.stream.Collectors.toSet;
 
 public record Patrimoine(
-    String nom, Personne possesseur, LocalDate t, Set<Possession> possessions)
-    implements Serializable/*note(no-serializable)*/ {
+        String nom, Personne possesseur, LocalDate t, Set<Possession> possessions)
+        implements Serializable {
 
   public int getValeurComptable() {
     return possessions.stream().mapToInt(Possession::getValeurComptable).sum();
   }
 
+  public int getValeurComptableEnDevise(String devise, double tauxChange, double appreciationAnnuelle) {
+    int valeurComptableTotale = 0;
+    for (Possession possession : possessions) {
+      int valeur = possession.getValeurComptable();
+      valeurComptableTotale += convertirEnDevise(valeur, devise, tauxChange, appreciationAnnuelle);
+    }
+    return valeurComptableTotale;
+  }
+
+  private int convertirEnDevise(int valeur, String devise, double tauxChange, double appreciationAnnuelle) {
+    double tauxJournalier = tauxChange * Math.pow(1 + appreciationAnnuelle / 365, LocalDate.now().until(t).getDays());
+    return (int) Math.round(valeur / tauxJournalier);
+  }
+
   public Patrimoine projectionFuture(LocalDate tFutur) {
     return new Patrimoine(
-        nom,
-        possesseur,
-        tFutur,
-        possessions.stream().map(p -> p.projectionFuture(tFutur)).collect(toSet()));
+            nom,
+            possesseur,
+            tFutur,
+            possessions.stream().map(p -> p.projectionFuture(tFutur)).collect(toSet()));
   }
 
   public Possession possessionParNom(String nom) {
