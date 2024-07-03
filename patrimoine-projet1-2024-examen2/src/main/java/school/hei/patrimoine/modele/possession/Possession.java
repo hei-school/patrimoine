fd8e27a51962089d3b1e463034093efb606079e6
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2024-07-03 12:56:44 +0300

    refactor: possession and materiel

--- patrimoine-projet1-2024-examen2/src/main/java/school/hei/patrimoine/modele/possession/Possession.java
+++ patrimoine-projet1-2024-examen2/src/main/java/school/hei/patrimoine/modele/possession/Possession.java
@@ -17,7 +17,6 @@ public sealed abstract class Possession implements Serializable /*note(no-serial
   protected final String nom;
   protected final LocalDate t;
   protected final int valeurComptable;
-  protected final String devise;
 
 
   public final int valeurComptableFuture(LocalDate tFutur) {
@@ -26,12 +25,4 @@ public sealed abstract class Possession implements Serializable /*note(no-serial
 
   public abstract Possession projectionFuture(LocalDate tFutur);
 
-  public double convertValeurComptable(String targetDevise, double tauxChange, double tauxAppreciation, long jours) {
-    if (this.devise.equals(targetDevise)) {
-      return valeurComptable;
-    } else {
-      double tauxConversion = tauxChange * Math.pow(1 + tauxAppreciation, jours / 365.0);
-      return valeurComptable / tauxConversion;
-    }
-  }
 }
