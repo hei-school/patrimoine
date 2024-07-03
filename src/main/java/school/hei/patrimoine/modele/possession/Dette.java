4968200806ef8468b589f745c9cface7d288a8fe
HarivolaANDY <hei.andy.43@gmail.com>
2024-07-03 13:03:07 +0300

    chore: modify Dette

--- src/main/java/school/hei/patrimoine/modele/possession/Dette.java
+++ src/main/java/school/hei/patrimoine/modele/possession/Dette.java
@@ -3,9 +3,20 @@ package school.hei.patrimoine.modele.possession;
 import java.time.LocalDate;
 
 public final class Dette extends Argent {
-  public Dette(String nom, LocalDate t, int valeurComptable) {
-    super(nom, t, valeurComptable);
-    if (valeurComptable > 0) {
+  private final double montantEUR;
+  private final double tauxDeChangeEURtoAr;
+
+  public Dette(String nom, LocalDate dateOuverture, LocalDate date, double montantEUR, double tauxDeChangeEURtoAr) {
+    super(nom, dateOuverture, date, 0); // La dette est initialement à 0, le montant est géré par les flux
+    this.montantEUR = montantEUR;
+    this.tauxDeChangeEURtoAr = tauxDeChangeEURtoAr;
+  }
+
+  @Override
+  public int getValeurComptableDevise(String devise) {
+    if ("Ar".equals(devise)) {
+      return (int) (montantEUR * tauxDeChangeEURtoAr);
+    } else {
       throw new IllegalArgumentException();
     }
   }
