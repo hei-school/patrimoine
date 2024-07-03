4bb8142b23d66e034eb80afe755db7016f34f3e2
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-07-03 09:11:28 +0300

    test: PatrimoineTest Zety

--- src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
@@ -1,14 +1,12 @@
 package school.hei.patrimoine.modele;
 
 import org.junit.jupiter.api.Test;
-import school.hei.patrimoine.modele.possession.Argent;
-import school.hei.patrimoine.modele.possession.FluxArgent;
-import school.hei.patrimoine.modele.possession.GroupePossession;
+import school.hei.patrimoine.modele.possession.*;
 
 import java.time.LocalDate;
 import java.util.Set;
 
-import static java.time.Month.MAY;
+import static java.time.Month.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PatrimoineTest {
@@ -18,10 +16,10 @@ class PatrimoineTest {
     var ilo = new Personne("Ilo");
 
     var patrimoineIloAu13mai24 = new Patrimoine(
-        "patrimoineIloAu13mai24",
-        ilo,
-        LocalDate.of(2024, MAY, 13),
-        Set.of());
+            "patrimoineIloAu13mai24",
+            ilo,
+            LocalDate.of(2024, MAY, 13),
+            Set.of());
 
     assertEquals(0, patrimoineIloAu13mai24.getValeurComptable());
   }
@@ -32,13 +30,13 @@ class PatrimoineTest {
 
     var au13mai24 = LocalDate.of(2024, MAY, 13);
     var patrimoineIloAu13mai24 = new Patrimoine(
-        "patrimoineIloAu13mai24",
-        ilo,
-        au13mai24,
-        Set.of(
-            new Argent("Espèces", au13mai24, 400_000),
-            new Argent("Compte epargne", au13mai24, 200_000),
-            new Argent("Compte courant", au13mai24, 600_000)));
+            "patrimoineIloAu13mai24",
+            ilo,
+            au13mai24,
+            Set.of(
+                    new Argent("Espèces", au13mai24, 400_000),
+                    new Argent("Compte epargne", au13mai24, 200_000),
+                    new Argent("Compte courant", au13mai24, 600_000)));
 
     assertEquals(1_200_000, patrimoineIloAu13mai24.getValeurComptable());
   }
@@ -49,15 +47,15 @@ class PatrimoineTest {
     var au13mai24 = LocalDate.of(2024, MAY, 13);
     var financeur = new Argent("Espèces", au13mai24, 600_000);
     var trainDeVie = new FluxArgent(
-        "Vie courante",
-        financeur, au13mai24.minusDays(100), au13mai24.plusDays(100), -100_000,
-        15);
+            "Vie courante",
+            financeur, au13mai24.minusDays(100), au13mai24.plusDays(100), -100_000,
+            15);
 
     var patrimoineIloAu13mai24 = new Patrimoine(
-        "patrimoineIloAu13mai24",
-        ilo,
-        au13mai24,
-        Set.of(financeur, trainDeVie));
+            "patrimoineIloAu13mai24",
+            ilo,
+            au13mai24,
+            Set.of(financeur, trainDeVie));
 
     assertEquals(500_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plusDays(10)).getValeurComptable());
     assertEquals(200_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plusDays(100)).getValeurComptable());
@@ -70,18 +68,46 @@ class PatrimoineTest {
     var au13mai24 = LocalDate.of(2024, MAY, 13);
     var financeur = new Argent("Espèces", au13mai24, 600_000);
     var trainDeVie = new FluxArgent(
-        "Vie courante",
-        financeur, au13mai24.minusDays(100), au13mai24.plusDays(100), -100_000,
-        15);
+            "Vie courante",
+            financeur, au13mai24.minusDays(100), au13mai24.plusDays(100), -100_000,
+            15);
 
     var patrimoineIloAu13mai24 = new Patrimoine(
-        "patrimoineIloAu13mai24",
-        ilo,
-        au13mai24,
-        Set.of(new GroupePossession("Le groupe", au13mai24, Set.of(financeur, trainDeVie))));
+            "patrimoineIloAu13mai24",
+            ilo,
+            au13mai24,
+            Set.of(new GroupePossession("Le groupe", au13mai24, Set.of(financeur, trainDeVie))));
 
     assertEquals(500_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plusDays(10)).getValeurComptable());
     assertEquals(200_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plusDays(100)).getValeurComptable());
     assertEquals(200_000, patrimoineIloAu13mai24.projectionFuture(au13mai24.plusDays(1_000)).getValeurComptable());
+  }
+
+  @Test
+  void valeur_patrimoine_zety_le_17_septembre_2024() {
+    var zety = new Personne("Zety");
+    var au3juillet24 = LocalDate.of(2024, JULY, 3);
+    var au17septembre24 = LocalDate.of(2024, SEPTEMBER, 17);
+
+    var ordinateur = new Materiel("Ordinateur", au3juillet24, 1_200_000, au3juillet24, -0.10);
+    var vetements = new Materiel("Vêtements", au3juillet24, 1_500_000, au3juillet24, -0.50);
+    var argentEspeces = new Argent("Espèces", au3juillet24, 800_000);
+
+    var fraisScolarite = new FluxArgent(
+            "Frais de scolarité", argentEspeces, LocalDate.of(2023, NOVEMBER, 27),
+            LocalDate.of(2024, AUGUST, 27), -200_000, 27);
+
+    var compteBancaire = new Argent("Compte bancaire", au3juillet24, 100_000);
+    var fraisTenueCompte = new FluxArgent(
+            "Frais de tenue de compte", compteBancaire, au3juillet24.withDayOfMonth(25),
+            LocalDate.of(2024, DECEMBER, 25), -20_000, 25);
+
+    var patrimoineZetyAu3juillet24 = new Patrimoine(
+            "patrimoineZetyAu3juillet24",
+            zety,
+            au3juillet24,
+            Set.of(ordinateur, vetements, argentEspeces, fraisScolarite, compteBancaire, fraisTenueCompte));
+
+    assertEquals(2978848, patrimoineZetyAu3juillet24.projectionFuture(au17septembre24).getValeurComptable());
   }
 }
