727a5e56182a0fcc848105ee5af433968bba5f13
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-07-03 10:17:19 +0300

    test: test valeur vetements

--- src/test/java/school/hei/patrimoine/Zety/Zety_etudie_en_2023_2024.java
+++ src/test/java/school/hei/patrimoine/Zety/Zety_etudie_en_2023_2024.java
@@ -23,5 +23,19 @@ class test_zety_etudie_en_2023_2024 {
         assertEquals(1_173_961, Math.round(valeurFinale));
     }
 
+    @Test
+    void testVetements() {
+        var dateInitiale = LocalDate.of(2024, JULY, 3);
+        var dateCible = LocalDate.of(2024, SEPTEMBER, 17);
+        var nombreJours = ChronoUnit.DAYS.between(dateInitiale, dateCible);
+        var valeurInitiale = 1_500_000;
+        var tauxAppréciationJournalier = Math.pow(1 - 0.5, 1.0 / 365);
+
+        var valeurFinale = valeurInitiale * Math.pow(tauxAppréciationJournalier, nombreJours);
+
+        System.out.println("Valeur finale calculée : " + valeurFinale);
+        assertEquals(1_298_408, Math.round(valeurFinale));
+    }
+
 
     }
