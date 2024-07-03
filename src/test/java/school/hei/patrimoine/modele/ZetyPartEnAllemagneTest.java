8872ad5244daa4f6225b78439d7500be54baee4a
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-07-03 12:07:01 +0300

    test: fix: change the value of assets of zety to 2_186_300

--- src/test/java/school/hei/patrimoine/modele/ZetyPartEnAllemagneTest.java
+++ src/test/java/school/hei/patrimoine/modele/ZetyPartEnAllemagneTest.java
@@ -2,9 +2,11 @@ package school.hei.patrimoine.modele;
 
 import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.possession.Argent;
+import school.hei.patrimoine.modele.possession.FluxArgent;
 import school.hei.patrimoine.modele.possession.Materiel;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -41,7 +43,25 @@ public class ZetyPartEnAllemagneTest {
                 LocalDate.of(2024, 7, 3),
                 100_000
         );
+        var fluxArgentFraisDeScolarite = new FluxArgent(
+                "frais de scolarité",
+                argentEnEspece,
+                LocalDate.of(2024, Month.NOVEMBER, 1),
+                LocalDate.of(2024, Month.AUGUST, 31),
+                -200_000,
+                27
+        );
 
+        var compteBancaire = new Argent("compte bancaire", LocalDate.of(2024, 7, 3), 100_000);
+
+        var fluxArgentCompteBancaire = new FluxArgent(
+                "compte bancaire",
+                compteBancaire,
+                LocalDate.of(2024, 7, 3),
+                LocalDate.now().plusYears(10),
+                -20_000,
+                25
+        );
         var date3Juillet2024 = LocalDate.of(2024, 7, 3);
 
         var date14Fevrier2025 = LocalDate.of(2025, 2, 14);
@@ -49,14 +69,11 @@ public class ZetyPartEnAllemagneTest {
                 "patrimoine_de_zety",
                 zety,
                 date3Juillet2024,
-                Set.of(ordinateur, vetement, argentEnEspece, compteEnBanque)
+                Set.of(ordinateur, vetement, fluxArgentCompteBancaire, fluxArgentFraisDeScolarite)
         );
 
-        assertEquals(3_086_300, patrimoine_de_zety.projectionFuture(date14Fevrier2025).getValeurComptable());
+        assertEquals(2_186_300, patrimoine_de_zety.projectionFuture(date14Fevrier2025).getValeurComptable());
     }
 
-    @Test
-    void la_valeur_du_patrimoine_un_quelconque_devise(){
 
-    }
 }
