9bc6044147efd00b62940b5c61e51d5d1c2258ec
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-07-03 09:44:10 +0200

    fix: Test:Patrimoine zety

--- src/test/java/school/hei/patrimoine/modele/Zety/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/modele/Zety/PatrimoineTest.java
@@ -4,35 +4,66 @@ import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.Patrimoine;
 import school.hei.patrimoine.modele.Personne;
 import school.hei.patrimoine.modele.possession.Argent;
+import school.hei.patrimoine.modele.possession.Materiel;
+import school.hei.patrimoine.modele.possession.FluxArgent;
+import school.hei.patrimoine.modele.possession.Possession;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
-import static java.time.LocalDate.now;
 import static java.time.Month.SEPTEMBER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PatrimoineTest {
+
     @Test
-    // Test patrimoine zety 17-09-24
-    void patrimoine_zety(){
+    void patrimoine_zety() {
+        // Définition des données au 3 juillet 2024
         var zety = new Personne("Zety");
-        var au17Sept24 = LocalDate.of(2024, SEPTEMBER, 17);
+        var au3Juillet24 = LocalDate.of(2024, 7, 3);
 
-        // Définir les montants réels d'espèces et de compte bancaire de Zety le 17 septembre 2024
-        var montantEspece = 400_000;
-        var montantCompteBancaire = 40_000;
+        // Création des possessions de Zety au 3 juillet 2024
+        Set<Possession> possessions = new HashSet<>();
 
+        // Ordinateur
+        Materiel ordinateur = new Materiel("Ordinateur", au3Juillet24, 1_200_000, au3Juillet24, -0.10);
+        possessions.add(ordinateur);
+
+        // Vêtements
+        Materiel vetements = new Materiel("Vêtements", au3Juillet24, 1_500_000, au3Juillet24, -0.50);
+        possessions.add(vetements);
+
+        // Espèces
+        possessions.add(new Argent("Espèces", au3Juillet24, 800_000));
+
+        // Compte bancaire
+        possessions.add(new Argent("Compte bancaire", au3Juillet24, 100_000));
+
+        // Simulation des frais de scolarité de novembre 2023 à août 2024
+        FluxArgent fraisScolarite = new FluxArgent(
+                "Frais de scolarité",
+                new Argent("Espèces", au3Juillet24, 0),
+                LocalDate.of(2023, 11, 27),
+                LocalDate.of(2024, 8, 27),
+                -200_000, 27);
+
+        // Ajout des frais de scolarité à la liste des possessions
+        possessions.add(fraisScolarite);
+
+        // Calcul de la valeur comptable au 17 septembre 2024
+        LocalDate au17Sept24 = LocalDate.of(2024, SEPTEMBER, 17);
+        int valeurComptableAttendue =3600000;
+
+        // Création du patrimoine de Zety au 17 septembre 2024
         var patrimoineZetyAu17Sept24 = new Patrimoine(
                 "patrimoineZetyAu17Sept24",
                 zety,
                 au17Sept24,
-                Set.of(
-                        new Argent("Espèces", au17Sept24, montantEspece),
-                        new Argent("Compte bancaire", au17Sept24, montantCompteBancaire))
+                possessions
         );
-        assertEquals(440000, patrimoineZetyAu17Sept24.getValeurComptable());
+
+        // Vérification
+        assertEquals(valeurComptableAttendue, patrimoineZetyAu17Sept24.getValeurComptable());
     }
-
-
 }
