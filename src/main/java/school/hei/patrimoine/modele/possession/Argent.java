d57b84a3a19582efac6602ce440a51827a01fff8
Eclisher <hei.harizo@gmail.com>
2024-07-03 09:43:09 +0300

    chore: add the diminutionPatrimoineEntre17Et18Septembre2024()::PatrimoineTest

--- src/main/java/school/hei/patrimoine/modele/possession/Argent.java
+++ src/main/java/school/hei/patrimoine/modele/possession/Argent.java
@@ -1,51 +1,51 @@
 package school.hei.patrimoine.modele.possession;
 
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+  import java.time.LocalDate;
+  import java.util.HashSet;
+  import java.util.Set;
 
-import static java.util.stream.Collectors.toSet;
+  import static java.util.stream.Collectors.toSet;
 
-public sealed class Argent extends Possession permits Dette, Creance {
-  private final LocalDate dateOuverture;
-  private final Set<FluxArgent> fluxArgents;
+  public sealed class Argent extends Possession permits Dette, Creance {
+    private final LocalDate dateOuverture;
+    private final Set<FluxArgent> fluxArgents;
 
-  public Argent(String nom, LocalDate t, int valeurComptable) {
-    this(nom, t, t, valeurComptable);
-  }
-
-  public Argent(String nom, LocalDate dateOuverture, LocalDate t, int valeurComptable) {
-    this(nom, dateOuverture, t, valeurComptable, new HashSet<>());
-  }
-
-  private Argent(String nom, LocalDate dateOuverture, LocalDate t, int valeurComptable, Set<FluxArgent> fluxArgents) {
-    super(nom, t, valeurComptable);
-    this.fluxArgents = fluxArgents;
-    this.dateOuverture = dateOuverture;
-  }
-
-  @Override
-  public Argent projectionFuture(LocalDate tFutur) {
-    if (tFutur.isBefore(dateOuverture)) {
-      return new Argent(nom, tFutur, 0);
+    public Argent(String nom, LocalDate t, int valeurComptable) {
+      this(nom, t, t, valeurComptable);
     }
 
-    return new Argent(
-        nom,
-        dateOuverture,
-        tFutur,
-        valeurComptable - financementsFutur(tFutur),
-        fluxArgents.stream().map(f -> f.projectionFuture(tFutur)).collect(toSet()));
-  }
+    public Argent(String nom, LocalDate dateOuverture, LocalDate t, int valeurComptable) {
+      this(nom, dateOuverture, t, valeurComptable, new HashSet<>());
+    }
 
-  private int financementsFutur(LocalDate tFutur) {
-    return fluxArgents.stream().
-        mapToInt(
-            f -> valeurComptable - f.projectionFuture(tFutur).getArgent().getValeurComptable())
-        .sum();
-  }
+    private Argent(String nom, LocalDate dateOuverture, LocalDate t, int valeurComptable, Set<FluxArgent> fluxArgents) {
+      super(nom, t, valeurComptable);
+      this.fluxArgents = fluxArgents;
+      this.dateOuverture = dateOuverture;
+    }
 
-  void addFinancés(FluxArgent fluxArgent) {
-    fluxArgents.add(fluxArgent);
+    @Override
+    public Argent projectionFuture(LocalDate tFutur) {
+      if (tFutur.isBefore(dateOuverture)) {
+        return new Argent(nom, tFutur, 0);
+      }
+
+      return new Argent(
+          nom,
+          dateOuverture,
+          tFutur,
+          valeurComptable - financementsFutur(tFutur),
+          fluxArgents.stream().map(f -> f.projectionFuture(tFutur)).collect(toSet()));
+    }
+
+    private int financementsFutur(LocalDate tFutur) {
+      return fluxArgents.stream().
+          mapToInt(
+              f -> valeurComptable - f.projectionFuture(tFutur).getArgent().getValeurComptable())
+          .sum();
+    }
+
+    void addFinancés(FluxArgent fluxArgent) {
+      fluxArgents.add(fluxArgent);
+    }
   }
-}
--- src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
+++ src/test/java/school/hei/patrimoine/modele/PatrimoineTest.java
@@ -1,10 +1,7 @@
 package school.hei.patrimoine.modele;
 
 import org.junit.jupiter.api.Test;
-import school.hei.patrimoine.modele.possession.Argent;
-import school.hei.patrimoine.modele.possession.FluxArgent;
-import school.hei.patrimoine.modele.possession.GroupePossession;
-import school.hei.patrimoine.modele.possession.Materiel;
+import school.hei.patrimoine.modele.possession.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -138,5 +135,55 @@ class PatrimoineTest {
 
     var valeurTotaleAttendue = valeurOrdinateurAu17Septembre2024 + valeurVetementsAu17Septembre2024 + valeurEspecesAu17Septembre2024 + valeurCompteBancaireAu17Septembre2024;
     assertEquals(valeurTotaleAttendue, valeurPatrimoineAu17Septembre2024);
+  }
+
+// dimunition de la valeur du  patrimoine de Zety entre le 17 et le 18 septembre 2024
+  @Test
+  void diminutionPatrimoineEntre17Et18Septembre2024() {
+    var zety = new Personne("Zety");
+    var au3juillet2024 = LocalDate.of(2024, Month.JULY, 3);
+    var ordinateur = new Materiel(
+            "Ordinateur",
+            au3juillet2024,
+            1_200_000,
+            au3juillet2024.minusDays(2),
+            -0.10);
+
+    var vetements = new Materiel(
+            "Vêtements",
+            au3juillet2024,
+            1_500_000,
+            au3juillet2024.minusDays(2),
+            -0.50);
+
+    var argentEspeces = new Argent("Espèces", au3juillet2024, 800_000);
+    var fraisScolarite = new FluxArgent(
+            "Frais de scolarité",
+            argentEspeces,
+            LocalDate.of(2023, Month.NOVEMBER, 27),
+            LocalDate.of(2024, Month.AUGUST, 27),
+            -200_000,
+            30);
+
+    var compteBancaire = new Argent("Compte bancaire", au3juillet2024, 100_000);
+
+    var fraisTenueCompte = new FluxArgent(
+            "Frais de tenue de compte",
+            compteBancaire,
+            au3juillet2024.minusMonths(1),
+            au3juillet2024.plusYears(1),
+            -20_000,
+            30);
+
+    var patrimoineZety = new Patrimoine(
+            "Patrimoine de Zety",
+            zety,
+            au3juillet2024,
+            Set.of(ordinateur, vetements, argentEspeces, fraisScolarite, compteBancaire, fraisTenueCompte));
+    var valeurPatrimoineAu17Septembre2024 = patrimoineZety.projectionFuture(LocalDate.of(2024, Month.SEPTEMBER, 17)).getValeurComptable();
+    var valeurPatrimoineAu18Septembre2024 = patrimoineZety.projectionFuture(LocalDate.of(2024, Month.SEPTEMBER, 18)).getValeurComptable();
+    var diminutionPatrimoine = valeurPatrimoineAu18Septembre2024 - valeurPatrimoineAu17Septembre2024;
+
+    assertEquals(-2384, diminutionPatrimoine);
   }
 }
