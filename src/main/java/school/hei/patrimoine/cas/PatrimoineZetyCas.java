f70a368489b0041068d3490803abbbeb76bc3ca5
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-07-03 10:34:33 +0200

    feat: Zety:: add indebtedness and test

--- src/main/java/school/hei/patrimoine/cas/PatrimoineZetyCas.java
+++ src/main/java/school/hei/patrimoine/cas/PatrimoineZetyCas.java
@@ -2,11 +2,10 @@ package school.hei.patrimoine.cas;
 
 import school.hei.patrimoine.modele.Patrimoine;
 import school.hei.patrimoine.modele.Personne;
-import school.hei.patrimoine.modele.possession.Argent;
-import school.hei.patrimoine.modele.possession.FluxArgent;
-import school.hei.patrimoine.modele.possession.Materiel;
-import school.hei.patrimoine.modele.possession.Possession;
+import school.hei.patrimoine.modele.possession.*;
+
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Supplier;
 import static java.time.LocalDate.now;
@@ -44,4 +43,44 @@ public class PatrimoineZetyCas implements Supplier<Patrimoine> {
                 ajd,
                 possessionsZety.stream().collect(toSet()));
     }
+
+    public static Patrimoine patrimoineZetyAvecEndettement() {
+        var zety = new Personne("Zety");
+        var au3Juillet24 = LocalDate.of(2024, 7, 3);
+        Set<Possession> possessions = new HashSet<>();
+        Materiel ordinateur = new Materiel("Ordinateur", au3Juillet24, 1_200_000, au3Juillet24, -0.10);
+        possessions.add(ordinateur);
+
+        Materiel vetements = new Materiel("Vêtements", au3Juillet24, 1_500_000, au3Juillet24, -0.50);
+        possessions.add(vetements);
+        possessions.add(new Argent("Espèces", au3Juillet24, 800_000));
+        possessions.add(new Argent("Compte bancaire", au3Juillet24, 100_000));
+
+        LocalDate au17Sept24 = LocalDate.of(2024, 9, 17);
+        var patrimoineZetyAu17Sept24 = new Patrimoine(
+                "patrimoineZetyAu17Sept24",
+                zety,
+                au17Sept24,
+                possessions
+        );
+
+        LocalDate au18Sept24 = LocalDate.of(2024, 9, 18);
+        Dette endettementZety = new Dette(
+                "Endettement",
+                au18Sept24,
+                -11_000_000
+        );
+        possessions.add(endettementZety);
+
+        patrimoineZetyAu17Sept24 = new Patrimoine(
+                patrimoineZetyAu17Sept24.nom(),
+                patrimoineZetyAu17Sept24.possesseur(),
+                au18Sept24,
+                possessions
+        );
+
+        return patrimoineZetyAu17Sept24;
+    }
+
+
 }
