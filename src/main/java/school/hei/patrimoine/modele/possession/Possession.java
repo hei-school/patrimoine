832a1787ffdb38b0ffd42ce1ae1010d7ef7ac7a2
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-07-03 11:19:02 +0200

    feat: Convert devise

--- src/main/java/school/hei/patrimoine/modele/possession/Possession.java
+++ src/main/java/school/hei/patrimoine/modele/possession/Possession.java
@@ -6,21 +6,35 @@ import lombok.Getter;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Map;
 
 @EqualsAndHashCode
 @AllArgsConstructor
 @Getter
 public sealed abstract class Possession implements Serializable /*note(no-serializable)*/ permits
-    Argent, FluxArgent, TransfertArgent,
-    Materiel, AchatMaterielAuComptant,
-    GroupePossession {
+        Argent, FluxArgent, TransfertArgent,
+        Materiel, AchatMaterielAuComptant,
+        GroupePossession {
   protected final String nom;
   protected final LocalDate t;
   protected final int valeurComptable;
+  protected final String devise;
 
   public final int valeurComptableFuture(LocalDate tFutur) {
     return projectionFuture(tFutur).getValeurComptable();
   }
 
   public abstract Possession projectionFuture(LocalDate tFutur);
+
+  public double convertirValeurComptable(LocalDate dateConversion, String deviseCible, Map<String, Double> tauxDeChange, Map<String, Double> tauxAppreciation) {
+    if (devise.equals(deviseCible)) {
+      return valeurComptable;
+    }
+
+    double tauxConversion = tauxDeChange.get(devise + "_" + deviseCible);
+    double tauxAppreciationAnnuelle = tauxAppreciation.get(devise + "_" + deviseCible);
+    double joursEcoules = java.time.temporal.ChronoUnit.DAYS.between(t, dateConversion);
+
+    return valeurComptable * tauxConversion * Math.pow(1 + tauxAppreciationAnnuelle / 365, joursEcoules);
+  }
 }
