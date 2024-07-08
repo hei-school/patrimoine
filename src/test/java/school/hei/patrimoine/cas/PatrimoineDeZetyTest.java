b62b430f02a3cd94fb0e23167c12925ac2eee3b9
Mahefaa <hei.mahefa@gmail.com>
2024-07-08 13:06:25 +0300

    chore(test): disable PatrimoineDeZetyTest::zety_part_en_Allemagne

--- src/test/java/school/hei/patrimoine/cas/PatrimoineDeZetyTest.java
+++ src/test/java/school/hei/patrimoine/cas/PatrimoineDeZetyTest.java
@@ -9,6 +9,7 @@ import static school.hei.patrimoine.cas.zety.PatrimoineZetyAu3Juillet2024.AU_3_J
 
 import java.time.LocalDate;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.cas.zety.PatrimoineZetyAu3Juillet2024;
 import school.hei.patrimoine.modele.Devise;
@@ -93,6 +94,7 @@ class PatrimoineDeZetyTest {
 
 
 	@Test
+	@Disabled("change chaque jour, parce que la devise s'apprécie")
 	void zety_part_en_Allemagne() {
 		Devise euro = new Devise("euro", 4821, AU_3_JUILLET_2024, -0.1);
 		var patrimoineDeZetyLe14Fevrier2025 = patrimoineDeZetyLe26Octobre2025(euro)
