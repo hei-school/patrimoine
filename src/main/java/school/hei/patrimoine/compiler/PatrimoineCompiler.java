b671a3d221df12dee2e8d8fe59086cd865d4f880
Lou Andria <lou@hei.school>
2024-09-24 16:06:55 +0200

    fix: out/production --> ~/patrimoine-io

--- src/main/java/school/hei/patrimoine/compiler/PatrimoineCompiler.java
+++ src/main/java/school/hei/patrimoine/compiler/PatrimoineCompiler.java
@@ -1,49 +1,61 @@
 package school.hei.patrimoine.compiler;
 
+import static javax.tools.ToolProvider.getSystemJavaCompiler;
+
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.function.BiFunction;
 import java.util.function.Supplier;
-import javax.tools.JavaCompiler;
-import javax.tools.ToolProvider;
+import lombok.SneakyThrows;
 import school.hei.patrimoine.modele.Patrimoine;
 
 public class PatrimoineCompiler implements BiFunction<String, String, Patrimoine> {
 
-  private static final String CLASS_OUTPUT_DIR = "out/production/classes/";
+  private static final String IO_DIR_NAME = System.getProperty("user.home") + "/patrimoine-io";
 
+  static {
+    new File(IO_DIR_NAME).mkdir();
+  }
+
+  @SneakyThrows
   @Override
   public Patrimoine apply(String className, String javaSource) {
-    try {
-      String javaFile = CLASS_OUTPUT_DIR + className + ".java";
+    var ioDirPath = Path.of(IO_DIR_NAME);
+    var sourcePath = Path.of(ioDirPath + "/" + className + ".java");
+    Files.write(sourcePath, javaSource.getBytes());
 
-      Files.write(Paths.get(javaFile), javaSource.getBytes());
+    compile(ioDirPath, sourcePath);
 
-      JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-      int result = compiler.run(null, null, null, "-d", CLASS_OUTPUT_DIR, javaFile);
+    var dynamicClass = loadClass(className, ioDirPath);
+    var patrimoineSupplier =
+        (Supplier<Patrimoine>) dynamicClass.getDeclaredConstructor().newInstance();
+    return patrimoineSupplier.get();
+  }
 
-      if (result != 0) {
-        throw new RuntimeException("Compilation failed.");
-      }
+  private static Class<?> loadClass(String className, Path ioDirPath)
+      throws MalformedURLException, ClassNotFoundException {
+    var classLoader = URLClassLoader.newInstance(new URL[] {ioDirPath.toUri().toURL()});
 
-      File outputDir = new File(CLASS_OUTPUT_DIR);
-      URLClassLoader classLoader =
-          URLClassLoader.newInstance(new URL[] {outputDir.toURI().toURL()});
-      Class<?> dynamicClass = Class.forName(className, true, classLoader);
+    return Class.forName(className, true, classLoader);
+  }
 
-      if (!Supplier.class.isAssignableFrom(dynamicClass)) {
-        throw new RuntimeException("The compiled class does not implement Supplier<Patrimoine>.");
-      }
+  private void compile(Path ioDirPath, Path sourcePath) {
+    var compiler = getSystemJavaCompiler();
+    int result =
+        compiler.run(
+            null,
+            null,
+            null,
+            "-d",
+            ioDirPath.toFile().getAbsolutePath(),
+            sourcePath.toFile().getAbsolutePath());
 
-      Supplier<Patrimoine> instance =
-          (Supplier<Patrimoine>) dynamicClass.getDeclaredConstructor().newInstance();
-
-      return instance.get();
-    } catch (Exception e) {
-      throw new RuntimeException("Error during compilation or execution", e);
+    if (result != 0) {
+      throw new RuntimeException("Compilation failed. Error code=" + result);
     }
   }
 }
