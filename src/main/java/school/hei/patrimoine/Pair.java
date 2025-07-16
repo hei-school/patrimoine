2c6c38eba0356131904eec35f960e064961d0c37
Sedera RAFALIMANANA <hei.sedera@gmail.com>
2025-07-16 09:06:50 +0300

    My first commit

--- src/main/java/school/hei/patrimoine/Pair.java
+++ src/main/java/school/hei/patrimoine/Pair.java
@@ -2,5 +2,6 @@ package school.hei.patrimoine;
 
 public record Pair<F, S>(F first, S second) {
   public static <F, S> Pair<F, S> of(F first, S second) {
-    return new Pair<>(first, second); }
+    return new Pair<>(first, second);
+  }
 }
