48b41325af21ce9a2464c977f0d833bfb0f20b31
Sedera RAFALIMANANA <hei.sedera@gmail.com>
2025-07-16 11:41:05 +0300

    feat: add function for sale and modified valeurComptable logic handling

--- src/main/java/school/hei/patrimoine/modele/possession/Possession.java
+++ src/main/java/school/hei/patrimoine/modele/possession/Possession.java
@@ -12,20 +12,19 @@ import school.hei.patrimoine.modele.objectif.Objectivable;
 @EqualsAndHashCode(callSuper = false)
 public abstract sealed class Possession extends Objectivable
     implements Serializable /*note(no-serializable)*/
-    permits AchatMaterielAuComptant,
-        Compte,
-        CompteCorrection,
-        Correction,
-        FluxArgent,
-        GroupePossession,
-        Materiel,
-        PatrimoinePersonnel,
-        PersonneMorale,
-        RemboursementDette,
-        TransfertArgent {
+        permits AchatMaterielAuComptant, Compte, CompteCorrection, Correction, Entreprise, FluxArgent, GroupePossession, Materiel, PatrimoinePersonnel, PersonneMorale, RemboursementDette, TransfertArgent {
   protected final String nom;
   protected final LocalDate t;
   protected final Argent valeurComptable;
+  // Note: valeurComptable is the value at time t, not the current value
+  @EqualsAndHashCode.Exclude @ToString.Exclude private Argent valeurMarche;
+
+  @EqualsAndHashCode.Exclude @ToString.Exclude private boolean estVendue = false;
+
+  @EqualsAndHashCode.Exclude @ToString.Exclude private LocalDate dateVente;
+
+  @EqualsAndHashCode.Exclude @ToString.Exclude private Compte compteBeneficiaire;
+
   @EqualsAndHashCode.Exclude @ToString.Exclude private CompteCorrection compteCorrection;
 
   public Possession(String nom, LocalDate t, Argent valeurComptable) {
@@ -43,6 +42,9 @@ public abstract sealed class Possession extends Objectivable
   }
 
   public Argent valeurComptable() {
+    if (estVendue && dateVente != null && !t.isBefore(dateVente)) {
+      return new Argent(0.0, valeurComptable.devise());
+    }
     return valeurComptable;
   }
 
@@ -66,5 +68,32 @@ public abstract sealed class Possession extends Objectivable
   @Override
   public Argent valeurAObjectifT(LocalDate t) {
     return projectionFuture(t).valeurComptable;
+  }
+
+  public Argent valeurMarche() {
+    if (typeAgregat() == TypeAgregat.IMMOBILISATION
+            || typeAgregat() == TypeAgregat.ENTREPRISE) {
+      return valeurMarche != null ? valeurMarche : valeurComptable;
+    }
+    return valeurComptable;
+  }
+
+  public void vendre(LocalDate date, Argent montant, Compte compteDestination) {
+    if (estVendue) {
+      throw new IllegalStateException("Possession déjà vendue : " + this);
+    }
+
+    this.estVendue = true;
+    this.dateVente = date;
+    this.compteBeneficiaire = compteDestination;
+    this.valeurMarche = montant;
+
+    new TransfertArgent(
+            "Vente de " + nom,
+            null,
+            compteDestination,
+            date,
+            montant
+    );
   }
 }
