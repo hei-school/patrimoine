f7d72b15775558bc0323badd9b7de0e7b4437b85
Sheddy <hei.sheddy.2@gmail.com>
2025-07-17 14:16:58 +0300

    feat(vente): package for sale possession

---
+++ src/main/java/school/hei/patrimoine/modele/vente/Vendable.java
@@ -0,0 +1,9 @@
+package school.hei.patrimoine.modele.vente;
+
+import school.hei.patrimoine.modele.Argent;
+
+import java.time.LocalDate;
+
+public abstract class Vendable {
+    public abstract Argent getValeurMarche(LocalDate t);
+}
