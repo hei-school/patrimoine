929342fa5d08382d6ced4a2b73fefcecc04b7894
TsioryR98 <hei.tsiory.2@gmail.com>
2025-07-22 09:54:09 +0300

    fix: implements Serializable to ValeurMarche class

--- src/main/java/school/hei/patrimoine/modele/vente/ValeurMarche.java
+++ src/main/java/school/hei/patrimoine/modele/vente/ValeurMarche.java
@@ -1,9 +1,10 @@
 package school.hei.patrimoine.modele.vente;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import school.hei.patrimoine.modele.Argent;
 
-public record ValeurMarche(LocalDate t, Argent valeur) {
+public record ValeurMarche(LocalDate t, Argent valeur) implements Serializable {
   public ValeurMarche {
     if (t == null || valeur == null)
       throw new IllegalArgumentException("Date and value cannot be null");
