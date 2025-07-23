c6528de4bc86a97c82da38a9bb05b3ce6a38dba0
andryraz <tiavinarazafindratsimba20@gmail.com>
2025-07-23 09:19:46 +0300

    test: correction of the sales test Material

--- src/test/java/school/hei/patrimoine/modele/possession/MaterielTest.java
+++ src/test/java/school/hei/patrimoine/modele/possession/MaterielTest.java
@@ -12,14 +12,15 @@ public class MaterielTest {
 
   @Test
   public void testVenteMateriel() {
-    // Arrange
+
     LocalDate dateAchat = LocalDate.of(2024, 1, 1);
     LocalDate dateComptable = LocalDate.of(2024, 1, 1);
     LocalDate dateVente = LocalDate.of(2025, 7, 17);
     Argent valeurComptable = new Argent(1_000, Devise.EUR);
-    //Argent valeurMarche = new Argent(1_200, Devise.EUR);
     Argent prixVente = new Argent(900, Devise.EUR);
-    double tauxAppreciation = 0.1;
+    double tauxAppreciation = 0.0;
+    Compte compteBeneficiaire = new Compte("Caisse", dateAchat, new Argent(0, Devise.EUR));
+
 
     Materiel materiel = new Materiel(
             "Ordinateur",
@@ -32,14 +33,22 @@ public class MaterielTest {
     // Assert avant vente
     assertFalse(materiel.estVendue(), "Le matériel ne doit pas être vendu avant appel à vendre()");
     assertTrue(materiel.estActiveALaDate(LocalDate.of(2024, 12, 31)));
+    assertEquals(valeurComptable, materiel.valeurComptableFuture(dateVente.minusDays(1)));
 
     // Act
-    materiel.vendre(dateVente, prixVente);
+    materiel.vendre(dateVente, prixVente, compteBeneficiaire);
 
     // Assert après vente
     assertTrue(materiel.estVendue(), "Le matériel doit être marqué comme vendu après appel à vendre()");
     assertEquals(dateVente, materiel.getDateVente(), "La date de vente doit être correcte");
     assertEquals(prixVente, materiel.getPrixVente(), "Le prix de vente doit être correctement enregistré");
+
+    // Vérification de la désactivation comptable : valeur comptable = 0 après la vente
+    assertEquals(
+            new Argent(0, Devise.EUR),
+            materiel.projectionFuture(dateVente.plusDays(1)).valeurComptable()
+
+    );
 
     // Vérifie que la possession est inactive après la vente
     assertFalse(materiel.estActiveALaDate(LocalDate.of(2025, 7, 18)), "Le matériel ne doit plus être actif après la date de vente");
