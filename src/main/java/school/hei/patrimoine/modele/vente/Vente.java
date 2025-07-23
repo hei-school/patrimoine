8bf1b16f347f8ce652b2ca903535fbc725514349
Phylosius <hei.phylosius@gmail.com>
2025-07-23 16:39:50 +0300

    chore: add getter to sell class

--- src/main/java/school/hei/patrimoine/modele/vente/Vente.java
+++ src/main/java/school/hei/patrimoine/modele/vente/Vente.java
@@ -1,10 +1,13 @@
 package school.hei.patrimoine.modele.vente;
 
 import java.time.LocalDate;
+
+import lombok.Getter;
 import school.hei.patrimoine.modele.Argent;
 import school.hei.patrimoine.modele.possession.Compte;
 import school.hei.patrimoine.modele.possession.Possession;
 
+@Getter
 public final class Vente {
   private final Possession possessionAVendre;
 
