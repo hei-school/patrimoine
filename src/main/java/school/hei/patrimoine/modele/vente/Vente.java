8b6b83870efb340324f90053bc89b30fe05495ff
Phylosius <hei.phylosius@gmail.com>
2025-07-23 16:28:16 +0300

    style: format

--- src/main/java/school/hei/patrimoine/modele/vente/Vente.java
+++ src/main/java/school/hei/patrimoine/modele/vente/Vente.java
@@ -1,15 +1,18 @@
 package school.hei.patrimoine.modele.vente;
 
+import java.time.LocalDate;
 import school.hei.patrimoine.modele.Argent;
 import school.hei.patrimoine.modele.possession.Compte;
 import school.hei.patrimoine.modele.possession.Possession;
 
-import java.time.LocalDate;
-
 public final class Vente {
   private final Possession possessionAVendre;
 
-  public Vente(Possession possessionAVendre, Argent valeurDeVente, LocalDate dateDeVente, Compte compteBeneficiaire) {
+  public Vente(
+      Possession possessionAVendre,
+      Argent valeurDeVente,
+      LocalDate dateDeVente,
+      Compte compteBeneficiaire) {
     this.possessionAVendre = possessionAVendre;
     this.possessionAVendre.vendre(valeurDeVente, dateDeVente, compteBeneficiaire);
   }
