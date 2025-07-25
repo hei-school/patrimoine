5bb7486d2e3c121f271881128d9e4abe77cb14ec
Neswilly <hei.neswilly.2@gmail.com>
2025-07-25 14:42:15 +0300

    fix: change type checking of Possession in addValeurMarche and vendre methods

--- src/main/java/school/hei/patrimoine/modele/possession/Possession.java
+++ src/main/java/school/hei/patrimoine/modele/possession/Possession.java
@@ -102,7 +102,7 @@ public abstract sealed class Possession extends Objectivable
 
   @Override
   public void addValeurMarche(ValeurMarchee v) {
-    if ((typeAgregat() != TypeAgregat.IMMOBILISATION) || (typeAgregat() != TypeAgregat.ENTREPRISE))
+    if ((typeAgregat() != TypeAgregat.IMMOBILISATION) && (typeAgregat() != TypeAgregat.ENTREPRISE))
       throw new UnsupportedOperationException("Impossible d'ajouter une valeur de marché pour ce type d'agrégat");
 
     informationDeVente.addValeurMarche(v);
@@ -130,7 +130,7 @@ public abstract sealed class Possession extends Objectivable
 
   @Override
   public void vendre(Argent valeurDeVente, LocalDate dateDeVente, Compte compteBeneficiaire) {
-    if ((typeAgregat() != TypeAgregat.IMMOBILISATION) || (typeAgregat() != TypeAgregat.ENTREPRISE))
+    if ((typeAgregat() != TypeAgregat.IMMOBILISATION) && (typeAgregat() != TypeAgregat.ENTREPRISE))
         throw new UnsupportedOperationException("Impossible de vendre ce type d'agrégat");
     informationDeVente.confirmeVente(this, valeurDeVente, dateDeVente, compteBeneficiaire);
   }
