7d9244a016c3c00a928c815b2419826c2dd6892f
Sheddy <hei.sheddy.2@gmail.com>
2025-07-29 00:00:56 +0300

    feat: ValeurMarcheVisitor interface

--- src/main/java/school/hei/patrimoine/patrilang/visitors/possession/ValeurMarcheVisitor.java
+++ src/main/java/school/hei/patrimoine/patrilang/visitors/possession/ValeurMarcheVisitor.java
@@ -1,4 +1,15 @@
 package school.hei.patrimoine.patrilang.visitors.possession;
 
-public interface ValeurMarcheVisitor {
+import java.util.function.Function;
+import school.hei.patrimoine.patrilang.antlr.PatriLangParser.DeclarationValeurMarcheContext;
+
+@FunctionalInterface
+public interface ValeurMarcheVisitor extends Function<DeclarationValeurMarcheContext, Void> {
+  @Override
+  default Void apply(DeclarationValeurMarcheContext ctx) {
+    visit(ctx);
+    return null;
+  }
+
+  void visit(DeclarationValeurMarcheContext ctx);
 }
