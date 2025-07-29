1a57493d4600e7933e96a8ef2b1d407c950818c3
Neswilly <hei.neswilly.2@gmail.com>
2025-07-29 14:48:16 +0300

    fix(test): add the scope to ValeurMarcheeVisitor and VenteVisitor

--- src/test/java/school/hei/patrimoine/patrilang/unit/visitors/possession/VenteVisitorTest.java
+++ src/test/java/school/hei/patrimoine/patrilang/unit/visitors/possession/VenteVisitorTest.java
@@ -1,12 +1,18 @@
 package school.hei.patrimoine.patrilang.unit.visitors.possession;
 
 import org.junit.jupiter.api.Test;
+import school.hei.patrimoine.modele.possession.Compte;
 import school.hei.patrimoine.patrilang.antlr.PatriLangParser;
 import school.hei.patrimoine.patrilang.utils.UnitTestVisitor;
 import school.hei.patrimoine.patrilang.visitors.possession.VenteVisitor;
 import school.hei.patrimoine.patrilang.visitors.variable.VariableVisitor;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static school.hei.patrimoine.modele.Argent.ariary;
+import static school.hei.patrimoine.patrilang.modele.variable.VariableType.TRESORERIES;
 
 public class VenteVisitorTest {
     VariableVisitor variableVisitor = new VariableVisitor();
@@ -20,12 +26,17 @@ public class VenteVisitorTest {
     };
 
     @Test
-    void throws_exception_when_possession_type_not_supported(){
+    void throws_exception_when_possession_type_is_not_immobilisation_or_entreprise() {
+        variableVisitor.addToScope("monCompte", TRESORERIES,
+                new Compte("monCompte", LocalDate.of(2025, 1, 1), ariary(100_000)));
+
         var input =
-                "*ventePossession, Dates:ajd, vente de Entreprise:monEntreprise pour 300000Ar vers Trésoreries:monCompte";
+                "*`ventePossession`, le 01 du 01-2025, vente de Trésoreries:monCompte pour 300000Ar vers Trésoreries:monCompte";
 
         var exception = assertThrows(UnsupportedOperationException.class, () ->
                 visitor.visit(input, PatriLangParser::ventePossession)
         );
+
+        assertTrue(exception.getMessage().contains("Impossible de vendre"));
     }
 }
