2c90b39723a3772249a2cf4415110cabd78b1c23
Sedera RAFALIMANANA <hei.sedera@gmail.com>
2025-07-30 14:39:22 +0300

    Test: ValeurMarcheVisitorTest

--- src/test/java/school/hei/patrimoine/patrilang/unit/visitors/possession/ValeurMarcheVisitorTest.java
+++ src/test/java/school/hei/patrimoine/patrilang/unit/visitors/possession/ValeurMarcheVisitorTest.java
@@ -3,12 +3,9 @@ package school.hei.patrimoine.patrilang.unit.visitors.possession;
 import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.Argent;
 import school.hei.patrimoine.modele.Devise;
-import school.hei.patrimoine.modele.possession.Compte;
 import school.hei.patrimoine.modele.possession.Materiel;
-import school.hei.patrimoine.modele.possession.Possession;
 import school.hei.patrimoine.modele.vente.ValeurMarche;
 import school.hei.patrimoine.patrilang.antlr.PatriLangParser;
-import school.hei.patrimoine.patrilang.antlr.PatriLangParser.ValeurMarcheContext;
 import school.hei.patrimoine.patrilang.utils.UnitTestVisitor;
 import school.hei.patrimoine.patrilang.visitors.possession.ValeurMarcheVisitor;
 import school.hei.patrimoine.patrilang.visitors.variable.VariableVisitor;
@@ -18,35 +15,34 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static school.hei.patrimoine.modele.Argent.ariary;
 import static school.hei.patrimoine.patrilang.modele.variable.VariableType.DATE;
+import static school.hei.patrimoine.patrilang.modele.variable.VariableType.TRESORERIES;
 
 public class ValeurMarcheVisitorTest {
-  private static final VariableVisitor variableVisitor = new VariableVisitor();
   private static final LocalDate AJD = LocalDate.now();
-  ValeurMarcheVisitor subject = new ValeurMarcheVisitor(new VariableVisitor());
+  private static final VariableVisitor variableVisitor = new VariableVisitor();
 
-  UnitTestVisitor visitor =
-          new UnitTestVisitor() {
-            @Override
-            public ValeurMarche visitValeurMarche(ValeurMarcheContext ctx) {
-              return subject.apply(ctx);
-            }
-          };
+  ValeurMarcheVisitor subject = new ValeurMarcheVisitor(variableVisitor);
 
-  static {
-    variableVisitor.addToScope("ajd", DATE, AJD);
-  }
+  UnitTestVisitor visitor = new UnitTestVisitor() {
+    @Override
+    public ValeurMarche visitValeurMarche(PatriLangParser.ValeurMarcheContext ctx) {
+      return subject.apply(ctx);
+    }
+  };
 
   @Test
   void parse_valeur_marche() {
-    var input = "* `valeurMarche` Dates:ajd, possessionTest, valant 1000000Ar";
+    Materiel possession = new Materiel("maisonTest", AJD, AJD, new Argent(1_000_000, Devise.MGA), 2.0);
+    variableVisitor.addToScope("possessionTest", TRESORERIES, possession);
+    variableVisitor.addToScope("ajd", DATE, AJD);
 
-    Possession possession = new Materiel("maisonTest", LocalDate.now(),LocalDate.now(), new Argent(1_000_000, Devise.MGA), 2.0);
-    var expected = new ValeurMarche(possession, LocalDate.now(), ariary(1_000_000));
+    var input = "* `valeurMarche` Dates:ajd, Trésoreries:possessionTest valant 1000000Ar";
 
+    ValeurMarche expected = new ValeurMarche(possession, AJD, ariary(1_000_000));
     ValeurMarche actual = visitor.visit(input, PatriLangParser::valeurMarche);
 
     assertEquals(expected.t(), actual.t());
-    assertEquals(expected.valeur(), actual.valeur());
-//    assertEquals(expercted.possession(), actual.possession());
+    assertEquals(expected.valeur().getMontant(), actual.valeur().getMontant());
+    assertEquals(expected.possession().nom(), actual.possession().nom());
   }
 }
