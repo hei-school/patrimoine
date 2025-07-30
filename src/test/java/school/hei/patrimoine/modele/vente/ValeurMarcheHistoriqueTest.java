59db0912c41b811d76b3560e60cbc47d8e85df88
Sedera RAFALIMANANA <hei.sedera@gmail.com>
2025-07-30 21:00:24 +0300

    Test: switching to var for variable

--- src/test/java/school/hei/patrimoine/modele/vente/ValeurMarcheHistoriqueTest.java
+++ src/test/java/school/hei/patrimoine/modele/vente/ValeurMarcheHistoriqueTest.java
@@ -14,18 +14,15 @@ public class ValeurMarcheHistoriqueTest {
 
   @Test
   void historique_valeur_marche_contient_valeur_initiale() {
-    // Given
-    LocalDate aujourdhui = LocalDate.now();
-    Argent valeurInitiale = new Argent(1000, Devise.EUR);
-    Materiel materiel = new Materiel("Ordinateur", aujourdhui, aujourdhui, valeurInitiale, 0.05);
+    var aujourdhui = LocalDate.now();
+    var valeurInitiale = new Argent(1000, Devise.EUR);
+    var materiel = new Materiel("Ordinateur", aujourdhui, aujourdhui, valeurInitiale, 0.05);
     new ValeurMarche(materiel, aujourdhui, valeurInitiale);
 
-    // When
-    Set<ValeurMarche> historique = materiel.historiqueValeurMarche();
+    var historique = materiel.historiqueValeurMarche();
 
-    // Then
     assertEquals(1, historique.size());
-    ValeurMarche premiereValeur = historique.iterator().next();
+    var premiereValeur = historique.iterator().next();
     assertEquals(materiel, premiereValeur.possession());
     assertEquals(aujourdhui, premiereValeur.t());
     assertEquals(valeurInitiale, premiereValeur.valeur());
@@ -33,37 +30,31 @@ public class ValeurMarcheHistoriqueTest {
 
   @Test
   void historique_valeur_marche_retourne_copie_defensive() {
-    // Given
-    LocalDate aujourdhui = LocalDate.now();
-    Argent valeurInitiale = new Argent(1000, Devise.EUR);
-    Materiel materiel = new Materiel("Ordinateur", aujourdhui, aujourdhui, valeurInitiale, 0.05);
+    var aujourdhui = LocalDate.now();
+    var valeurInitiale = new Argent(1000, Devise.EUR);
+    var materiel = new Materiel("Ordinateur", aujourdhui, aujourdhui, valeurInitiale, 0.05);
     new ValeurMarche(materiel, aujourdhui, valeurInitiale);
 
-    // When
-    Set<ValeurMarche> copie = materiel.historiqueValeurMarche();
+    var copie = materiel.historiqueValeurMarche();
     copie.clear();
 
-    // Then
     assertEquals(1, materiel.historiqueValeurMarche().size());
     assertTrue(copie.isEmpty());
   }
 
   @Test
   void historique_valeur_marche_apres_ajout_nouvelle_valeur() {
-    // Given
-    LocalDate aujourdhui = LocalDate.now();
-    Argent valeurInitiale = new Argent(1000, Devise.EUR);
-    Materiel materiel = new Materiel("Ordinateur", aujourdhui, aujourdhui, valeurInitiale, 0.05);
+    var aujourdhui = LocalDate.now();
+    var valeurInitiale = new Argent(1000, Devise.EUR);
+    var materiel = new Materiel("Ordinateur", aujourdhui, aujourdhui, valeurInitiale, 0.05);
     new ValeurMarche(materiel, aujourdhui, valeurInitiale);
 
-    LocalDate demain = aujourdhui.plusDays(1);
-    Argent nouvelleValeur = new Argent(1050, Devise.EUR);
+    var demain = aujourdhui.plusDays(1);
+    var nouvelleValeur = new Argent(1050, Devise.EUR);
 
-    // When
     materiel.ajouterValeurMarche(new ValeurMarche(materiel, demain, nouvelleValeur));
 
-    // Then
-    Set<ValeurMarche> historique = materiel.historiqueValeurMarche();
+    var historique = materiel.historiqueValeurMarche();
     assertEquals(2, historique.size());
     assertTrue(historique.contains(new ValeurMarche(materiel, aujourdhui, valeurInitiale)));
     assertTrue(historique.contains(new ValeurMarche(materiel, demain, nouvelleValeur)));
@@ -71,17 +62,14 @@ public class ValeurMarcheHistoriqueTest {
 
   @Test
   void historique_valeur_marche_pour_type_non_eligible() {
-    // Given
-    LocalDate aujourdhui = LocalDate.now();
-    Argent valeurInitiale = new Argent(1000, Devise.EUR);
-    Compte compte = new Compte("Compte Courant", aujourdhui, valeurInitiale);
+    var aujourdhui = LocalDate.now();
+    var valeurInitiale = new Argent(1000, Devise.EUR);
+    var compte = new Compte("Compte Courant", aujourdhui, valeurInitiale);
 
-    // When/Then
     assertThrows(UnsupportedOperationException.class, () -> {
       compte.ajouterValeurMarche(new ValeurMarche(compte, aujourdhui, valeurInitiale));
     });
 
-    // Then
     assertEquals(0, compte.historiqueValeurMarche().size());
   }
 }
--- src/test/java/school/hei/patrimoine/patrilang/unit/visitors/possession/ValeurMarcheVisitorTest.java
+++ src/test/java/school/hei/patrimoine/patrilang/unit/visitors/possession/ValeurMarcheVisitorTest.java
@@ -21,8 +21,8 @@ import static school.hei.patrimoine.patrilang.modele.variable.VariableType.TRESO
 
 public class ValeurMarcheVisitorTest {
   private static final LocalDate AJD = LocalDate.now();
-  private static final LocalDate DATE_PASSEE = LocalDate.now().minusYears(1);
-  private static final LocalDate DATE_FUTURE = LocalDate.now().plusYears(1);
+  private static final LocalDate DATE_PASSEE = AJD.minusYears(1);
+  private static final LocalDate DATE_FUTURE = AJD.plusYears(1);
 
   private VariableVisitor variableVisitor;
   private ValeurMarcheVisitor subject;
@@ -42,13 +42,13 @@ public class ValeurMarcheVisitorTest {
 
   @Test
   void parse_valeur_marche_materiel() {
-    Materiel possession = new Materiel("maisonTest", AJD, AJD, new Argent(1_000_000, Devise.MGA), 2.0);
+    var possession = new Materiel("maisonTest", AJD, AJD, new Argent(1_000_000, Devise.MGA), 2.0);
     variableVisitor.addToScope("possessionTest", TRESORERIES, possession);
     variableVisitor.addToScope("ajd", DATE, AJD);
 
     var input = "* `valeurMarche` Dates:ajd, Trésoreries:possessionTest valant 1000000Ar";
 
-    ValeurMarche expected = new ValeurMarche(possession, AJD, ariary(1_000_000));
+    var expected = new ValeurMarche(possession, AJD, ariary(1_000_000));
     ValeurMarche actual = visitor.visit(input, PatriLangParser::valeurMarche);
 
     assertEquals(expected.t(), actual.t());
@@ -58,13 +58,13 @@ public class ValeurMarcheVisitorTest {
 
   @Test
   void parse_valeur_marche_date_passee() {
-    Materiel possession = new Materiel("voiture", DATE_PASSEE, DATE_PASSEE, ariary(10_000_000), 10.0);
+    var possession = new Materiel("voiture", DATE_PASSEE, DATE_PASSEE, ariary(10_000_000), 10.0);
     variableVisitor.addToScope("voitureTest", TRESORERIES, possession);
     variableVisitor.addToScope("datePassee", DATE, DATE_PASSEE);
 
     var input = "* `valeurMarche` Dates:datePassee, Trésoreries:voitureTest valant 8000000Ar";
 
-    ValeurMarche expected = new ValeurMarche(possession, DATE_PASSEE, ariary(8_000_000));
+    var expected = new ValeurMarche(possession, DATE_PASSEE, ariary(8_000_000));
     ValeurMarche actual = visitor.visit(input, PatriLangParser::valeurMarche);
 
     assertEquals(expected.t(), actual.t());
@@ -73,13 +73,13 @@ public class ValeurMarcheVisitorTest {
 
   @Test
   void parse_valeur_marche_date_future() {
-    Materiel possession = new Materiel("ordinateur", AJD, AJD, ariary(2_000_000), 5.0);
+    var possession = new Materiel("ordinateur", AJD, AJD, ariary(2_000_000), 5.0);
     variableVisitor.addToScope("ordiTest", TRESORERIES, possession);
     variableVisitor.addToScope("dateFuture", DATE, DATE_FUTURE);
 
     var input = "* `valeurMarche` Dates:dateFuture, Trésoreries:ordiTest valant 1500000Ar";
 
-    ValeurMarche expected = new ValeurMarche(possession, DATE_FUTURE, ariary(1_500_000));
+    var expected = new ValeurMarche(possession, DATE_FUTURE, ariary(1_500_000));
     ValeurMarche actual = visitor.visit(input, PatriLangParser::valeurMarche);
 
     assertEquals(expected.t(), actual.t());
