eb1e42f69f5b88a932f70b8c4ffe46f973af5f0d
Mihajatiana <hei.mihajatiana@gmail.com>
2025-07-30 16:03:42 +0300

    chore: format code

--- src/main/java/school/hei/patrimoine/patrilang/visitors/possession/VenteVisitor.java
+++ src/main/java/school/hei/patrimoine/patrilang/visitors/possession/VenteVisitor.java
@@ -1,5 +1,6 @@
 package school.hei.patrimoine.patrilang.visitors.possession;
 
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import school.hei.patrimoine.modele.Argent;
 import school.hei.patrimoine.modele.possession.Compte;
@@ -9,18 +10,16 @@ import school.hei.patrimoine.patrilang.antlr.PatriLangParser.VenteContext;
 import school.hei.patrimoine.patrilang.visitors.SimpleVisitor;
 import school.hei.patrimoine.patrilang.visitors.variable.VariableVisitor;
 
-import java.time.LocalDate;
-
 @RequiredArgsConstructor
 public class VenteVisitor implements SimpleVisitor<VenteContext, Vente> {
-    private final VariableVisitor variableVisitor;
+  private final VariableVisitor variableVisitor;
 
-    @Override
-    public Vente apply(VenteContext ctx) {
-        LocalDate tVente = this.variableVisitor.asDate(ctx.dateValue);
-        Possession possession = this.variableVisitor.asPossession(ctx.possession);
-        Compte compteBeneficiaire = this.variableVisitor.asCompte(ctx.compteBeneficiaire);
-        Argent prixVente = this.variableVisitor.asArgent(ctx.prixVente);
-        return new Vente(tVente, possession, compteBeneficiaire, prixVente);
-    }
+  @Override
+  public Vente apply(VenteContext ctx) {
+    LocalDate tVente = this.variableVisitor.asDate(ctx.dateValue);
+    Possession possession = this.variableVisitor.asPossession(ctx.possession);
+    Compte compteBeneficiaire = this.variableVisitor.asCompte(ctx.compteBeneficiaire);
+    Argent prixVente = this.variableVisitor.asArgent(ctx.prixVente);
+    return new Vente(tVente, possession, compteBeneficiaire, prixVente);
+  }
 }
