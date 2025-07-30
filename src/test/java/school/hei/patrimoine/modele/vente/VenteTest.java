18cf7e001df6d6389fb21ae480e16a603b533eb2
Mihajatiana <hei.mihajatiana@gmail.com>
2025-07-30 16:33:14 +0300

    feat: add VenteTest

--- src/test/java/school/hei/patrimoine/modele/vente/VenteTest.java
+++ src/test/java/school/hei/patrimoine/modele/vente/VenteTest.java
@@ -13,33 +13,33 @@ import school.hei.patrimoine.modele.possession.Vente;
 public class VenteTest {
 
   @Test
-  void creationVenteDoitModifierPossessionEtCrediterCompte() {
+  void creation_vente_doit_modifier_possession_et_crediter_compte() {
     LocalDate dateVente = LocalDate.of(2025, 7, 30);
     Argent prixVente = Argent.ariary(5000);
     Compte compteBeneficiaire =
-        new Compte(
-            "Compte test", dateVente.minusMonths(1), dateVente.minusMonths(1), Argent.ariary(1000));
+            new Compte(
+                    "Compte test", dateVente.minusMonths(1), dateVente.minusMonths(1), Argent.ariary(1000));
     Compte possession =
-        new Compte(
-            "Possession test",
-            dateVente.minusYears(1),
-            dateVente.minusYears(1),
-            Argent.ariary(3000));
+            new Compte(
+                    "Possession test",
+                    dateVente.minusYears(1),
+                    dateVente.minusYears(1),
+                    Argent.ariary(3000));
 
     new Vente(dateVente, possession, compteBeneficiaire, prixVente);
 
     assertTrue(possession.estVendu(dateVente), "La possession devrait être marquée comme vendue");
     assertEquals(
-        Optional.of(dateVente),
-        possession.getDateVente(),
-        "La date de vente devrait être correctement enregistrée");
+            Optional.of(dateVente),
+            possession.getDateVente(),
+            "La date de vente devrait être correctement enregistrée");
     assertEquals(
-        Optional.of(prixVente),
-        possession.getPrixVente(),
-        "Le prix de vente devrait être correctement enregistré");
+            Optional.of(prixVente),
+            possession.getPrixVente(),
+            "Le prix de vente devrait être correctement enregistré");
     assertEquals(
-        1,
-        compteBeneficiaire.getFluxArgents().size(),
-        "Le compte bénéficiaire devrait contenir un flux financier");
+            1,
+            compteBeneficiaire.getFluxArgents().size(),
+            "Le compte bénéficiaire devrait contenir un flux financier");
   }
 }
