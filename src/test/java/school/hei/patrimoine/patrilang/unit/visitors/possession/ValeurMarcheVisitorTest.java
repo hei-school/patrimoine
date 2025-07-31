f2b1819b5a1fcdbe376b6eae51ddaf547fde1541
Mihajatiana <hei.mihajatiana@gmail.com>
2025-07-31 21:32:18 +0300

    fix: MATERIEL instead of TRESORERIE and restructure test

--- src/test/java/school/hei/patrimoine/patrilang/unit/visitors/possession/ValeurMarcheVisitorTest.java
+++ src/test/java/school/hei/patrimoine/patrilang/unit/visitors/possession/ValeurMarcheVisitorTest.java
@@ -3,17 +3,16 @@ package school.hei.patrimoine.patrilang.unit.visitors.possession;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static school.hei.patrimoine.modele.Argent.ariary;
-import static school.hei.patrimoine.patrilang.modele.variable.VariableType.DATE;
-import static school.hei.patrimoine.patrilang.modele.variable.VariableType.TRESORERIES;
+import static school.hei.patrimoine.patrilang.modele.variable.VariableType.*;
 
 import java.time.LocalDate;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.Argent;
 import school.hei.patrimoine.modele.Devise;
 import school.hei.patrimoine.modele.possession.Materiel;
 import school.hei.patrimoine.modele.vente.ValeurMarche;
 import school.hei.patrimoine.patrilang.antlr.PatriLangParser;
+import school.hei.patrimoine.patrilang.antlr.PatriLangParser.ValeurMarcheContext;
 import school.hei.patrimoine.patrilang.utils.UnitTestVisitor;
 import school.hei.patrimoine.patrilang.visitors.possession.ValeurMarcheVisitor;
 import school.hei.patrimoine.patrilang.visitors.variable.VariableVisitor;
@@ -22,33 +21,44 @@ public class ValeurMarcheVisitorTest {
   private static final LocalDate AJD = LocalDate.now();
   private static final LocalDate DATE_PASSEE = AJD.minusYears(1);
   private static final LocalDate DATE_FUTURE = AJD.plusYears(1);
+  private static final Materiel POSSESSION1 =
+      new Materiel("Téléphone", AJD, AJD, new Argent(1_000_000, Devise.MGA), 2.0);
+  private static final Materiel POSSESSION2 =
+      new Materiel("voiture", DATE_PASSEE, DATE_PASSEE, ariary(10_000_000), 10.0);
+  private static final Materiel POSSESSION3 =
+      new Materiel("ordinateur", AJD, AJD, ariary(2_000_000), 5.0);
 
-  private VariableVisitor variableVisitor;
-  private ValeurMarcheVisitor subject;
-  private UnitTestVisitor visitor;
+  private static final VariableVisitor variableVisitor = new VariableVisitor();
 
-  @BeforeEach
-  void setUp() {
-    variableVisitor = new VariableVisitor();
-    subject = new ValeurMarcheVisitor(variableVisitor);
-    visitor =
-        new UnitTestVisitor() {
-          @Override
-          public ValeurMarche visitValeurMarche(PatriLangParser.ValeurMarcheContext ctx) {
-            return subject.apply(ctx);
-          }
-        };
+  ValeurMarcheVisitor subject = new ValeurMarcheVisitor(variableVisitor);
+
+  UnitTestVisitor visitor =
+      new UnitTestVisitor() {
+        @Override
+        public ValeurMarche visitValeurMarche(ValeurMarcheContext ctx) {
+          return subject.apply(ctx);
+        }
+      };
+
+  static {
+    variableVisitor.addToScope("objet", MATERIEL, POSSESSION1);
+    variableVisitor.addToScope("objetAvecDatePasse", MATERIEL, POSSESSION2);
+    variableVisitor.addToScope("objetAvecDateFuture", MATERIEL, POSSESSION3);
+    variableVisitor.addToScope("ajd", DATE, AJD);
+    variableVisitor.addToScope("datePassee", DATE, DATE_PASSEE);
+    variableVisitor.addToScope("dateFuture", DATE, DATE_FUTURE);
   }
 
   @Test
   void parse_valeur_marche_materiel() {
-    var possession = new Materiel("maisonTest", AJD, AJD, new Argent(1_000_000, Devise.MGA), 2.0);
-    variableVisitor.addToScope("possessionTest", TRESORERIES, possession);
-    variableVisitor.addToScope("ajd", DATE, AJD);
 
-    var input = "* `valeurMarche` Dates:ajd, Trésoreries:possessionTest valant 1000000Ar";
+    var input =
+        """
+    * `valeurMarche` Dates:ajd, Matériel:objet valant 2000000Ar
+""";
 
-    var expected = new ValeurMarche(possession, AJD, ariary(1_000_000));
+    var expected = new ValeurMarche(POSSESSION1, AJD, ariary(2_000_000));
+
     ValeurMarche actual = visitor.visit(input, PatriLangParser::valeurMarche);
 
     assertEquals(expected.t(), actual.t());
@@ -58,13 +68,13 @@ public class ValeurMarcheVisitorTest {
 
   @Test
   void parse_valeur_marche_date_passee() {
-    var possession = new Materiel("voiture", DATE_PASSEE, DATE_PASSEE, ariary(10_000_000), 10.0);
-    variableVisitor.addToScope("voitureTest", TRESORERIES, possession);
-    variableVisitor.addToScope("datePassee", DATE, DATE_PASSEE);
+    var input =
+        """
+    * `valeurMarche` Dates:datePassee, Matériel:objetAvecDatePasse valant 8000000Ar
+""";
 
-    var input = "* `valeurMarche` Dates:datePassee, Trésoreries:voitureTest valant 8000000Ar";
+    var expected = new ValeurMarche(POSSESSION2, DATE_PASSEE, ariary(8_000_000));
 
-    var expected = new ValeurMarche(possession, DATE_PASSEE, ariary(8_000_000));
     ValeurMarche actual = visitor.visit(input, PatriLangParser::valeurMarche);
 
     assertEquals(expected.t(), actual.t());
@@ -73,13 +83,10 @@ public class ValeurMarcheVisitorTest {
 
   @Test
   void parse_valeur_marche_date_future() {
-    var possession = new Materiel("ordinateur", AJD, AJD, ariary(2_000_000), 5.0);
-    variableVisitor.addToScope("ordiTest", TRESORERIES, possession);
-    variableVisitor.addToScope("dateFuture", DATE, DATE_FUTURE);
+    var input = "* `valeurMarche` Dates:dateFuture, Matériel:objetAvecDateFuture valant 1500000Ar";
 
-    var input = "* `valeurMarche` Dates:dateFuture, Trésoreries:ordiTest valant 1500000Ar";
+    var expected = new ValeurMarche(POSSESSION3, DATE_FUTURE, ariary(1_500_000));
 
-    var expected = new ValeurMarche(possession, DATE_FUTURE, ariary(1_500_000));
     ValeurMarche actual = visitor.visit(input, PatriLangParser::valeurMarche);
 
     assertEquals(expected.t(), actual.t());
@@ -88,9 +95,7 @@ public class ValeurMarcheVisitorTest {
 
   @Test
   void parse_valeur_marche_possession_inconnue() {
-    variableVisitor.addToScope("ajd", DATE, AJD);
-
-    var input = "* `valeurMarche` Dates:ajd, Trésoreries:inexistante valant 1000Ar";
+    var input = "* `valeurMarche` Dates:ajd, Matériel:inexistante valant 1000Ar";
 
     assertThrows(
         IllegalArgumentException.class, () -> visitor.visit(input, PatriLangParser::valeurMarche));
