0a95ec9df2a0860a3abbfaf14a58e9cc06145998
Sheddy <hei.sheddy.2@gmail.com>
2025-10-15 16:04:25 +0300

    chore: PatriLangFilesPage refactor method and conditions

--- src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/pages/PatriLangFilesPage.java
+++ src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/pages/PatriLangFilesPage.java
@@ -1,6 +1,7 @@
 package school.hei.patrimoine.visualisation.swing.ihm.google.pages;
 
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
+import static school.hei.patrimoine.patrilang.PatriLangTranspiler.TOUT_CAS_FILE_EXTENSION;
 import static school.hei.patrimoine.patrilang.PatriLangTranspiler.transpileToutCas;
 import static school.hei.patrimoine.visualisation.swing.ihm.google.component.AppBar.*;
 import static school.hei.patrimoine.visualisation.swing.ihm.google.utils.MessageDialog.showError;
@@ -12,6 +13,7 @@ import java.util.Map;
 import java.util.Set;
 import javax.swing.*;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import school.hei.patrimoine.cas.Cas;
 import school.hei.patrimoine.cas.CasSet;
 import school.hei.patrimoine.cas.CasSetAnalyzer;
@@ -28,10 +30,10 @@ import school.hei.patrimoine.visualisation.swing.ihm.google.modele.State;
 import school.hei.patrimoine.visualisation.swing.ihm.google.utils.AsyncTask;
 
 @Getter
+@Slf4j
 public class PatriLangFilesPage extends LazyPage {
   public static final String PAGE_NAME = "patrilang-files";
   private static final Integer COMMENT_PAGE_SIZE = 2;
-  private static final String INITIAL_CAS_NAME = "CasSet.tout.md";
 
   private CasSet plannedCasSet;
   private CasSet doneCasSet;
@@ -56,8 +58,7 @@ public class PatriLangFilesPage extends LazyPage {
     globalState().subscribe(Set.of("newUpdate"), this::updateCasSet);
     updateCas();
 
-    state.subscribe("selectedFile", this::updateCas);
-    state.subscribe("selectedFile", this::updateAddImprevuButtonVisibility);
+    state.subscribe("selectedFile", () -> { this.updateCas(); this.updateAddImprevuButtonVisibility(); });
 
     setLayout(new BorderLayout());
   }
@@ -69,7 +70,7 @@ public class PatriLangFilesPage extends LazyPage {
   }
 
   private void updateAddImprevuButtonVisibility() {
-    if (addImprevuButton == null) return;
+    assert addImprevuButton != null;
 
     File selectedFile = state.get("selectedFile");
 
@@ -80,35 +81,40 @@ public class PatriLangFilesPage extends LazyPage {
             return;
           }
 
-          boolean isToutCasFile = selectedFile.getName().equals(INITIAL_CAS_NAME);
+          boolean isToutCasFile = selectedFile.getName().endsWith(TOUT_CAS_FILE_EXTENSION);
           addImprevuButton.setVisible(!isToutCasFile);
         });
   }
 
+  private Button addImprevuButton() {
+    return new Button(
+        "Ajouter un imprévu",
+        e -> {
+          if (state.get("selectedFile") == null) {
+            showError("Erreur", "Veuillez sélectionner un fichier avant d'ajouter un imprévu");
+            return;
+          }
+
+          if (plannedCasSet == null || doneCasSet == null) {
+            updateCasSet();
+          }
+
+          updateCas();
+
+          if (state.get("plannedCas") == null) {
+            showError(
+                "Erreur",
+                "Le cas planifié n'a pas été trouvé. Veuillez vérifier que le fichier"
+                    + " sélectionné est correct.");
+            return;
+          }
+
+          new AddImprevuDialog(state);
+        });
+  }
+
   private void addAppBar() {
-    addImprevuButton =
-        new Button(
-            "Ajouter un imprévu",
-            e -> {
-              if (state.get("selectedFile") == null) {
-                showError("Erreur", "Veuillez sélectionner un fichier avant d'ajouter un imprévu");
-                return;
-              }
-
-              if (plannedCasSet == null || doneCasSet == null) updateCasSet();
-
-              updateCas();
-
-              if (state.get("plannedCas") == null) {
-                showError(
-                    "Erreur",
-                    "Le cas planifié n'a pas été trouvé. Veuillez vérifier que le fichier"
-                        + " sélectionné est correct.");
-                return;
-              }
-
-              new AddImprevuDialog(state);
-            });
+    this.addImprevuButton = addImprevuButton();
 
     var appBar =
         new AppBar(
@@ -195,15 +201,19 @@ public class PatriLangFilesPage extends LazyPage {
   private void updateCas() {
     File selectedFile = state.get("selectedFile");
 
-    if (selectedFile == null || plannedCasSet == null || doneCasSet == null) return;
+    if (selectedFile == null || plannedCasSet == null || doneCasSet == null) {
+      return;
+    }
 
-    if (selectedFile.getName().equals(INITIAL_CAS_NAME)) return;
+    if (selectedFile.getName().endsWith(TOUT_CAS_FILE_EXTENSION)) {
+      return;
+    }
 
     var plannedCas = getCasPatriLangFilesPage(selectedFile, plannedCasSet);
     var doneCas = getCasPatriLangFilesPage(selectedFile, doneCasSet);
 
     if (plannedCas == null || doneCas == null) {
-      System.err.println("No cases found for " + selectedFile.getName());
+      log.error("No cases found for " + selectedFile.getName());
       return;
     }
 
@@ -211,7 +221,9 @@ public class PatriLangFilesPage extends LazyPage {
   }
 
   private static Cas getCasPatriLangFilesPage(File file, CasSet casSet) {
-    if (file == null || casSet == null) return null;
+    if (file == null || casSet == null) {
+      return null;
+    }
 
     var fileName = file.getName();
     var baseName = fileName.contains(".") ? fileName.substring(0, fileName.indexOf('.')) : fileName;
