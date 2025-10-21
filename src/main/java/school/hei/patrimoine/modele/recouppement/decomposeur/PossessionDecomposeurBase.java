48fd9fa47709a4b88becfb9e66497d988959586e
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2025-10-21 16:35:56 +0300

    chore: decompose GroupePossession

--- src/main/java/school/hei/patrimoine/modele/recouppement/decomposeur/PossessionDecomposeurBase.java
+++ src/main/java/school/hei/patrimoine/modele/recouppement/decomposeur/PossessionDecomposeurBase.java
@@ -1,16 +1,15 @@
 package school.hei.patrimoine.modele.recouppement.decomposeur;
 
 import java.time.LocalDate;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import school.hei.patrimoine.modele.possession.Possession;
 
 @RequiredArgsConstructor
-public class PossessionDecomposeurBase<T extends Possession> implements PossessionDecomposeur<T> {
-  protected final LocalDate finSimulation;
+public abstract class PossessionDecomposeurBase<ToDecompose, Decomposed>
+    implements PossessionDecomposeur<ToDecompose, Decomposed> {
+  private final LocalDate finSimulation;
 
   @Override
-  public List<T> apply(T possession) {
-    return List.of(possession);
+  public LocalDate getFinSimulation() {
+    return finSimulation;
   }
 }
