3f2cfeac510b7144e2ad2e13621472eeec1e7408
Sheddy <hei.sheddy.2@gmail.com>
2025-10-22 20:35:31 +0300

    chore: fix StateTest syntax

--- src/test/java/school/hei/patrimoine/visualisation/swing/ihm/google/modele/StateTest.java
+++ src/test/java/school/hei/patrimoine/visualisation/swing/ihm/google/modele/StateTest.java
@@ -25,53 +25,52 @@ class StateTest {
 
   @Test
   void update() {
-    AtomicInteger callbackCount = new AtomicInteger(0);
+    var callbackCount = new AtomicInteger(0);
 
     state.subscribe("name", callbackCount::incrementAndGet);
     state.update(Map.of("name", "updated", "value", 30));
 
     assertEquals("updated", state.get("name"));
     assertEquals(30, (int) state.get("value"));
-    assertEquals(1, callbackCount.get(), "Callback should be called once for 'name' update");
+    assertEquals(1, callbackCount.get());
   }
 
   @Test
   void invalidate() {
-    AtomicInteger callbackCount = new AtomicInteger(0);
+    var callbackCount = new AtomicInteger(0);
 
     state.subscribe("value", callbackCount::incrementAndGet);
     state.invalidate(Set.of("value"));
 
     assertNull(state.get("value"));
-    assertEquals(1, callbackCount.get(), "Callback should be called once for 'value' invalidation");
+    assertEquals(1, callbackCount.get());
   }
 
   @Test
-  void testUpdate() {
-    AtomicInteger callbackCount = new AtomicInteger(0);
+  void test_update() {
+    var callbackCount = new AtomicInteger(0);
 
     state.subscribe("value", callbackCount::incrementAndGet);
     state.update("value", 50);
 
     assertEquals(50, (int) state.get("value"));
-    assertEquals(1, callbackCount.get(), "Callback should be called once for 'value' update");
+    assertEquals(1, callbackCount.get());
   }
 
   @Test
   void subscribe() {
-    AtomicInteger callbackCount = new AtomicInteger(0);
+    var callbackCount = new AtomicInteger(0);
 
     state.subscribe(Set.of("name", "value"), callbackCount::incrementAndGet);
 
     state.update("name", "newName");
     state.update("value", 100);
 
-    assertEquals(
-        2, callbackCount.get(), "Callback should be called twice for 'name' and 'value' updates");
+    assertEquals(2, callbackCount.get());
   }
 
   @Test
-  void getData() {
+  void get_data() {
     Map<String, Object> data = state.getData();
     assertEquals("test", data.get("name"));
     assertEquals(25, data.get("value"));
@@ -79,13 +78,12 @@ class StateTest {
   }
 
   @Test
-  void getSubscriptions() {
+  void get_subscriptions() {
     state.subscribe("name", () -> {});
     var subscriptions = state.getSubscriptions();
 
-    assertTrue(subscriptions.containsKey("name"), "Subscriptions should contain key 'name'");
-    assertEquals(
-        1, subscriptions.get("name").size(), "There should be one subscription for 'name'");
-    assertFalse(subscriptions.isEmpty(), "Subscriptions should not be empty");
+    assertTrue(subscriptions.containsKey("name"));
+    assertEquals(1, subscriptions.get("name").size());
+    assertFalse(subscriptions.isEmpty());
   }
 }
