2f447e089f3b0b07739f01daed28a4d77f97dc11
Sheddy <hei.sheddy.2@gmail.com>
2025-10-23 15:16:53 +0300

    test: IdPatriLangGeneratorTest

--- src/test/java/school/hei/patrimoine/patrilang/generator/IdPatriLangGeneratorTest.java
+++ src/test/java/school/hei/patrimoine/patrilang/generator/IdPatriLangGeneratorTest.java
@@ -5,8 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class IdPatriLangGeneratorTest {
+    private final IdPatriLangGenerator subject = new IdPatriLangGenerator();
 
     @Test
     void apply() {
+        var result = subject.apply("  Mon Identifiant Test  ");
+        assertEquals("Mon_Identifiant_Test", result);
+    }
+
+    @Test
+    void apply_empty_string() {
+        var result = subject.apply("     ");
+        assertEquals("", result);
+    }
+
+    @Test
+    void apply_with_special_characters_and_long_spaces() {
+        var result = subject.apply("  Identifiant      @    2024!  ");
+        assertEquals("Identifiant_@_2024!", result);
     }
 }
