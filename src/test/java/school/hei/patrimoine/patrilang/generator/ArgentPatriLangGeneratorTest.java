a919c761a4ed4fc02349cd9b76929cfb3f223c11
Sheddy <hei.sheddy.2@gmail.com>
2025-10-23 14:25:00 +0300

    chore: symbols::setDecimalSeparator on ArgentPatriLangGenerator

---
+++ src/test/java/school/hei/patrimoine/patrilang/generator/ArgentPatriLangGeneratorTest.java
@@ -0,0 +1,16 @@
+package school.hei.patrimoine.patrilang.generator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgentPatriLangGeneratorTest {
+
+    @Test
+    void apply() {
+    }
+
+    @Test
+    void montant() {
+    }
+}
