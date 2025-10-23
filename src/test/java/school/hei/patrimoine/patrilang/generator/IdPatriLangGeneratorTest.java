ab6e23c953ee9a8a160b262ceba88e80e47c57c7
Sheddy <hei.sheddy.2@gmail.com>
2025-10-23 15:16:00 +0300

    chore: update apply to remove long spaces

---
+++ src/test/java/school/hei/patrimoine/patrilang/generator/IdPatriLangGeneratorTest.java
@@ -0,0 +1,12 @@
+package school.hei.patrimoine.patrilang.generator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdPatriLangGeneratorTest {
+
+    @Test
+    void apply() {
+    }
+}
