2a72ab0a14e06582b829c29d227ccee714e37e1b
Mihajatiana <154539464+Mihajatiana-dev@users.noreply.github.com>
2025-10-24 09:16:56 +0300

    feat: use Debouncer class for loading files in RecoupementPage
    * feat: extract debounce logic into reusable Debounce class
    
    * feat: use Debouncer for loading files in RecoupementPage
    
    * refactor: move Debouncer to modele package

---
+++ src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/modele/Debouncer.java
@@ -0,0 +1,21 @@
+package school.hei.patrimoine.visualisation.swing.ihm.google.modele;
+
+import javax.swing.*;
+
+public class Debouncer {
+  private static final int DEFAULT_DELAY = 500;
+  private final Timer timer;
+
+  public Debouncer(int delay, boolean repeat, Runnable runnable) {
+    this.timer = new Timer(delay, (e) -> runnable.run());
+    this.timer.setRepeats(repeat);
+  }
+
+  public Debouncer(Runnable runnable) {
+    this(DEFAULT_DELAY, false, runnable);
+  }
+
+  public void restart() {
+    this.timer.restart();
+  }
+}
