3c319ee67591db35342a9c41361e2cdb11c1249b
Sheddy <hei.sheddy.2@gmail.com>
2025-11-04 16:38:02 +0300

    feat: new component MultiViews & ViewFactory

--- src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/app/MultiViews.java
+++ src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/app/MultiViews.java
@@ -1,37 +1,33 @@
 package school.hei.patrimoine.visualisation.swing.ihm.google.component.app;
 
-import javax.swing.*;
+import static java.util.stream.Collectors.toMap;
+
 import java.awt.*;
 import java.util.Map;
 import java.util.Set;
+import javax.swing.*;
 
-import static java.util.stream.Collectors.toMap;
+public class MultiViews extends JPanel {
+  private final CardLayout cardLayout;
+  private final Map<String, JComponent> dialogs;
 
-public class PageManager extends JPanel {
-    private final CardLayout cardLayout;
-    private final Map<String, JComponent> dialogs;
+  public MultiViews(String defaultPage, Set<JComponent> pages) {
+    this.cardLayout = new CardLayout();
+    this.dialogs = pages.stream().collect(toMap(JComponent::getName, p -> p));
 
-    public PageManager(String defaultPage, Set<JComponent> pages) {
-        this.cardLayout = new CardLayout();
-        this.dialogs = pages.stream().collect(toMap(JComponent::getName, p -> p));
+    setLayout(cardLayout);
 
-        setLayout(cardLayout);
+    pages.forEach(page -> add(page.getName(), page));
 
-        pages.forEach(page -> add(page.getName(), page));
+    navigate(defaultPage);
+  }
 
-        navigate(defaultPage);
+  public void navigate(String name) {
+    if (dialogs.containsKey(name)) {
+      cardLayout.show(this, name);
+      AppContext.getDefault().globalState().update("dialog-page", name);
+    } else {
+      throw new IllegalArgumentException("page " + name + " not found in PageManager");
     }
-
-    public static void navigateTo(PageManager manager, String name) {
-        manager.navigate(name);
-    }
-
-    public void navigate(String name) {
-        if (dialogs.containsKey(name)) {
-            cardLayout.show(this, name);
-            AppContext.getDefault().globalState().update("dialog-page", name);
-        } else {
-            throw new IllegalArgumentException("page " + name + " not found in PageManager");
-        }
-    }
+  }
 }
--- src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/app/ViewFactory.java
+++ src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/app/ViewFactory.java
@@ -1,4 +1,12 @@
 package school.hei.patrimoine.visualisation.swing.ihm.google.component.app;
 
-public class ViewFactory {
+import javax.swing.*;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class ViewFactory {
+  public static JComponent makeView(String name, JComponent component) {
+    component.setName(name);
+    return component;
+  }
 }
