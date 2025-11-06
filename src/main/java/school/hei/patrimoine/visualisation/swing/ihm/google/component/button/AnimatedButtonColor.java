bd0ac3cccfdb7bb02e5e5bb40d45346babf42fd6
Sheddy <hei.sheddy.2@gmail.com>
2025-11-06 16:50:10 +0300

    feat: new components to animate buttons on mouseover

--- src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/button/AnimatedButtonColor.java
+++ src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/button/AnimatedButtonColor.java
@@ -1,4 +1,33 @@
 package school.hei.patrimoine.visualisation.swing.ihm.google.component.button;
 
+import java.awt.*;
+
 public class AnimatedButtonColor {
+  private final Color baseColor1;
+  private final Color baseColor2;
+  private final Color hoverColor1;
+  private final Color hoverColor2;
+
+  public AnimatedButtonColor(
+      Color baseColor1, Color baseColor2, Color hoverColor1, Color hoverColor2) {
+    this.baseColor1 = baseColor1;
+    this.baseColor2 = baseColor2;
+    this.hoverColor1 = hoverColor1;
+    this.hoverColor2 = hoverColor2;
+  }
+
+  public Color getInterpolatedColor1(float t) {
+    return interpolate(baseColor1, hoverColor1, t);
+  }
+
+  public Color getInterpolatedColor2(float t) {
+    return interpolate(baseColor2, hoverColor2, t);
+  }
+
+  private Color interpolate(Color c1, Color c2, float t) {
+    var r = (int) (c1.getRed() + (c2.getRed() - c1.getRed()) * t);
+    var g = (int) (c1.getGreen() + (c2.getGreen() - c1.getGreen()) * t);
+    var b = (int) (c1.getBlue() + (c2.getBlue() - c1.getBlue()) * t);
+    return new Color(r, g, b);
+  }
 }
--- src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/button/HoverAnimator.java
+++ src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/button/HoverAnimator.java
@@ -1,4 +1,28 @@
 package school.hei.patrimoine.visualisation.swing.ihm.google.component.button;
 
+import javax.swing.*;
+import lombok.Getter;
+import lombok.Setter;
+
 public class HoverAnimator {
+  @Getter private float progress = 0f;
+  @Setter private boolean hover = false;
+  private final Timer timer;
+
+  public HoverAnimator(Runnable repaintCallback) {
+    timer =
+        new Timer(
+            15,
+            e -> {
+              var speed = 0.08f;
+              if (hover && progress < 1f) {
+                progress = Math.min(1f, progress + speed);
+                repaintCallback.run();
+              } else if (!hover && progress > 0f) {
+                progress = Math.max(0f, progress - speed);
+                repaintCallback.run();
+              }
+            });
+    timer.start();
+  }
 }
