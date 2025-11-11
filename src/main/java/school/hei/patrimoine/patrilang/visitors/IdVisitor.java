4dbdd37e6c75bb6ae97bc43fd495e778e2c60111
Sheddy <hei.sheddy.2@gmail.com>
2025-11-11 15:46:06 +0300

    fix: debug idVisitor and fix tests

--- src/main/java/school/hei/patrimoine/patrilang/visitors/IdVisitor.java
+++ src/main/java/school/hei/patrimoine/patrilang/visitors/IdVisitor.java
@@ -20,6 +20,6 @@ public class IdVisitor implements SimpleVisitor<IdContext, String> {
     }
 
     var variableValue = this.variableVisitor.apply(ctx.variable());
-    return (baseId).replaceAll("-", "_") + variableValue.value();
+    return ((baseId) + variableValue.value()).replaceAll("-", "_");
   }
 }
