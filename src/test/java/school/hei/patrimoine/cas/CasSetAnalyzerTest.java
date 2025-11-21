7143bcfe00d7fb2cc3b1edeafb1e38415630e3b9
Sheddy <hei.sheddy.2@gmail.com>
2025-11-21 10:52:46 +0300

    test: fix cas_set_objective_achieved_ko on CasSetAnalyserTest

--- src/test/java/school/hei/patrimoine/cas/CasSetAnalyzerTest.java
+++ src/test/java/school/hei/patrimoine/cas/CasSetAnalyzerTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import static school.hei.patrimoine.conf.TestUtils.*;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import school.hei.patrimoine.modele.objectif.ObjectifExeption;
 
 class CasSetAnalyzerTest {
 
@@ -21,11 +21,8 @@ class CasSetAnalyzerTest {
     assertDoesNotThrow(() -> analyzer.accept(casSet1()));
   }
 
-  @Disabled
   @Test
   void cas_set_objective_achieved_ko() {
-    RuntimeException exception =
-        assertThrows(RuntimeException.class, () -> analyzer.accept(casSet2()));
-    assertTrue(exception.getMessage().contains("Objectifs non atteints"));
+    assertThrows(ObjectifExeption.class, () -> analyzer.accept(casSet2()));
   }
 }
