86176dbbca74e4782f31bec6536209c5ef0a6c12
Sheddy <hei.sheddy.2@gmail.com>
2025-11-21 15:14:14 +0300

    fix: message error

--- src/main/java/school/hei/patrimoine/modele/objectif/ObjectifExeption.java
+++ src/main/java/school/hei/patrimoine/modele/objectif/ObjectifExeption.java
@@ -4,8 +4,12 @@ import java.util.Set;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public class ObjectifExeption extends RuntimeException {
   private final Set<ObjectifNonAtteint> objectifNonAtteints;
+
+  public ObjectifExeption(Set<ObjectifNonAtteint> objectifNonAtteints) {
+    super("Les objectifs suivants n'ont pas été atteints : " + objectifNonAtteints);
+    this.objectifNonAtteints = objectifNonAtteints;
+  }
 }
