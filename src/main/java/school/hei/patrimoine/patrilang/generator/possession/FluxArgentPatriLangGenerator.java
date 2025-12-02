1f5a0fa5e6b1eed6c97c6184e93437117088da08
Sheddy <hei.sheddy.2@gmail.com>
2025-12-02 11:51:12 +0300

    feat: apply flux argent typed

--- src/main/java/school/hei/patrimoine/patrilang/generator/possession/FluxArgentPatriLangGenerator.java
+++ src/main/java/school/hei/patrimoine/patrilang/generator/possession/FluxArgentPatriLangGenerator.java
@@ -1,8 +1,13 @@
 package school.hei.patrimoine.patrilang.generator.possession;
 
+import lombok.ToString;
 import school.hei.patrimoine.modele.possession.FluxArgent;
+import school.hei.patrimoine.modele.possession.TypeFEC;
 import school.hei.patrimoine.patrilang.generator.*;
 
+import static school.hei.patrimoine.modele.possession.TypeFEC.*;
+
+@ToString
 public class FluxArgentPatriLangGenerator implements PatriLangGenerator<FluxArgent> {
   private final IdPatriLangGenerator idGenerator;
   private final DatePatriLangGenerator dateGenerator;
@@ -19,10 +24,26 @@ public class FluxArgentPatriLangGenerator implements PatriLangGenerator<FluxArge
   @Override
   public String apply(FluxArgent fluxArgent) {
     if (fluxArgent.getFluxMensuel().lt(0)) {
+      if (fluxArgent.getTypeFEC() != null) {
+        return sortir_type(fluxArgent);
+      }
       return sortir(fluxArgent);
+    } else if (fluxArgent.getTypeFEC() != null) {
+      return entrer_type(fluxArgent);
     }
 
     return entrer(fluxArgent);
+  }
+
+  private TypeFEC getTypeFECString(String s) {
+    var raw = s.trim().toUpperCase();
+    return switch (raw) {
+      case "Charge", "CHG" -> CHARGE;
+      case "Produit", "PRD" -> PRODUIT;
+      case "Immobilisation", "IMMO" -> IMMOBILISATION;
+      case "CCA" -> CCA;
+      default -> AUTRE;
+    };
   }
 
   private String sortir(FluxArgent fluxArgent) {
@@ -43,5 +64,27 @@ public class FluxArgentPatriLangGenerator implements PatriLangGenerator<FluxArge
     var type = variableTypeGenerator.apply(fluxArgent.getCompte());
 
     return String.format("* `%s`, %s entrer %s vers %s:%s", nom, date, argent, type, compte);
+  }
+
+  private String sortir_type(FluxArgent fluxArgent) {
+    var alias = getTypeFECString(fluxArgent.getTypeFEC().name());
+    var nomTypé = idGenerator.apply("[" + alias + "]_" + fluxArgent.nom());
+    var date = dateGenerator.apply(fluxArgent.t());
+    var argent = argentGenerator.apply(fluxArgent.getFluxMensuel().mult(-1));
+    var type = variableTypeGenerator.apply(fluxArgent.getCompte());
+    var compte = fluxArgent.getCompte().nom();
+
+    return String.format("* `%s`, %s sortir %s depuis %s:%s", nomTypé, date, argent, type, compte);
+  }
+
+  private String entrer_type(FluxArgent fluxArgent) {
+    var alias = getTypeFECString(fluxArgent.getTypeFEC().name());
+    var nomTypé = "[" + alias + "]_" + fluxArgent.nom();
+    var compte = fluxArgent.getCompte().nom();
+    var date = dateGenerator.apply(fluxArgent.t());
+    var argent = argentGenerator.apply(fluxArgent.getFluxMensuel());
+    var type = variableTypeGenerator.apply(fluxArgent.getCompte());
+
+    return String.format("* `%s`, %s entrer %s vers %s:%s", nomTypé, date, argent, type, compte);
   }
 }
