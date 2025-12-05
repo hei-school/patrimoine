9b87e208467150c568d945be0b3dc346873fb771
Sheddy <hei.sheddy.2@gmail.com>
2025-12-05 16:23:13 +0300

    feat(visitor): pieces justificative visitor

--- src/main/java/school/hei/patrimoine/patrilang/visitors/possession/pj/PieceJustfificativeVisitor.java
+++ src/main/java/school/hei/patrimoine/patrilang/visitors/possession/pj/PieceJustfificativeVisitor.java
@@ -1,21 +1,22 @@
 package school.hei.patrimoine.patrilang.visitors.possession.pj;
 
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
-
+import school.hei.patrimoine.modele.possession.pj.PiecesJustificative;
 import school.hei.patrimoine.patrilang.antlr.PatriLangParser.PieceJustificativeContext;
 import school.hei.patrimoine.patrilang.visitors.IdVisitor;
 import school.hei.patrimoine.patrilang.visitors.variable.VariableVisitor;
 
 @RequiredArgsConstructor
-public class PjVisitor {
-    private final VariableVisitor variableVisitor;
-    private final IdVisitor idVisitor;
+public class PieceJustfificativeVisitor {
+  private final VariableVisitor variableVisitor;
+  private final IdVisitor idVisitor;
 
-    public String apply(PieceJustificativeContext ctx) {
-        String idFlux = this.idVisitor.apply(ctx.id());
-        String dateEmission = this.variableVisitor.asDate(ctx.dateValue).toString();
-        String driveLink = ctx.pjUrl.getText();
+  public PiecesJustificative apply(PieceJustificativeContext ctx) {
+    String idFlux = this.idVisitor.apply(ctx.id());
+    LocalDate dateEmission = this.variableVisitor.asDate(ctx.dateValue);
+    String driveLink = ctx.pjUrl.getText();
 
-        return String.format("* `%s`, %s, %s", idFlux, dateEmission, driveLink);
-    }
+    return new PiecesJustificative(idFlux, dateEmission, driveLink);
+  }
 }
