730c0ccaceccb3ed69113d8651fa7f55aa21e690
Sheddy <hei.sheddy.2@gmail.com>
2026-01-09 13:02:17 +0300

    test: supporting document recouped test

--- src/test/java/school/hei/patrimoine/modele/recouppement/RecoupeurDePieceJustificativeTest.java
+++ src/test/java/school/hei/patrimoine/modele/recouppement/RecoupeurDePieceJustificativeTest.java
@@ -1,4 +1,88 @@
 package school.hei.patrimoine.modele.recouppement;
 
-public class RecoupeurDePieceJustificativeTest {
+import static java.time.Month.JANUARY;
+import static java.time.Month.MARCH;
+import static org.junit.jupiter.api.Assertions.*;
+import static school.hei.patrimoine.modele.Argent.ariary;
+
+import java.time.LocalDate;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import school.hei.patrimoine.modele.possession.Compte;
+import school.hei.patrimoine.modele.possession.FluxArgent;
+import school.hei.patrimoine.modele.possession.pj.PieceJustificative;
+
+class RecoupeurDePieceJustificativeTest {
+  @Test
+  void associe_possession_et_piece_justificative_quand_id_correspond_au_nom() {
+    var date = LocalDate.of(2025, JANUARY, 1);
+
+    var compte = new Compte("comptePersonnel", date, ariary(0));
+    var salaire = new FluxArgent("salaire", compte, date, ariary(200));
+
+    var pj = new PieceJustificative("salaire", date, "http://example.com/salaire.pdf");
+
+    var subject =
+        new RecoupeurDePieceJustificative(Set.of(pj), Set.of(compte, salaire), LocalDate.now());
+
+    var possessionsAvecPj = subject.getClass().getDeclaredMethods();
+  }
+
+  @Test
+  void trouve_piece_justificative_pour_possession() {
+    var date = LocalDate.of(2025, JANUARY, 1);
+
+    var compte = new Compte("comptePersonnel", date, ariary(0));
+    var salaire = new FluxArgent("salaire", compte, date, ariary(200));
+
+    var pj = new PieceJustificative("salaire", date, "http://example.com/salaire.pdf");
+
+    var subject =
+        new RecoupeurDePieceJustificative(Set.of(pj), Set.of(compte, salaire), LocalDate.now());
+
+    System.out.println(salaire);
+    System.out.println(pj);
+    var actual = subject.getPossessionWithPj();
+    System.out.println(actual);
+
+    assertEquals(13, actual.size());
+
+    var association = actual.iterator().next();
+    assertEquals("salaire", association.possession().nom());
+    assertEquals("salaire__du_2025_12_01", association.pieceJustificative().id());
+  }
+
+  @Test
+  void detecte_possession_sans_piece_justificative() {
+    var date = LocalDate.of(2025, JANUARY, 1);
+
+    var compte = new Compte("comptePersonnel", date, ariary(0));
+    var salaire = new FluxArgent("salaire", compte, date, ariary(200));
+
+    var subject =
+        new RecoupeurDePieceJustificative(Set.of(), Set.of(compte, salaire), LocalDate.now());
+
+    var actual = subject.getPossessionWithPj();
+
+    assertTrue(actual.isEmpty());
+  }
+
+  @Test
+  void recoupe_apres_decomposition_des_possessions() {
+    var au01Janvier2025 = LocalDate.of(2025, JANUARY, 1);
+    var au02Mars2025 = LocalDate.of(2025, MARCH, 2);
+
+    var compte = new Compte("comptePersonnel", au01Janvier2025, ariary(0));
+
+    var flux = new FluxArgent("salaire", compte, au01Janvier2025, au02Mars2025, 1, ariary(200));
+
+    var pj = new PieceJustificative("salaire", au01Janvier2025, "http://example.com/salaire.pdf");
+
+    var subject =
+        new RecoupeurDePieceJustificative(Set.of(pj), Set.of(compte, flux), LocalDate.now());
+
+    var actual = subject.getPossessionWithPj();
+    System.out.println(actual);
+    assertFalse(actual.isEmpty());
+  }
 }
