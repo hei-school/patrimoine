c8f6972fdd7b2c4c05418547b4347f13903f5140
mihajatiana <hei.mihajatiana@gmail.com>
2026-01-09 14:59:45 +0300

    feat: add getCategory method to classify files

--- src/main/java/school/hei/patrimoine/patrilang/files/PatriLangFileContext.java
+++ src/main/java/school/hei/patrimoine/patrilang/files/PatriLangFileContext.java
@@ -1,13 +1,40 @@
 package school.hei.patrimoine.patrilang.files;
 
+import static school.hei.patrimoine.visualisation.swing.ihm.google.modele.FileCategory.*;
+
+import school.hei.patrimoine.visualisation.swing.ihm.google.modele.FileCategory;
+
 public class PatriLangFileContext {
   private final PatriLangFileWritter.FileWritterInput input;
+  private final String filePath;
 
   public PatriLangFileContext(PatriLangFileWritter.FileWritterInput input) {
     this.input = input;
+    this.filePath = input.file().getAbsolutePath().replace("\\", "/");
   }
 
-  public boolean isPlanned() {
-    return input.file().getAbsolutePath().replace("\\", "/").contains("/download/planifies");
+  public FileCategory getCategory() {
+    if (isJustificative()) {
+      return JUSTIFICATIVE;
+    } else if (isPlanned()) {
+      return PLANNED;
+    } else if (isDone()) {
+      return DONE;
+    } else {
+      throw new IllegalStateException(
+          "File path does not match any category: " + input.file().getAbsolutePath());
+    }
+  }
+
+  private boolean isJustificative() {
+    return filePath.contains("/download/justificatifs");
+  }
+
+  private boolean isPlanned() {
+    return filePath.contains("/download/planifies");
+  }
+
+  private boolean isDone() {
+    return filePath.contains("/download/realises");
   }
 }
