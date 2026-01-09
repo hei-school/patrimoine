1227ef0ecfbcde4e832d3e41f73897a58b05e9de
mihajatiana <hei.mihajatiana@gmail.com>
2026-01-09 13:51:05 +0300

    feat: extract file name for the pj file

--- src/main/java/school/hei/patrimoine/compiler/PatriLangFileNameExtractor.java
+++ src/main/java/school/hei/patrimoine/compiler/PatriLangFileNameExtractor.java
@@ -1,7 +1,6 @@
 package school.hei.patrimoine.compiler;
 
-import static school.hei.patrimoine.patrilang.PatriLangTranspiler.CAS_FILE_EXTENSION;
-import static school.hei.patrimoine.patrilang.PatriLangTranspiler.TOUT_CAS_FILE_EXTENSION;
+import static school.hei.patrimoine.patrilang.PatriLangTranspiler.*;
 
 import java.util.List;
 import java.util.regex.Matcher;
@@ -15,8 +14,28 @@ public class PatriLangFileNameExtractor implements FileNameExtractor {
   private static final List<Pattern> CAS_FILE_NAME_PATTERNS =
       List.of(Pattern.compile("Cas de (\\w+)"), Pattern.compile("Cas De (\\w+)"));
 
+  private static final List<Pattern> PJ_FILE_NAME_PATTERNS =
+      List.of(
+          Pattern.compile("Pièces justificatives"),
+          Pattern.compile("Pièces Justificatives"),
+          Pattern.compile("Pieces justificatives"),
+          Pattern.compile("Pieces Justificatives"));
+
   @Override
   public String apply(String code) {
+    for (Pattern pattern : PJ_FILE_NAME_PATTERNS) {
+      Matcher matcher = pattern.matcher(code);
+      if (matcher.find()) {
+        for (Pattern casPattern : CAS_FILE_NAME_PATTERNS) {
+          Matcher casNameMatcher = casPattern.matcher(code);
+          if (casNameMatcher.find()) {
+            return casNameMatcher.group(1) + PJ_FILE_EXTENSION;
+          }
+        }
+        throw new IllegalArgumentException("PJ file without 'Cas de ...' declaration.");
+      }
+    }
+
     for (Pattern pattern : CAS_FILE_NAME_PATTERNS) {
       Matcher matcher = pattern.matcher(code);
 
