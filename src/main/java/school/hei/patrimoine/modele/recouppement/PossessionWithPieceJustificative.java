0f6d3c485282ece044e1c3df18d14846732956cf
Sheddy <hei.sheddy.2@gmail.com>
2026-01-09 12:57:37 +0300

    feat: new record PossessionWithPieceJustificative

--- src/main/java/school/hei/patrimoine/modele/recouppement/PossessionWithPieceJustificative.java
+++ src/main/java/school/hei/patrimoine/modele/recouppement/PossessionWithPieceJustificative.java
@@ -1,4 +1,7 @@
 package school.hei.patrimoine.modele.recouppement;
 
-public record PossessionWithPieceJustificative() {
-}
+import school.hei.patrimoine.modele.possession.Possession;
+import school.hei.patrimoine.modele.possession.pj.PieceJustificative;
+
+public record PossessionWithPieceJustificative<T extends Possession>(
+    T possession, PieceJustificative pieceJustificative) {}
