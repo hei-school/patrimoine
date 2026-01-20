966e9bd476cdbd535851a5647097f993f539b16c
Sheddy <hei.sheddy.2@gmail.com>
2026-01-20 10:28:48 +0300

    feat: abstract method for possessions

--- src/main/java/school/hei/patrimoine/modele/possession/Possession.java
+++ src/main/java/school/hei/patrimoine/modele/possession/Possession.java
@@ -62,6 +62,8 @@ public abstract sealed class Possession extends Objectivable
 
   public abstract TypeAgregat typeAgregat();
 
+  public abstract TypeFEC getTypeFEC();
+
   @Override
   public String nom() {
     return nom;
