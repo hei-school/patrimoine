9ad13cb2dd26f2e96213a6d9632f51af9f7d9a4c
Sheddy <hei.sheddy.2@gmail.com>
2026-01-23 09:11:05 +0300

    fix: FluxArgentGenerator and tests

--- src/main/java/school/hei/patrimoine/patrilang/generator/possession/FluxArgentPatriLangGenerator.java
+++ src/main/java/school/hei/patrimoine/patrilang/generator/possession/FluxArgentPatriLangGenerator.java
@@ -9,6 +9,8 @@ public class FluxArgentPatriLangGenerator implements PatriLangGenerator<FluxArge
   private final ArgentPatriLangGenerator argentGenerator;
   private final VariableTypePatriLangGenerator variableTypeGenerator;
 
+  private static final String SEPARATEUR_ID = "_";
+
   public FluxArgentPatriLangGenerator() {
     this.idGenerator = new IdPatriLangGenerator();
     this.dateGenerator = new DatePatriLangGenerator();
@@ -19,9 +21,8 @@ public class FluxArgentPatriLangGenerator implements PatriLangGenerator<FluxArge
   @Override
   public String apply(FluxArgent fluxArgent) {
     var isSortie = fluxArgent.getFluxMensuel().lt(0);
-    var hasType = fluxArgent.getTypeFEC() != null;
 
-    var nom = buildNom(fluxArgent, hasType);
+    var nom = buildNom(fluxArgent);
     var date = dateGenerator.apply(fluxArgent.t());
     var type = variableTypeGenerator.apply(fluxArgent.getCompte());
     var compte = fluxArgent.getCompte().nom();
@@ -35,13 +36,13 @@ public class FluxArgentPatriLangGenerator implements PatriLangGenerator<FluxArge
     return String.format("* `%s`, %s entrer %s vers %s:%s", nom, date, argent, type, compte);
   }
 
-  private String buildNom(FluxArgent fluxArgent, boolean hasType) {
-    if (!hasType) {
+  private String buildNom(FluxArgent fluxArgent) {
+    if (fluxArgent.getTypeFEC() == null) {
       return idGenerator.apply(fluxArgent.nom());
     }
 
     var type = fluxArgent.getTypeFEC();
-    var nomTypé = "[" + type + "]_" + fluxArgent.nom();
+    var nomTypé = type + SEPARATEUR_ID + fluxArgent.nom();
     return idGenerator.apply(nomTypé);
   }
 }
