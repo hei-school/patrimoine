dfed653cd29531bdece594d6e64b68e40fa33b80
Sheddy <hei.sheddy.2@gmail.com>
2026-01-29 13:25:48 +0300

    fix: remove prefix fec type on generating patrilang fluxArgent id

--- src/test/java/school/hei/patrimoine/patrilang/generator/possession/FluxArgentPatriLangGeneratorTest.java
+++ src/test/java/school/hei/patrimoine/patrilang/generator/possession/FluxArgentPatriLangGeneratorTest.java
@@ -10,7 +10,6 @@ import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 import school.hei.patrimoine.modele.possession.Compte;
 import school.hei.patrimoine.modele.possession.FluxArgent;
-import school.hei.patrimoine.modele.possession.TypeFEC;
 
 class FluxArgentPatriLangGeneratorTest {
   private static final FluxArgentPatriLangGenerator subject = new FluxArgentPatriLangGenerator();
@@ -18,20 +17,20 @@ class FluxArgentPatriLangGeneratorTest {
   private static final Compte compte = new Compte("comptePersonnel", date, ariary(5000));
 
   @Test
-  void test_entree_type_produit_par_defaut() {
+  void test_entree() {
     var flux = new FluxArgent("flux1", compte, date, ariary(1000));
     var actual = subject.apply(flux);
-    var expected =
-        "* `PRODUIT_flux1`, le 1 janvier 2025 entrer 1_000Ar vers Trésoreries:comptePersonnel";
+    var expected = "* `flux1`, le 1 janvier 2025 entrer 1_000Ar vers Trésoreries:comptePersonnel";
+
     assertEquals(expected, actual);
   }
 
   @Test
-  void test_sortie_type_produit_par_defaut() {
+  void test_sortie() {
     var flux = new FluxArgent("flux1", compte, date, ariary(-1000));
     var actual = subject.apply(flux);
-    var expected =
-        "* `CHARGE_flux1`, le 1 janvier 2025 sortir 1_000Ar depuis Trésoreries:comptePersonnel";
+    var expected = "* `flux1`, le 1 janvier 2025 sortir 1_000Ar depuis Trésoreries:comptePersonnel";
+
     assertEquals(expected, actual);
   }
 
@@ -39,85 +38,8 @@ class FluxArgentPatriLangGeneratorTest {
   void test_zero_flux() {
     var flux = new FluxArgent("flux1", compte, date, ariary(0));
     var actual = subject.apply(flux);
-    var expected =
-        "* `PRODUIT_flux1`, le 1 janvier 2025 entrer 0Ar vers Trésoreries:comptePersonnel";
+    var expected = "* `flux1`, le 1 janvier 2025 entrer 0Ar vers Trésoreries:comptePersonnel";
     assertEquals(expected, actual);
-  }
-
-  @Test
-  void test_entree_avec_type_fec() {
-    var flux = new FluxArgent("flux1", compte, date, ariary(1_000), PRODUIT);
-    var actual = subject.apply(flux);
-    var expected =
-        "* `PRODUIT_flux1`, le 1 janvier 2025 entrer 1_000Ar vers Trésoreries:comptePersonnel";
-
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  void test_sortie_avec_type_fec() {
-    var flux = new FluxArgent("flux1", compte, date, ariary(-1_000), CHARGE);
-    var actual = subject.apply(flux);
-    var expected =
-        "* `CHARGE_flux1`, le 1 janvier 2025 sortir 1_000Ar depuis Trésoreries:comptePersonnel";
-
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  void test_zero_flux_avec_type_fec() {
-    var flux = new FluxArgent("flux1", compte, date, ariary(0), AUTRE);
-    var actual = subject.apply(flux);
-    var expected = "* `AUTRE_flux1`, le 1 janvier 2025 entrer 0Ar vers Trésoreries:comptePersonnel";
-
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  void test_entree_avec_tous_les_types_fec() {
-    for (var type : TypeFEC.values()) {
-      var flux = new FluxArgent("fluxType", compte, date, ariary(500), type);
-      var actual = subject.apply(flux);
-      var expected =
-          "* `"
-              + type
-              + "_fluxType`, le 1 janvier 2025 entrer 500Ar vers Trésoreries:comptePersonnel";
-
-      assertEquals(expected, actual);
-    }
-  }
-
-  @Test
-  void test_sortie_avec_tous_les_types_fec() {
-    for (var type : TypeFEC.values()) {
-      var flux = new FluxArgent("fluxType", compte, date, ariary(-750), type);
-      var actual = subject.apply(flux);
-      var expected =
-          "* `"
-              + type
-              + "_fluxType`, le 1 janvier 2025 sortir 750Ar depuis Trésoreries:comptePersonnel";
-      assertEquals(expected, actual);
-    }
-  }
-
-  @Test
-  void test_nom_avec_type_est_prefixe() {
-    var flux = new FluxArgent("nomSimple", compte, date, ariary(100), IMMOBILISATION);
-    var actual = subject.apply(flux);
-    var expected = extractId(actual);
-
-    assertEquals("IMMOBILISATION_nomSimple", expected);
-  }
-
-  @Test
-  void test_nom_sans_type_est_id_genere() {
-    var flux = new FluxArgent("nomSimple", compte, date, ariary(100));
-
-    var actual = subject.apply(flux);
-
-    var expected = extractId(actual);
-
-    assertEquals("PRODUIT_nomSimple", expected);
   }
 
   @Test
@@ -127,17 +49,6 @@ class FluxArgentPatriLangGeneratorTest {
     var second = subject.apply(flux);
 
     assertEquals(first, second);
-  }
-
-  @Test
-  void test_compte_different() {
-    var autreCompte = new Compte("compteEpargne", date, ariary(10_000));
-    var flux = new FluxArgent("flux", autreCompte, date, ariary(200));
-    var actual = subject.apply(flux);
-    var expected =
-        "* `PRODUIT_flux`, le 1 janvier 2025 entrer 200Ar vers Trésoreries:compteEpargne";
-
-    assertEquals(expected, actual);
   }
 
   @Test
