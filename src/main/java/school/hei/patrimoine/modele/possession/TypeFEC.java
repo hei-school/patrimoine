91b9c8f37afa11b441990b2895457ab86172288d
Sheddy <hei.sheddy.2@gmail.com>
2026-02-04 16:16:00 +0300

    chore: add method for abbreviating FEC types

--- src/main/java/school/hei/patrimoine/modele/possession/TypeFEC.java
+++ src/main/java/school/hei/patrimoine/modele/possession/TypeFEC.java
@@ -1,9 +1,19 @@
 package school.hei.patrimoine.modele.possession;
 
 public enum TypeFEC {
-  CCA,
-  PRODUIT,
-  IMMOBILISATION,
-  CHARGE,
-  AUTRE
+  CCA("CCA"),
+  PRODUIT("PRD"),
+  IMMOBILISATION("IMMO"),
+  CHARGE("CHG"),
+  AUTRE("AUTRE");
+
+  private final String abrev;
+
+  TypeFEC(String abrev) {
+    this.abrev = abrev;
+  }
+
+  public String abrev() {
+    return abrev;
+  }
 }
