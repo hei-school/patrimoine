39d51a46f868d4e60248e79e3ba9c2544811b30a
Sheddy <hei.sheddy.2@gmail.com>
2026-02-04 16:11:22 +0300

    feat: add enum for mapping typeFEC to codeFEC with timezone

--- src/main/java/school/hei/patrimoine/modele/possession/FECCodeResolver.java
+++ src/main/java/school/hei/patrimoine/modele/possession/FECCodeResolver.java
@@ -1,4 +1,21 @@
 package school.hei.patrimoine.modele.possession;
 
+import lombok.NoArgsConstructor;
+import school.hei.patrimoine.modele.possession.enumFEC.RegionComptable;
+import school.hei.patrimoine.modele.possession.enumFEC.codeFEC;
+
+@NoArgsConstructor
 public class FECCodeResolver {
+  public static String resolve(TypeFEC type, RegionComptable region) {
+    return switch (type) {
+      case CCA ->
+          region == RegionComptable.MADAGASCAR ? codeFEC.CCA_MG.code() : codeFEC.CCA_FR.code();
+
+      case PRODUIT -> codeFEC.PRODUIT.code();
+      case CHARGE -> codeFEC.CHARGE.code();
+      case IMMOBILISATION -> codeFEC.IMMOBILISATION.code();
+
+      default -> codeFEC.AUTRE.code();
+    };
+  }
 }
--- src/main/java/school/hei/patrimoine/modele/possession/enumFEC/codeFEC.java
+++ src/main/java/school/hei/patrimoine/modele/possession/enumFEC/codeFEC.java
@@ -1,4 +1,22 @@
 package school.hei.patrimoine.modele.possession.enumFEC;
 
 public enum codeFEC {
+  CCA_FR("401"),
+  CCA_MG("411"),
+
+  PRODUIT("701"),
+  CHARGE("601"),
+  IMMOBILISATION("215"),
+
+  AUTRE("000");
+
+  private final String code;
+
+  codeFEC(String number) {
+    this.code = number;
+  }
+
+  public String code() {
+    return code;
+  }
 }
