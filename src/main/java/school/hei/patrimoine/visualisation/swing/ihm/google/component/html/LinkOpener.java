4e57755c0a27f2200a75dc94a004d1831b6ddc85
mihajatiana <hei.mihajatiana@gmail.com>
2026-02-04 14:46:05 +0300

    fix: handle unsupported desktop

--- src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/html/LinkOpener.java
+++ src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/html/LinkOpener.java
@@ -8,11 +8,23 @@ public class LinkOpener implements Consumer<String> {
   @Override
   public void accept(String url) {
     try {
-      if (!Desktop.isDesktopSupported()) {
-        throw new UnsupportedOperationException("Desktop API not supported");
+      if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+        Desktop.getDesktop().browse(new URI(url));
+        return;
       }
 
-      Desktop.getDesktop().browse(URI.create(url));
+      String os = System.getProperty("os.name").toLowerCase();
+      ProcessBuilder processBuilder;
+
+      if (os.contains("win")) {
+        processBuilder = new ProcessBuilder("cmd", "/c", "start", "", url);
+      } else if (os.contains("mac")) {
+        processBuilder = new ProcessBuilder("open", url);
+      } else {
+        processBuilder = new ProcessBuilder("xdg-open", url);
+      }
+
+      processBuilder.start();
     } catch (Exception e) {
       throw new RuntimeException("Failed to open link: " + url, e);
     }
