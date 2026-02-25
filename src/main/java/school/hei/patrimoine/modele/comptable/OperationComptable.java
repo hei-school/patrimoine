6475caa73042f4fdd34c07720a18b43105ee401e
Sheddy <hei.sheddy.2@gmail.com>
2026-02-25 14:54:40 +0300

    chore: refactor constructor and create method make to determine accounting type

--- src/main/java/school/hei/patrimoine/modele/comptable/OperationComptable.java
+++ src/main/java/school/hei/patrimoine/modele/comptable/OperationComptable.java
@@ -3,7 +3,12 @@ package school.hei.patrimoine.modele.comptable;
 import school.hei.patrimoine.modele.possession.Possession;
 
 public record OperationComptable(Possession possession, TypeComptable typeComptable) {
-  public OperationComptable(Possession possession) {
-    this(possession, TypeComptable.from(possession));
+  public OperationComptable(Possession possession, TypeComptable typeComptable) {
+    this.possession = possession;
+    this.typeComptable = typeComptable;
+  }
+
+  public static OperationComptable make(Possession possession) {
+    return new OperationComptable(possession, TypeComptable.from(possession));
   }
 }
