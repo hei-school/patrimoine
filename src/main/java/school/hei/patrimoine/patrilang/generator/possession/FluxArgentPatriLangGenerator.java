f1865efdbe69cad34b940752aa64f6804a774c4e
Sheddy <hei.sheddy.2@gmail.com>
2026-02-26 17:30:22 +0300

    refactor: simplify FluxArgentPatriLangGenerator

--- src/main/java/school/hei/patrimoine/patrilang/generator/possession/FluxArgentPatriLangGenerator.java
+++ src/main/java/school/hei/patrimoine/patrilang/generator/possession/FluxArgentPatriLangGenerator.java
@@ -18,29 +18,18 @@ public class FluxArgentPatriLangGenerator implements PatriLangGenerator<FluxArge
 
   @Override
   public String apply(FluxArgent fluxArgent) {
-    if (fluxArgent.getFluxMensuel().lt(0)) {
-      return sortir(fluxArgent);
-    }
-
-    return entrer(fluxArgent);
-  }
-
-  private String sortir(FluxArgent fluxArgent) {
+    var isSortie = fluxArgent.getFluxMensuel().lt(0);
     var nom = idGenerator.apply(fluxArgent.nom());
     var date = dateGenerator.apply(fluxArgent.t());
-    var argent = argentGenerator.apply(fluxArgent.getFluxMensuel().mult(-1));
     var type = variableTypeGenerator.apply(fluxArgent.getCompte());
     var compte = fluxArgent.getCompte().nom();
 
-    return String.format("* `%s`, %s sortir %s depuis %s:%s", nom, date, argent, type, compte);
-  }
+    if (isSortie) {
+      var argent = argentGenerator.apply(fluxArgent.getFluxMensuel().mult(-1));
+      return String.format("* `%s`, %s sortir %s depuis %s:%s", nom, date, argent, type, compte);
+    }
 
-  private String entrer(FluxArgent fluxArgent) {
-    var nom = fluxArgent.nom();
-    var compte = fluxArgent.getCompte().nom();
-    var date = dateGenerator.apply(fluxArgent.t());
     var argent = argentGenerator.apply(fluxArgent.getFluxMensuel());
-    var type = variableTypeGenerator.apply(fluxArgent.getCompte());
 
     return String.format("* `%s`, %s entrer %s vers %s:%s", nom, date, argent, type, compte);
   }
