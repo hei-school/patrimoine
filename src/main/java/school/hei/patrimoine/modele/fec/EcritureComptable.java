a8a032b52b4a48e6de0f665f21350dbf0aa9cadd
mihajatiana <hei.mihajatiana@gmail.com>
2026-02-27 23:38:56 +0300

    fix: move pj in EcritureComptable

--- src/main/java/school/hei/patrimoine/modele/fec/EcritureComptable.java
+++ src/main/java/school/hei/patrimoine/modele/fec/EcritureComptable.java
@@ -3,11 +3,13 @@ package school.hei.patrimoine.modele.fec;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Builder;
+import school.hei.patrimoine.modele.possession.pj.PieceJustificative;
 
 @Builder
 public record EcritureComptable(
     String id,
     LocalDate date,
     String libelle,
+    LocalDate dateValidation,
     List<LigneEcriture> lignes,
-    LocalDate dateValidation) {}
+    PieceJustificative pieceJustificative) {}
