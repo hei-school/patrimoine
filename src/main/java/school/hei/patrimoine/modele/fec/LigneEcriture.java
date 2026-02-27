61fee24b21b0007a1a5b6862d543f8dc7430ae05
mihajatiana <hei.mihajatiana@gmail.com>
2026-02-27 23:38:01 +0300

    fix: remove pj field and add type

--- src/main/java/school/hei/patrimoine/modele/fec/LigneEcriture.java
+++ src/main/java/school/hei/patrimoine/modele/fec/LigneEcriture.java
@@ -3,14 +3,14 @@ package school.hei.patrimoine.modele.fec;
 import java.time.LocalDate;
 import lombok.Builder;
 import school.hei.patrimoine.modele.Argent;
+import school.hei.patrimoine.modele.comptable.TypeComptable;
 import school.hei.patrimoine.modele.possession.Compte;
-import school.hei.patrimoine.modele.possession.pj.PieceJustificative;
 
 @Builder
 public record LigneEcriture(
     Compte compte,
     Compte compteAuxiliaire,
-    PieceJustificative pieceJustificative,
     Argent flux,
     String lettrage,
-    LocalDate dateLettrage) {}
+    LocalDate dateLettrage,
+    TypeComptable type) {}
