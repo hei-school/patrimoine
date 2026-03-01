51f6b2952b78f0d05d6150e11daa2706fe042a6a
mihajatiana <hei.mihajatiana@gmail.com>
2026-03-01 18:22:52 +0300

    refactor: move the ternary condition into a method

--- src/main/java/school/hei/patrimoine/modele/fec/PossessionCompteResolver.java
+++ src/main/java/school/hei/patrimoine/modele/fec/PossessionCompteResolver.java
@@ -2,6 +2,7 @@ package school.hei.patrimoine.modele.fec;
 
 import static java.time.LocalDate.now;
 
+import org.jspecify.annotations.NonNull;
 import school.hei.patrimoine.modele.Argent;
 import school.hei.patrimoine.modele.comptable.OperationComptable;
 import school.hei.patrimoine.modele.possession.*;
@@ -15,10 +16,7 @@ public class PossessionCompteResolver {
 
   public static Comptes resolve(OperationComptable operation) {
     return switch (operation.possession()) {
-      case FluxArgent flux ->
-          flux.getFluxMensuel().montant() < 0
-              ? new Comptes(COMPTE_ATTENTE, flux.getCompte())
-              : new Comptes(flux.getCompte(), COMPTE_ATTENTE);
+      case FluxArgent flux -> getComptes(flux);
       case TransfertArgent transfert ->
           new Comptes(transfert.versCompte(), transfert.depuisCompte());
       case Compte compte -> new Comptes(compte, CAPITAL_SOCIAL);
@@ -29,6 +27,13 @@ public class PossessionCompteResolver {
           throw new IllegalArgumentException(
               "Impossible de déterminer les comptes pour " + operation.getClass().getSimpleName());
     };
+  }
+
+  private static @NonNull Comptes getComptes(FluxArgent flux) {
+    if (flux.getFluxMensuel().montant() < 0) {
+      return new Comptes(COMPTE_ATTENTE, flux.getCompte());
+    }
+    return new Comptes(flux.getCompte(), COMPTE_ATTENTE);
   }
 
   public record Comptes(Compte compteDébiteur, Compte compteCréditeur) {}
