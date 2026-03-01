e90a7b4799aa2ce75c1682ab7e0b33a4934efae0
mihajatiana <hei.mihajatiana@gmail.com>
2026-03-01 18:14:41 +0300

    test: import static ariary method

--- src/test/java/school/hei/patrimoine/modele/fec/JournalTest.java
+++ src/test/java/school/hei/patrimoine/modele/fec/JournalTest.java
@@ -2,11 +2,11 @@ package school.hei.patrimoine.modele.fec;
 
 import static java.time.LocalDate.now;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static school.hei.patrimoine.modele.Argent.ariary;
 import static school.hei.patrimoine.modele.fec.JournalCode.JN;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import school.hei.patrimoine.modele.Argent;
 import school.hei.patrimoine.modele.comptable.OperationComptable;
 import school.hei.patrimoine.modele.possession.Compte;
 import school.hei.patrimoine.modele.possession.FluxArgent;
@@ -21,8 +21,8 @@ class JournalTest {
 
   @Test
   void ecriture_of_journal_should_contain_at_least_two_ligne_ecriture() {
-    var compte = new Compte("Compte principal", now(), Argent.ariary(0));
-    var possession = new FluxArgent("Achats", compte, now(), now(), 1, Argent.ariary(4000));
+    var compte = new Compte("Compte principal", now(), ariary(0));
+    var possession = new FluxArgent("Achats", compte, now(), now(), 1, ariary(4000));
     var operation = OperationComptable.make(possession);
 
     subject.addEcriture(operation, null);
@@ -33,8 +33,8 @@ class JournalTest {
 
   @Test
   void ecriture_ids_should_be_sequential() {
-    var compte = new Compte("Compte principal", now(), Argent.ariary(0));
-    var possession = new FluxArgent("Achats", compte, now(), now(), 1, Argent.ariary(4000));
+    var compte = new Compte("Compte principal", now(), ariary(0));
+    var possession = new FluxArgent("Achats", compte, now(), now(), 1, ariary(4000));
     var operation = OperationComptable.make(possession);
 
     subject.addEcriture(operation, null);
--- src/test/java/school/hei/patrimoine/modele/fec/factory/JournalFactoryTest.java
+++ src/test/java/school/hei/patrimoine/modele/fec/factory/JournalFactoryTest.java
@@ -2,20 +2,20 @@ package school.hei.patrimoine.modele.fec.factory;
 
 import static java.time.LocalDate.now;
 import static org.junit.jupiter.api.Assertions.*;
+import static school.hei.patrimoine.modele.Argent.ariary;
 import static school.hei.patrimoine.modele.fec.JournalCode.JN;
 
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import school.hei.patrimoine.modele.Argent;
 import school.hei.patrimoine.modele.comptable.OperationComptable;
 import school.hei.patrimoine.modele.possession.Compte;
 import school.hei.patrimoine.modele.possession.Possession;
 
 class JournalFactoryTest {
   private List<OperationComptable> operations;
-  private final Possession possession = new Compte("Épargne", now(), Argent.ariary(500_000));
+  private final Possession possession = new Compte("Épargne", now(), ariary(500_000));
 
   @BeforeEach
   void setUp() {
