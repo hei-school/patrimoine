17a405bd742adab664d5f2a0057273d5ce2f0a20
mihajatiana <hei.mihajatiana@gmail.com>
2026-03-01 18:21:30 +0300

    refactor: create debit and credit lines directly in the method

--- src/main/java/school/hei/patrimoine/modele/fec/factory/EcritureComptableFactory.java
+++ src/main/java/school/hei/patrimoine/modele/fec/factory/EcritureComptableFactory.java
@@ -3,13 +3,10 @@ package school.hei.patrimoine.modele.fec.factory;
 import static school.hei.patrimoine.modele.fec.PossessionCompteResolver.resolve;
 
 import java.util.List;
-import org.jspecify.annotations.NonNull;
-import school.hei.patrimoine.modele.Argent;
 import school.hei.patrimoine.modele.comptable.OperationComptable;
 import school.hei.patrimoine.modele.fec.EcritureComptable;
 import school.hei.patrimoine.modele.fec.Journal;
 import school.hei.patrimoine.modele.fec.LigneEcriture;
-import school.hei.patrimoine.modele.fec.PossessionCompteResolver.Comptes;
 import school.hei.patrimoine.modele.possession.pj.PieceJustificative;
 
 public class EcritureComptableFactory {
@@ -18,35 +15,27 @@ public class EcritureComptableFactory {
     var possession = operation.possession();
     var comptes = resolve(operation);
     var valeurRealisee = possession.valeurComptable();
-    var mouvementComptable = getMouvementComptable(comptes, valeurRealisee, operation);
 
-    return EcritureComptable.builder()
-        .id((journal.getNextId()))
-        .date(possession.t())
-        .libelle(possession.nom())
-        .lignes(List.of(mouvementComptable.ligneDebit(), mouvementComptable.ligneCredit()))
-        .dateValidation(pj != null ? pj.date() : null)
-        .build();
-  }
-
-  private static @NonNull MouvementComptable getMouvementComptable(
-      Comptes comptes, Argent valeurRealisee, OperationComptable operation) {
-    var ligneDebit =
+    var debit =
         LigneEcriture.builder()
             .compte(comptes.compteDébiteur())
             .flux(valeurRealisee)
             .type(operation.type())
             .build();
 
-    var ligneCredit =
+    var credit =
         LigneEcriture.builder()
             .compte(comptes.compteCréditeur())
             .flux(valeurRealisee.mult(-1))
             .type(operation.type())
             .build();
 
-    return new MouvementComptable(ligneDebit, ligneCredit);
+    return EcritureComptable.builder()
+        .id((journal.getNextId()))
+        .date(possession.t())
+        .libelle(possession.nom())
+        .lignes(List.of(debit, credit))
+        .dateValidation(pj != null ? pj.date() : null)
+        .build();
   }
-
-  private record MouvementComptable(LigneEcriture ligneDebit, LigneEcriture ligneCredit) {}
 }
