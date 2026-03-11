b1fbce96e55808f68f6fabadf6bbcf258cee09a6
mihajatiana <hei.mihajatiana@gmail.com>
2026-03-11 10:33:39 +0300

    refactor: use CompteComptable instead of Compte

--- src/main/java/school/hei/patrimoine/modele/fec/LigneEcriture.java
+++ src/main/java/school/hei/patrimoine/modele/fec/LigneEcriture.java
@@ -3,14 +3,12 @@ package school.hei.patrimoine.modele.fec;
 import java.time.LocalDate;
 import lombok.Builder;
 import school.hei.patrimoine.modele.Argent;
-import school.hei.patrimoine.modele.comptable.TypeComptable;
-import school.hei.patrimoine.modele.possession.Compte;
+import school.hei.patrimoine.modele.comptable.CompteComptable;
 
 @Builder
 public record LigneEcriture(
-    Compte compte,
-    Compte compteAuxiliaire,
+    CompteComptable compte,
+    CompteComptable compteAuxiliaire,
     Argent flux,
     String lettrage,
-    LocalDate dateLettrage,
-    TypeComptable type) {}
+    LocalDate dateLettrage) {}
