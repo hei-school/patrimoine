3acb395ecd5168c0af06fcda3f203b3009da39a1
mihajatiana <hei.mihajatiana@gmail.com>
2026-03-12 15:55:33 +0300

    refactor(OperationComptable): use CompteComptable
    # Conflicts:
    #	src/main/java/school/hei/patrimoine/modele/comptable/OperationComptable.java

--- src/main/java/school/hei/patrimoine/modele/comptable/OperationComptable.java
+++ src/main/java/school/hei/patrimoine/modele/comptable/OperationComptable.java
@@ -1,10 +1,31 @@
 package school.hei.patrimoine.modele.comptable;
 
+import static school.hei.patrimoine.modele.fec.PossessionCompteResolver.resolve;
+
+import lombok.Getter;
 import school.hei.patrimoine.modele.possession.Possession;
 
-public record OperationComptable(Possession possession, TypeComptable type) {
-  public OperationComptable(Possession possession, TypeComptable type) {
+@Getter
+public class OperationComptable {
+  private final Possession possession;
+  private final CompteComptable compteDebiteur;
+  private final CompteComptable compteCrediteur;
+
+  public OperationComptable(Possession possession) {
     this.possession = possession;
-    this.type = type;
+    var comptes = getComptesComptables();
+    this.compteDebiteur = comptes.compteDebiteur;
+    this.compteCrediteur = comptes.compteCrediteur;
   }
+
+  public static OperationComptable make(Possession possession) {
+    return new OperationComptable(possession);
+  }
+
+  private ComptesComptables getComptesComptables() {
+    var comptes = resolve(possession);
+    return new ComptesComptables(comptes.compteDébiteur(), comptes.compteCréditeur());
+  }
+
+  record ComptesComptables(CompteComptable compteDebiteur, CompteComptable compteCrediteur) {}
 }
