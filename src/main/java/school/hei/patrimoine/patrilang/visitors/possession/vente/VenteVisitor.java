02745ed0432ceb4ffda7271a3b8378fe720e299d
Sheddy <hei.sheddy.2@gmail.com>
2026-03-12 16:55:23 +0300

    feat(visitor): create VenteVisitor

--- src/main/java/school/hei/patrimoine/patrilang/visitors/possession/vente/VenteVisitor.java
+++ src/main/java/school/hei/patrimoine/patrilang/visitors/possession/vente/VenteVisitor.java
@@ -1,4 +1,26 @@
 package school.hei.patrimoine.patrilang.visitors.possession.vente;
 
-public class VenteVisitor {
+import lombok.RequiredArgsConstructor;
+import school.hei.patrimoine.modele.Argent;
+import school.hei.patrimoine.modele.possession.Compte;
+import school.hei.patrimoine.modele.possession.Possession;
+import school.hei.patrimoine.modele.possession.Vente;
+import school.hei.patrimoine.patrilang.antlr.PatriLangParser.VenteContext;
+import school.hei.patrimoine.patrilang.visitors.SimpleVisitor;
+import school.hei.patrimoine.patrilang.visitors.variable.VariableVisitor;
+
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+public class VenteVisitor implements SimpleVisitor<VenteContext, Vente> {
+    private final VariableVisitor variableVisitor;
+
+    @Override
+    public Vente apply(VenteContext ctx) {
+        LocalDate t = this.variableVisitor.asDate(ctx.dateValue);
+        Possession possession = this.variableVisitor.asPossession(ctx.possession);
+        Compte compteBeneficiaire = this.variableVisitor.asCompte(ctx.compteBeneficiaire);
+        Argent prixVente = this.variableVisitor.asArgent(ctx.prixVente);
+        return new Vente(t, possession, compteBeneficiaire, prixVente);
+    }
 }
