74dedd290c4b0a4553adab8f20a3c7e76d3236eb
mihajatiana <hei.mihajatiana@gmail.com>
2026-03-12 15:25:17 +0300

    feat: add classes to represent the FEC

---
+++ src/main/java/school/hei/patrimoine/modele/fec/EcritureComptable.java
@@ -0,0 +1,15 @@
+package school.hei.patrimoine.modele.fec;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Builder;
+import school.hei.patrimoine.modele.possession.pj.PieceJustificative;
+
+@Builder
+public record EcritureComptable(
+    String id,
+    LocalDate date,
+    String libelle,
+    LocalDate dateValidation,
+    List<LigneEcriture> lignes,
+    PieceJustificative pj) {}
---
+++ src/main/java/school/hei/patrimoine/modele/fec/Journal.java
@@ -0,0 +1,23 @@
+package school.hei.patrimoine.modele.fec;
+
+import java.util.ArrayList;
+import java.util.List;
+import school.hei.patrimoine.modele.comptable.OperationComptable;
+import school.hei.patrimoine.modele.fec.factory.EcritureComptableFactory;
+import school.hei.patrimoine.modele.possession.pj.PieceJustificative;
+
+public record Journal(JournalCode code, String libelle, List<EcritureComptable> ecritures) {
+
+  public Journal(JournalCode code, String libelle) {
+    this(code, libelle, new ArrayList<>());
+  }
+
+  public void addEcriture(OperationComptable operation, PieceJustificative pj) {
+    var ecritureToAdd = EcritureComptableFactory.make(this, operation, pj);
+    ecritures.add(ecritureToAdd);
+  }
+
+  public String getNextId() {
+    return code().toString() + String.format("%03d", ecritures.size());
+  }
+}
