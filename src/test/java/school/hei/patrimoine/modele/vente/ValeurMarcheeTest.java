e5e5069871635ee6b577ee574d4acb1eb13a92ae
Sheddy <hei.sheddy.2@gmail.com>
2026-03-12 14:29:46 +0300

    test: add ValeurMarcheTest

--- src/test/java/school/hei/patrimoine/modele/vente/ValeurMarcheeTest.java
+++ src/test/java/school/hei/patrimoine/modele/vente/ValeurMarcheeTest.java
@@ -1,4 +1,39 @@
 package school.hei.patrimoine.modele.vente;
 
-public class ValeurMarcheeTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import school.hei.patrimoine.modele.Argent;
+import school.hei.patrimoine.modele.possession.Materiel;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ValeurMarcheeTest {
+  LocalDate dateAcquisitionPc;
+  Argent valeurPcInitiale;
+  Materiel pc;
+  ValeurMarche subject;
+
+  @BeforeAll
+  void setUp() {
+    var now = LocalDate.now();
+
+    valeurPcInitiale = Argent.ariary(300_000);
+    dateAcquisitionPc = now.minusDays(7);
+    pc = new Materiel("Asus X555", dateAcquisitionPc, now, valeurPcInitiale, 10);
+  }
+
+  @Test
+  void creer_valeur_marchee() {
+    var dateValeurMarchee = dateAcquisitionPc.plusDays(3);
+    subject =
+        new ValeurMarche(
+            pc,
+            dateValeurMarchee,
+            valeurPcInitiale.minus(Argent.ariary(20_000), dateValeurMarchee));
+
+    assertEquals(subject, pc.getValeurMarche(dateValeurMarchee.plusDays(1)));
+  }
 }
