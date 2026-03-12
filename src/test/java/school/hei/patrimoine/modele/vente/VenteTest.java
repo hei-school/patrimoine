8d69fe3dc3d311732b96db867bde740e14079119
Sheddy <hei.sheddy.2@gmail.com>
2026-03-12 14:29:28 +0300

    test: add VenteTest

--- src/test/java/school/hei/patrimoine/modele/vente/VenteTest.java
+++ src/test/java/school/hei/patrimoine/modele/vente/VenteTest.java
@@ -1,4 +1,28 @@
 package school.hei.patrimoine.modele.vente;
 
-public class VenteTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import school.hei.patrimoine.modele.Argent;
+import school.hei.patrimoine.modele.possession.Compte;
+import school.hei.patrimoine.modele.possession.Materiel;
+import school.hei.patrimoine.modele.possession.Vente;
+
+class VenteTest {
+  @Test
+  void vendre_possession() {
+    var t = LocalDate.now();
+    var valeur = Argent.ariary(2000);
+    var dateAcquisition = LocalDate.now().minusDays(1);
+    var donut = new Materiel("donut", dateAcquisition, t, valeur, 0);
+
+    var compteBeneficiaire =
+        new Compte("Mon porte feuille", dateAcquisition.minusDays(1), Argent.ariary(0));
+
+    new Vente(t, donut, valeur, t.plusDays(1), compteBeneficiaire);
+
+    assertEquals(Argent.ariary(0), donut.valeurComptableFuture(t.plusDays(2)));
+    assertEquals(Argent.ariary(2000), compteBeneficiaire.valeurComptableFuture(t.plusDays(2)));
+  }
 }
