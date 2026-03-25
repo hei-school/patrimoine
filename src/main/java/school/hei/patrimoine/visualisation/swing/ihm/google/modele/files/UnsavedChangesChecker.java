2dcb5b0ef33eb655b67fb5d4f94506312a6818a5
Sheddy <hei.sheddy.2@gmail.com>
2026-03-25 11:56:22 +0300

    feat: add UnsavedChangesChecker to detect locally modified unsaved files

--- src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/modele/files/UnsavedChangesChecker.java
+++ src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/modele/files/UnsavedChangesChecker.java
@@ -1,4 +1,29 @@
 package school.hei.patrimoine.visualisation.swing.ihm.google.modele.files;
 
+import static school.hei.patrimoine.visualisation.swing.ihm.google.component.html.ViewMode.EDIT;
+import static school.hei.patrimoine.visualisation.swing.ihm.google.modele.files.PatriLangFileContentManager.getAllModifiedFiles;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import school.hei.patrimoine.visualisation.swing.ihm.google.component.html.HtmlViewer;
+import school.hei.patrimoine.visualisation.swing.ihm.google.component.html.ViewMode;
+
 public class UnsavedChangesChecker {
+  private static void flushEditorIfNeeded(ViewMode currentMode, HtmlViewer htmlViewer) {
+    if (EDIT.equals(currentMode)) {
+      htmlViewer.saveLastFileToTempContent();
+    }
+  }
+
+  public static boolean hasUnsavedChanges(ViewMode currentMode, HtmlViewer htmlViewer) {
+    flushEditorIfNeeded(currentMode, htmlViewer);
+    return !getAllModifiedFiles().isEmpty();
+  }
+
+  public static Set<String> getUnsavedFileNames(ViewMode currentMode, HtmlViewer htmlViewer) {
+    flushEditorIfNeeded(currentMode, htmlViewer);
+    return getAllModifiedFiles().stream()
+        .map(input -> input.file().getBaseFileName())
+        .collect(Collectors.toSet());
+  }
 }
