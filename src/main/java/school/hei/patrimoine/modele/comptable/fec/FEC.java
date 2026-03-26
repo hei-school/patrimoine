a62c84321cf6e1b21ff919bfee9d635ba78f0701
mihajatiana <hei.mihajatiana@gmail.com>
2026-03-26 16:42:13 +0300

    refactor: convert FEC to record

--- src/main/java/school/hei/patrimoine/modele/comptable/fec/FEC.java
+++ src/main/java/school/hei/patrimoine/modele/comptable/fec/FEC.java
@@ -4,13 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import school.hei.patrimoine.modele.comptable.fec.io.FECWriter;
 
-@RequiredArgsConstructor
-public class FEC {
-  private final List<Journal> journals;
-
+public record FEC(List<Journal> journals) {
   public File export(Path outputPath) throws IOException {
     try (var fecWriter = new FECWriter(outputPath)) {
       fecWriter.writeFEC(journals);
