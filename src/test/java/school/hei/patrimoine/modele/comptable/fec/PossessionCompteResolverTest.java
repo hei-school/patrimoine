8a5d08313c3349f919af92ae8731e27563caf24c
mihajatiana <hei.mihajatiana@gmail.com>
2026-04-01 21:52:33 +0300

    test: add more tests for PossessionCompteResolver

--- src/test/java/school/hei/patrimoine/modele/comptable/fec/PossessionCompteResolverTest.java
+++ src/test/java/school/hei/patrimoine/modele/comptable/fec/PossessionCompteResolverTest.java
@@ -2,47 +2,104 @@ package school.hei.patrimoine.modele.comptable.fec;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static school.hei.patrimoine.modele.Argent.ariary;
-import static school.hei.patrimoine.modele.comptable.MouvementComptable.CREDIT;
-import static school.hei.patrimoine.modele.comptable.MouvementComptable.DEBIT;
 import static school.hei.patrimoine.modele.comptable.fec.PossessionCompteResolver.resolve;
 
 import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import school.hei.patrimoine.modele.possession.Compte;
-import school.hei.patrimoine.modele.possession.TransfertArgent;
+import school.hei.patrimoine.modele.possession.*;
 
 class PossessionCompteResolverTest {
-  private static final Compte COMPTE_COURANT =
-      new Compte("Compte courant", LocalDate.of(2026, 1, 1), ariary(5_000_000));
-  private static final Compte COMPTE_EPARGNE =
-      new Compte("Compte épargne", LocalDate.of(2025, 1, 1), ariary(700_000));
-  private static final TransfertArgent TRANSFERT =
-      new TransfertArgent(
-          "Transfert vers épargne",
-          COMPTE_COURANT,
-          COMPTE_EPARGNE,
-          LocalDate.of(2026, 3, 10),
-          LocalDate.of(2026, 3, 10),
-          5,
-          ariary(1_000_000));
+  private Compte compte;
 
-  @Test
-  void should_return_two_compte_comptable() {
-    var comptes = resolve(TRANSFERT);
-    var debiteur = comptes.compteDébiteur().compte();
-    var crediteur = comptes.compteCréditeur().compte();
-
-    assertEquals(TRANSFERT.getVersCompte(), debiteur);
-    assertEquals(TRANSFERT.getDepuisCompte(), crediteur);
+  @BeforeEach
+  void setup() {
+    compte = new Compte("Compte principal", LocalDate.of(2026, 1, 1), ariary(5_000_000));
   }
 
   @Test
-  void should_have_correct_mouvement_for_operation() {
-    var comptes = resolve(TRANSFERT);
-    var debiteur = comptes.compteDébiteur();
-    var crediteur = comptes.compteCréditeur();
+  void should_handle_flux_argent_operation() {
+    var flux =
+        new FluxArgent(
+            "Entrée argent",
+            compte,
+            LocalDate.of(2026, 3, 10),
+            LocalDate.of(2026, 3, 10),
+            5,
+            ariary(100_000));
 
-    assertEquals(DEBIT, debiteur.mouvementComptable());
-    assertEquals(CREDIT, crediteur.mouvementComptable());
+    var comptes = resolve(flux);
+    var debiteur = comptes.compteDébiteur().compte();
+    var crediteur = comptes.compteCréditeur().compte();
+
+    assertEquals(flux.getCompte(), debiteur);
+    assertEquals("Compte d'attente", crediteur.nom());
+  }
+
+  @Test
+  void should_handle_transfert_argent_operation() {
+    var destinataire = new Compte("Compte épargne", LocalDate.of(2025, 1, 1), ariary(700_000));
+    var transfert =
+        new TransfertArgent(
+            "Transfert",
+            compte,
+            destinataire,
+            LocalDate.of(2026, 3, 10),
+            LocalDate.of(2026, 3, 10),
+            5,
+            ariary(1_000_000));
+
+    var comptes = resolve(transfert);
+    var debiteur = comptes.compteDébiteur().compte();
+    var crediteur = comptes.compteCréditeur().compte();
+
+    assertEquals(transfert.getVersCompte(), debiteur);
+    assertEquals(transfert.getDepuisCompte(), crediteur);
+  }
+
+  @Test
+  void should_handle_compte_operation() {
+    var compte = new Compte("Compte épargne", LocalDate.of(2025, 1, 1), ariary(700_000));
+
+    var comptes = resolve(compte);
+    var debiteur = comptes.compteDébiteur().compte();
+
+    assertEquals(compte, debiteur);
+  }
+
+  @Test
+  void should_handle_remboursement_dette_operation() {
+    var compteRembourseur = new Compte("Rembourseur", LocalDate.of(2025, 6, 1), ariary(8_000_000));
+    var dette = new Dette("Dette", LocalDate.of(2024, 7, 1), ariary(-500_000));
+    var creance = new Creance("Creance", LocalDate.of(2024, 7, 1), ariary(500_000));
+    var remboursement =
+        new RemboursementDette(
+            "Rembourserment dette",
+            compteRembourseur,
+            compte,
+            dette,
+            creance,
+            LocalDate.of(2026, 1, 31),
+            ariary(500_000));
+
+    var comptes = resolve(remboursement);
+    var debiteur = comptes.compteDébiteur().compte();
+    var crediteur = comptes.compteCréditeur().compte();
+
+    assertEquals(remboursement.getRemboursé(), debiteur);
+    assertEquals(remboursement.getRembourseur(), crediteur);
+  }
+
+  @Test
+  void should_handle_achat_materiel_au_comptant_operation() {
+    var achat =
+        new AchatMaterielAuComptant(
+            "Achat ordinateur", LocalDate.of(2026, 1, 31), ariary(500_000), 2.4, compte);
+    var comptes = resolve(achat);
+    var debiteur = comptes.compteDébiteur().compte();
+    var crediteur = comptes.compteCréditeur().compte();
+
+    assertEquals("Matériel", debiteur.nom());
+    assertEquals(achat.getFinanceur(), crediteur);
   }
 }
