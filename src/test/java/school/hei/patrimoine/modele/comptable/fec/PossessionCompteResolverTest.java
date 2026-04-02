b86228d88ecc0f91ad8422635d4569a0e4efa046
Sheddy <hei.sheddy.2@gmail.com>
2026-04-02 14:46:57 +0300

    chore: fix month on date using import static

--- src/test/java/school/hei/patrimoine/modele/comptable/fec/PossessionCompteResolverTest.java
+++ src/test/java/school/hei/patrimoine/modele/comptable/fec/PossessionCompteResolverTest.java
@@ -1,5 +1,6 @@
 package school.hei.patrimoine.modele.comptable.fec;
 
+import static java.time.Month.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static school.hei.patrimoine.modele.Argent.ariary;
 import static school.hei.patrimoine.modele.comptable.fec.PossessionCompteResolver.resolve;
@@ -14,7 +15,7 @@ class PossessionCompteResolverTest {
 
   @BeforeEach
   void setup() {
-    compte = new Compte("Compte principal", LocalDate.of(2026, 1, 1), ariary(5_000_000));
+    compte = new Compte("Compte principal", LocalDate.of(2026, JANUARY, 1), ariary(5_000_000));
   }
 
   @Test
@@ -23,8 +24,8 @@ class PossessionCompteResolverTest {
         new FluxArgent(
             "Entrée argent",
             compte,
-            LocalDate.of(2026, 3, 10),
-            LocalDate.of(2026, 3, 10),
+            LocalDate.of(2026, MARCH, 10),
+            LocalDate.of(2026, MARCH, 10),
             5,
             ariary(100_000));
 
@@ -38,14 +39,15 @@ class PossessionCompteResolverTest {
 
   @Test
   void should_handle_transfert_argent_operation() {
-    var destinataire = new Compte("Compte épargne", LocalDate.of(2025, 1, 1), ariary(700_000));
+    var destinataire =
+        new Compte("Compte épargne", LocalDate.of(2025, JANUARY, 1), ariary(700_000));
     var transfert =
         new TransfertArgent(
             "Transfert",
             compte,
             destinataire,
-            LocalDate.of(2026, 3, 10),
-            LocalDate.of(2026, 3, 10),
+            LocalDate.of(2026, MARCH, 10),
+            LocalDate.of(2026, MARCH, 10),
             5,
             ariary(1_000_000));
 
@@ -59,7 +61,7 @@ class PossessionCompteResolverTest {
 
   @Test
   void should_handle_compte_operation() {
-    var compte = new Compte("Compte épargne", LocalDate.of(2025, 1, 1), ariary(700_000));
+    var compte = new Compte("Compte épargne", LocalDate.of(2025, JANUARY, 1), ariary(700_000));
 
     var comptes = resolve(compte);
     var debiteur = comptes.compteDébiteur().compte();
@@ -69,9 +71,10 @@ class PossessionCompteResolverTest {
 
   @Test
   void should_handle_remboursement_dette_operation() {
-    var compteRembourseur = new Compte("Rembourseur", LocalDate.of(2025, 6, 1), ariary(8_000_000));
-    var dette = new Dette("Dette", LocalDate.of(2024, 7, 1), ariary(-500_000));
-    var creance = new Creance("Creance", LocalDate.of(2024, 7, 1), ariary(500_000));
+    var compteRembourseur =
+        new Compte("Rembourseur", LocalDate.of(2025, JUNE, 1), ariary(8_000_000));
+    var dette = new Dette("Dette", LocalDate.of(2024, JULY, 1), ariary(-500_000));
+    var creance = new Creance("Creance", LocalDate.of(2024, JULY, 1), ariary(500_000));
     var remboursement =
         new RemboursementDette(
             "Rembourserment dette",
@@ -79,7 +82,7 @@ class PossessionCompteResolverTest {
             compte,
             dette,
             creance,
-            LocalDate.of(2026, 1, 31),
+            LocalDate.of(2026, JANUARY, 31),
             ariary(500_000));
 
     var comptes = resolve(remboursement);
@@ -94,7 +97,7 @@ class PossessionCompteResolverTest {
   void should_handle_achat_materiel_au_comptant_operation() {
     var achat =
         new AchatMaterielAuComptant(
-            "Achat ordinateur", LocalDate.of(2026, 1, 31), ariary(500_000), 2.4, compte);
+            "Achat ordinateur", LocalDate.of(2026, JANUARY, 31), ariary(500_000), 2.4, compte);
     var comptes = resolve(achat);
     var debiteur = comptes.compteDébiteur().compte();
     var crediteur = comptes.compteCréditeur().compte();
