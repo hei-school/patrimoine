3928719fb8c503ebc231db8aeb29064ff95f0674
Sheddy <hei.sheddy.2@gmail.com>
2026-04-21 23:03:22 +0300

    feat: add method to validate id of pj with duplication then throw if true

--- src/main/java/school/hei/patrimoine/patrilang/files/validator/PJFileValidator.java
+++ src/main/java/school/hei/patrimoine/patrilang/files/validator/PJFileValidator.java
@@ -2,12 +2,30 @@ package school.hei.patrimoine.patrilang.files.validator;
 
 import static school.hei.patrimoine.patrilang.PatriLangTranspiler.transpilePieceJustificative;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Consumer;
+import school.hei.patrimoine.modele.possession.pj.PieceJustificative;
 import school.hei.patrimoine.patrilang.files.PatriLangFile;
 
 public class PJFileValidator implements Consumer<PatriLangFile> {
   @Override
   public void accept(PatriLangFile pj) {
-    transpilePieceJustificative(pj);
+    var pieces = transpilePieceJustificative(pj);
+    validateNoDuplicateId(pieces);
+  }
+
+  private void validateNoDuplicateId(List<PieceJustificative> pieces) {
+    Set<String> seenIds = new HashSet<>();
+    for (var pj : pieces) {
+      if (!seenIds.add(pj.id())) {
+        throw new IllegalArgumentException(
+            "Une opération ne devrait avoir qu'une seule pj."
+                + " La pièce justificative \""
+                + pj.id()
+                + "\" existe déjà");
+      }
+    }
   }
 }
