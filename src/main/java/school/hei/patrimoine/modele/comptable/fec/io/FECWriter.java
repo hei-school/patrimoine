37b455d16f86bbe34d03909fde4a2fcb4354c4d1
mihajatiana <hei.mihajatiana@gmail.com>
2026-05-07 15:54:35 +0300

    refactor: optimize FEC writing

--- src/main/java/school/hei/patrimoine/modele/comptable/fec/io/FECWriter.java
+++ src/main/java/school/hei/patrimoine/modele/comptable/fec/io/FECWriter.java
@@ -8,48 +8,36 @@ import static school.hei.patrimoine.modele.comptable.fec.FECLine.headers;
 import com.opencsv.CSVWriter;
 import java.io.*;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import school.hei.patrimoine.modele.comptable.fec.Journal;
 import school.hei.patrimoine.modele.comptable.fec.mapper.CompteNumResolver;
 import school.hei.patrimoine.modele.comptable.fec.mapper.FECLineMapper;
 
 public class FECWriter implements Closeable {
-  private final Writer writer;
   private final CSVWriter csvWriter;
 
   public FECWriter(Path path) throws IOException {
-    this.writer = new OutputStreamWriter(newOutputStream(path), UTF_8);
-
+    var writer = new BufferedWriter(new OutputStreamWriter(newOutputStream(path), UTF_8));
     this.csvWriter =
         new CSVWriter(writer, '\t', NO_QUOTE_CHARACTER, DEFAULT_ESCAPE_CHARACTER, DEFAULT_LINE_END);
   }
 
   public void writeFEC(Collection<Journal> journals) {
+    csvWriter.writeNext(headers());
+
     var mapper = new FECLineMapper(new CompteNumResolver());
-
-    List<String[]> allLines = new ArrayList<>();
-    allLines.add(headers());
-
     for (var journal : journals) {
       for (var ecriture : journal.ecritures()) {
         for (var ligne : ecriture.lignes()) {
           var line = mapper.toFECLine(journal, ecriture, ligne);
-          allLines.add(line.values());
+          csvWriter.writeNext(line.values());
         }
       }
-    }
-
-    int chunckSize = 1_000;
-    for (int i = 0; i < allLines.size(); i += chunckSize) {
-      csvWriter.writeAll(allLines.subList(i, Math.min(i + chunckSize, allLines.size())));
     }
   }
 
   @Override
   public void close() throws IOException {
     csvWriter.close();
-    writer.close();
   }
 }
